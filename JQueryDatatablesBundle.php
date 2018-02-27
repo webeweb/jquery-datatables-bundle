93452f0ae89c711dae43f5c83478769618d1ea20
webeweb <support@webeweb.fr>
2018-02-27 22:27:34 +0100

    Add main class

---
+++ JQueryDatatablesBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * jQuery Datatables bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\jQuery\DatatablesBundle
+ * @final
+ */
+final class JQueryDatatablesBundle extends Bundle {
+
+}
