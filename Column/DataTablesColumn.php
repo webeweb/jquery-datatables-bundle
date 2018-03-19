e8d6376f8f8564955054156a9eea4715ef4de445
webeweb <support@webeweb.fr>
2018-03-19 14:23:04 +0100

    Add DataTables column

---
+++ Column/DataTablesColumn.php
@@ -0,0 +1,526 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle\Column;
+
+use JsonSerializable;
+
+/**
+ * DataTables column.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DatatablesBundle\Column
+ * @final
+ */
+final class DataTablesColumn implements JsonSerializable {
+
+    /**
+     * Cell type.
+     *
+     * @var string
+     */
+    private $cellType = "td";
+
+    /**
+     * Class name.
+     *
+     * @var string
+     */
+    private $classname;
+
+    /**
+     * Content padding.
+     *
+     * @var string
+     */
+    private $contentPadding;
+
+    /**
+     * Data.
+     *
+     * @var integer|string
+     */
+    private $data;
+
+    /**
+     * Default content.
+     *
+     * @var string
+     */
+    private $defaultContent;
+
+    /**
+     * Name.
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Order data.
+     *
+     * @var integer|array
+     */
+    private $orderData;
+
+    /**
+     * Order data type.
+     *
+     * @var string
+     */
+    private $orderDataType;
+
+    /**
+     * Order sequence.
+     *
+     * @var string
+     */
+    private $orderSequence;
+
+    /**
+     * Orderable.
+     *
+     * @var boolean
+     */
+    private $orderable = true;
+
+    /**
+     * Searchable.
+     *
+     * @var boolean
+     */
+    private $searchable = true;
+
+    /**
+     * Title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Type.
+     *
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Visible.
+     *
+     * @var boolean
+     */
+    private $visible = true;
+
+    /**
+     * Width.
+     *
+     * @var string
+     */
+    private $width;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the cell type.
+     *
+     * @return string Returns the cell type.
+     */
+    public function getCellType() {
+        return $this->cellType;
+    }
+
+    /**
+     * Get the class name.
+     *
+     * @return string Returns the class name.
+     */
+    public function getClassname() {
+        return $this->classname;
+    }
+
+    /**
+     * Get the content padding.
+     *
+     * @return string Returns the content padding.
+     */
+    public function getContentPadding() {
+        return $this->contentPadding;
+    }
+
+    /**
+     * Get the data.
+     *
+     * @return integer|string Returns the data.
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * Get the default content
+     *
+     * @return string Returns the default content.
+     */
+    public function getDefaultContent() {
+        return $this->defaultContent;
+    }
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * Get the order data.
+     *
+     * @return integer|array Returns the order data.
+     */
+    public function getOrderData() {
+        return $this->orderData;
+    }
+
+    /**
+     * Get the order data type.
+     *
+     * @return string Returns the order data type.
+     */
+    public function getOrderDataType() {
+        return $this->orderDataType;
+    }
+
+    /**
+     * Get the order sequence.
+     *
+     * @return string Returns the order sequence.
+     */
+    public function getOrderSequence() {
+        return $this->orderSequence;
+    }
+
+    /**
+     * Get the orderable.
+     *
+     * @return boolean Returns the orderable.
+     */
+    public function getOrderable() {
+        return $this->orderable;
+    }
+
+    /**
+     * Get the searchable.
+     *
+     * @return boolean Returns the searchable.
+     */
+    public function getSearchable() {
+        return $this->searchable;
+    }
+
+    /**
+     * Get the title.
+     *
+     * @return string Returns the title.
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * Get the type.
+     *
+     * @return string Returns the type.
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Get the visible.
+     *
+     * @return boolean Returns the visible.
+     */
+    public function getVisible() {
+        return $this->visible;
+    }
+
+    /**
+     * Get the width.
+     *
+     * @return string Returns the width.
+     */
+    public function getWidth() {
+        return $this->width;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() {
+        return $this->toArray();
+    }
+
+    /**
+     * Set the cell type.
+     *
+     * @param string $cellType The cell type.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setCellType($cellType) {
+        switch ($cellType) {
+            case "td":
+            case "th":
+                $this->cellType = $cellType;
+        }
+        return $this;
+    }
+
+    /**
+     * Set the class name.
+     *
+     * @param string $classname The class name.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setClassname($classname) {
+        $this->classname = $classname;
+        return $this;
+    }
+
+    /**
+     * Set the content padding.
+     *
+     * @param string $contentPadding The content padding.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setContentPadding($contentPadding) {
+        $this->contentPadding = $contentPadding;
+        return $this;
+    }
+
+    /**
+     * Set the data.
+     *
+     * @param integer|strign $data The data.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setData($data) {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * Set the default content.
+     *
+     * @param string $defaultContent The default content.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setDefaultContent($defaultContent) {
+        $this->defaultContent = $defaultContent;
+        return $this;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $name The name.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Set the order data.
+     *
+     * @param integer|array $orderData The order data.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setOrderData($orderData) {
+        $this->orderData = $orderData;
+        return $this;
+    }
+
+    /**
+     * Set the order data type.
+     *
+     * @param string $orderDataType The order data type.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setOrderDataType($orderDataType) {
+        $this->orderDataType = $orderDataType;
+        return $this;
+    }
+
+    /**
+     * Set the order sequence.
+     *
+     * @param string $orderSequence The order sequence.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setOrderSequence($orderSequence) {
+        switch ($orderSequence) {
+            case "asc":
+            case "desc":
+                $this->orderSequence = $orderSequence;
+                break;
+            default:
+                $this->orderSequence = null;
+        }
+        return $this;
+    }
+
+    /**
+     * Set the orderable.
+     *
+     * @param boolean $orderable The orderable.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setOrderable($orderable) {
+        $this->orderable = $orderable;
+        return $this;
+    }
+
+    /**
+     * Set the searchable.
+     *
+     * @param boolean $searchable The searchable.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setSearchable($searchable) {
+        $this->searchable = $searchable;
+        return $this;
+    }
+
+    /**
+     * Set the title.
+     *
+     * @param string $title The title.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Set the type.
+     *
+     * @param string $type The type.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setType($type) {
+        switch ($type) {
+            case "date":
+            case "num":
+            case "num-fmt":
+            case "html":
+            case "html-num":
+            case "string":
+                $this->type = $type;
+                break;
+            default :
+                $this->type = null;
+        }
+        return $this;
+    }
+
+    /**
+     * Set the visible.
+     *
+     * @param boolean $visible The visible.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setVisible($visible) {
+        $this->visible = $visible;
+        return $this;
+    }
+
+    /**
+     * Set the width.
+     *
+     * @param string $width The width.
+     * @return DataTablesColumn Returns the DataTables column.
+     */
+    public function setWidth($width) {
+        $this->width = $width;
+        return $this;
+    }
+
+    /**
+     * Convert into an array representing this instance.
+     *
+     * @return array Returns an array representing this instance.
+     */
+    public function toArray() {
+
+        // Initialize the output.
+        $output = [];
+
+        if (null !== $this->cellType) {
+            $output["cellType"] = $this->cellType;
+        }
+        if (null !== $this->classname) {
+            $output["classname"] = $this->classname;
+        }
+        if (null !== $this->contentPadding) {
+            $output["contentPadding"] = $this->contentPadding;
+        }
+        if (null !== $this->data) {
+            $output["data"] = $this->data;
+        }
+        if (null !== $this->defaultContent) {
+            $output["defaultContent"] = $this->defaultContent;
+        }
+        if (null !== $this->name) {
+            $output["name"] = $this->name;
+        }
+        if (null !== $this->orderable && false === $this->orderable) {
+            $output["orderable"] = $this->orderable;
+        }
+        if (null !== $this->orderData) {
+            $output["orderData"] = $this->orderData;
+        }
+        if (null !== $this->orderDataType) {
+            $output["orderDataType"] = $this->orderDataType;
+        }
+        if (null !== $this->orderSequence) {
+            $output["orderSequence"] = $this->orderSequence;
+        }
+        if (null !== $this->searchable && false === $this->searchable) {
+            $output["searchable"] = $this->searchable;
+        }
+        if (null !== $this->title) {
+            $output["title"] = $this->title;
+        }
+        if (null !== $this->type) {
+            $output["type"] = $this->type;
+        }
+        if (null !== $this->visible && false === $this->visible) {
+            $output["visible"] = $this->visible;
+        }
+        if (null !== $this->width) {
+            $output["width"] = $this->width;
+        }
+
+        // Return the output.
+        return $output;
+    }
+
+}
---
+++ Tests/Column/DataTablesColumnTest.php
@@ -0,0 +1,369 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle\Tests\Column;
+
+use PHPUnit_Framework_TestCase;
+use WBW\Bundle\JQuery\DatatablesBundle\Column\DataTablesColumn;
+
+/**
+ * DataTables column test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DatatablesBundle\Tests\Column
+ * @final
+ */
+final class DataTablesColumnTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void.
+     */
+    public function testConstructor() {
+
+        $obj = new DataTablesColumn();
+
+        $this->assertEquals("td", $obj->getCellType());
+        $this->assertEquals(null, $obj->getClassname());
+        $this->assertEquals(null, $obj->getContentPadding());
+        $this->assertEquals(null, $obj->getData());
+        $this->assertEquals(null, $obj->getDefaultContent());
+        $this->assertEquals(null, $obj->getName());
+        $this->assertEquals(null, $obj->getOrderData());
+        $this->assertEquals(null, $obj->getOrderDataType());
+        $this->assertEquals(null, $obj->getOrderSequence());
+        $this->assertEquals(true, $obj->getSearchable());
+        $this->assertEquals(null, $obj->getTitle());
+        $this->assertEquals(null, $obj->getType());
+        $this->assertEquals(true, $obj->getVisible());
+        $this->assertEquals(null, $obj->getWidth());
+    }
+
+    /**
+     * Tests the setCellType() method.
+     *
+     * @return void
+     */
+    public function testSetCellType() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setCellType("exception");
+        $this->assertEquals("td", $obj->getCellType());
+
+        $obj->setCellType("th");
+        $this->assertEquals("th", $obj->getCellType());
+    }
+
+    /**
+     * Tests the setClassname() method.
+     *
+     * @return void
+     */
+    public function testSetClassname() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setClassname("classname");
+        $this->assertEquals("classname", $obj->getClassname());
+    }
+
+    /**
+     * Tests the setContentPadding() method.
+     *
+     * @return void
+     */
+    public function testSetContentPadding() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setContentPadding("contentPadding");
+        $this->assertEquals("contentPadding", $obj->getContentPadding());
+    }
+
+    /**
+     * Tests the setData() method.
+     *
+     * @return void
+     */
+    public function testSetData() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setData("data");
+        $this->assertEquals("data", $obj->getData());
+    }
+
+    /**
+     * Tests the setDefaultContent() method.
+     *
+     * @return void
+     */
+    public function testSetDefaultContent() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setDefaultContent("defaultContent");
+        $this->assertEquals("defaultContent", $obj->getDefaultContent());
+    }
+
+    /**
+     * Tests the setName() method.
+     *
+     * @return void
+     */
+    public function testSetName() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setName("name");
+        $this->assertEquals("name", $obj->getName());
+    }
+
+    /**
+     * Tests the setOrderData() method.
+     *
+     * @return void
+     */
+    public function testSetOrderData() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setOrderData(1);
+        $this->assertEquals(1, $obj->getOrderData());
+    }
+
+    /**
+     * Tests the setOrderDataType() method.
+     *
+     * @return void
+     */
+    public function testSetOrderDataType() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setOrderDataType("orderDataType");
+        $this->assertEquals("orderDataType", $obj->getOrderDataType());
+    }
+
+    /**
+     * Tests the setOrderSequence() method.
+     *
+     * @return void
+     */
+    public function testSetOrderSequence() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setOrderSequence("exception");
+        $this->assertEquals(null, $obj->getOrderSequence());
+
+        $obj->setOrderSequence("asc");
+        $this->assertEquals("asc", $obj->getOrderSequence());
+
+        $obj->setOrderSequence("desc");
+        $this->assertEquals("desc", $obj->getOrderSequence());
+    }
+
+    /**
+     * Tests the setOrderable() method.
+     *
+     * @return void
+     */
+    public function testSetOrderable() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setOrderable(false);
+        $this->assertEquals(false, $obj->getOrderable());
+    }
+
+    /**
+     * Tests the setSearchable() method.
+     *
+     * @return void
+     */
+    public function testSetSearchable() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setSearchable(false);
+        $this->assertEquals(false, $obj->getSearchable());
+    }
+
+    /**
+     * Tests the setTitle() method.
+     *
+     * @return void
+     */
+    public function testSetTitle() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setTitle("title");
+        $this->assertEquals("title", $obj->getTitle());
+    }
+
+    /**
+     * Tests the setType() method.
+     *
+     * @return void
+     */
+    public function testSetType() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setType("exception");
+        $this->assertEquals(null, $obj->getType());
+
+        $obj->setType("date");
+        $this->assertEquals("date", $obj->getType());
+
+        $obj->setType("num");
+        $this->assertEquals("num", $obj->getType());
+
+        $obj->setType("num-fmt");
+        $this->assertEquals("num-fmt", $obj->getType());
+
+        $obj->setType("html");
+        $this->assertEquals("html", $obj->getType());
+
+        $obj->setType("html-num");
+        $this->assertEquals("html-num", $obj->getType());
+
+        $obj->setType("string");
+        $this->assertEquals("string", $obj->getType());
+    }
+
+    /**
+     * Tests the setVisible() method.
+     *
+     * @return void
+     */
+    public function testSetVisible() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setVisible(false);
+        $this->assertEquals(false, $obj->getVisible());
+    }
+
+    /**
+     * Tests the setWidth() method.
+     *
+     * @return void
+     */
+    public function testSetWidth() {
+
+        $obj = new DataTablesColumn();
+
+        $obj->setWidth("width");
+        $this->assertEquals("width", $obj->getWidth());
+    }
+
+    /**
+     * Tests the toArray() method.
+     *
+     * @return void
+     */
+    public function testToArray() {
+
+        $obj = new DataTablesColumn();
+
+        $res1 = ["cellType" => "td"];
+        $this->assertEquals($res1, $obj->toArray());
+
+        $obj->setClassname("classname");
+        $res2 = ["cellType" => "td", "classname" => "classname"];
+        $this->assertEquals($res2, $obj->toArray());
+
+        $obj->setClassname(null);
+        $obj->setContentPadding("contentPadding");
+        $res3 = ["cellType" => "td", "contentPadding" => "contentPadding"];
+        $this->assertEquals($res3, $obj->toArray());
+
+        $obj->setContentPadding(null);
+        $obj->setData("data");
+        $res4 = ["cellType" => "td", "data" => "data"];
+        $this->assertEquals($res4, $obj->toArray());
+
+        $obj->setData(null);
+        $obj->setDefaultContent("defaultContent");
+        $res5 = ["cellType" => "td", "defaultContent" => "defaultContent"];
+        $this->assertEquals($res5, $obj->toArray());
+
+        $obj->setDefaultContent(null);
+        $obj->setName("name");
+        $res6 = ["cellType" => "td", "name" => "name"];
+        $this->assertEquals($res6, $obj->toArray());
+
+        $obj->setName(null);
+        $obj->setOrderData("orderData");
+        $res7 = ["cellType" => "td", "orderData" => "orderData"];
+        $this->assertEquals($res7, $obj->toArray());
+
+        $obj->setOrderData(null);
+        $obj->setOrderDataType("orderDataType");
+        $res8 = ["cellType" => "td", "orderDataType" => "orderDataType"];
+        $this->assertEquals($res8, $obj->toArray());
+
+        $obj->setOrderDataType(null);
+        $obj->setOrderSequence("asc");
+        $res9 = ["cellType" => "td", "orderSequence" => "asc"];
+        $this->assertEquals($res9, $obj->toArray());
+
+        $obj->setOrderSequence(null);
+        $obj->setOrderable(false);
+        $res10 = ["cellType" => "td", "orderable" => false];
+        $this->assertEquals($res10, $obj->toArray());
+
+        $obj->setOrderable(true);
+        $obj->setSearchable(false);
+        $res11 = ["cellType" => "td", "searchable" => false];
+        $this->assertEquals($res11, $obj->toArray());
+
+        $obj->setSearchable(true);
+        $obj->setTitle("title");
+        $res12 = ["cellType" => "td", "title" => "title"];
+        $this->assertEquals($res12, $obj->toArray());
+
+        $obj->setTitle(null);
+        $obj->setType("string");
+        $res13 = ["cellType" => "td", "type" => "string"];
+        $this->assertEquals($res13, $obj->toArray());
+
+        $obj->setType(null);
+        $obj->setVisible(false);
+        $res14 = ["cellType" => "td", "visible" => false];
+        $this->assertEquals($res14, $obj->toArray());
+
+        $obj->setVisible(true);
+        $obj->setWidth("width");
+        $res15 = ["cellType" => "td", "width" => "width"];
+        $this->assertEquals($res15, $obj->toArray());
+    }
+
+    /**
+     * Tests the jsonSerialize() method.
+     *
+     * @return void
+     * @depends testToArray
+     */
+    public function testJsonSerialize() {
+
+        $obj = new DataTablesColumn();
+
+        $res = ["cellType" => "td"];
+        $this->assertEquals($res, $obj->jsonSerialize());
+    }
+
+}
