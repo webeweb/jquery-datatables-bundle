5b4151f3f60319e3c00efe0e79f46ebb82367a46
webeweb <support@webeweb.fr>
2018-03-19 21:49:23 +0100

    Add DataTables provider interface

---
+++ Provider/DataTablesProviderInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle\Provider;
+
+/**
+ * DataTables provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package namespace WBW\Bundle\JQuery\DatatablesBundle\Provider;
+ */
+interface DataTablesProviderInterface {
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName();
+}
