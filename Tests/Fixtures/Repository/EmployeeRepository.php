8b1b5fd3dca15352cd7f95321c272478fa23a3f9
webeweb <support@webeweb.fr>
2018-04-06 16:43:13 +0200

    Add Employee repository

---
+++ Tests/Fixtures/Repository/EmployeeRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle\Tests\Fixtures\Repository;
+
+use WBW\Bundle\JQuery\DatatablesBundle\Repository\DefaultDataTablesRepository;
+
+/**
+ * Employee repository.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DatatablesBundle\Tests\Fixtures\Repository
+ * @final
+ */
+final class EmployeeRepository extends DefaultDataTablesRepository {
+
+}
