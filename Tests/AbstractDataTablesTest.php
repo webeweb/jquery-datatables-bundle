68121d941ebc58685a00e4ce774b4a6af6410759
webeweb <support@webeweb.fr>
2018-04-09 22:34:57 +0200

    Add Abstract DataTables test

---
+++ Tests/AbstractDataTablesTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * Disclaimer: This source code is protected by copyright law and by
+ * international conventions.
+ *
+ * Any reproduction or partial or total distribution of the source code, by any
+ * means whatsoever, is strictly forbidden.
+ *
+ * Anyone not complying with these provisions will be guilty of the offense of
+ * infringement and the penal sanctions provided for by law.
+ *
+ * © 2018 All rights reserved.
+ */
+
+namespace WBW\Bundle\JQuery\DatatablesBundle\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use WBW\Bundle\JQuery\DatatablesBundle\Request\DataTablesRequest;
+use WBW\Bundle\JQuery\DatatablesBundle\Response\DataTablesResponse;
+use WBW\Bundle\JQuery\DatatablesBundle\Wrapper\DataTablesWrapper;
+
+/**
+ * Abstract DataTables test.
+ *
+ * @author Camille A. <camille@ingeneo.eu>
+ * @package WBW\Bundle\JQuery\DatatablesBundle\Tests
+ */
+abstract class AbstractDataTablesTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * DataTables request.
+     *
+     * @var DataTablesRequest
+     */
+    protected $dataTablesRequest;
+
+    /**
+     * DataTables response.
+     *
+     * @var DataTablesResponse
+     */
+    protected $dataTablesResponse;
+
+    /**
+     * DataTables wrapper.
+     *
+     * @var DataTablesWrapper
+     */
+    protected $dataTablesWrapper;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() {
+
+        // Set a DataTables wrappper mock.
+        $this->dataTablesWrapper = new DataTablesWrapper("prefix", "POST", "route");
+
+        // Set a DataTables request mock.
+        $this->dataTablesRequest = new DataTablesRequest($this->dataTablesWrapper, new Request());
+
+        // Set a DataTables response mock.
+        $this->dataTablesResponse = new DataTablesResponse($this->dataTablesRequest);
+    }
+
+}
