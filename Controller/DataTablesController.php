ec9af52897ae7b374ff29c9e33aa33145c40e185
webeweb <support@webeweb.fr>
2018-04-22 16:24:59 +0200

    Split indexAction() and add log traces

--- Controller/DataTablesController.php
+++ Controller/DataTablesController.php
@@ -11,6 +11,7 @@
 
 namespace WBW\Bundle\JQuery\DatatablesBundle\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,7 @@ use WBW\Bundle\JQuery\DatatablesBundle\API\DataTablesWrapper;
 use WBW\Bundle\JQuery\DatatablesBundle\Exception\BadDataTablesRepositoryException;
 use WBW\Bundle\JQuery\DatatablesBundle\Exception\UnregisteredDataTablesProviderException;
 use WBW\Bundle\JQuery\DatatablesBundle\Manager\DataTablesManager;
+use WBW\Bundle\JQuery\DatatablesBundle\Provider\DataTablesProviderInterface;
 use WBW\Bundle\JQuery\DatatablesBundle\Repository\DataTablesRepositoryInterface;
 use WBW\Library\Core\IO\HTTPInterface;
 
@@ -32,23 +34,75 @@ use WBW\Library\Core\IO\HTTPInterface;
 final class DataTablesController extends Controller {
 
     /**
+     * Get the DataTables provider.
+     *
+     * @return DataTablesProviderInterface Returns the DataTables provider.
+     */
+    private function getDataTablesProvider($name) {
+
+        // Log a debug trace.
+        $this->getLogger()->debug(sprintf("DataTables controller search for a DataTables provider with name \"%s\"", $name));
+
+        // Get the DataTables provider.
+        $dtProvider = $this->get(DataTablesManager::SERVICE_NAME)->getProvider($name);
+
+        // Log an info trace.
+        $this->getLogger()->info(sprintf("DataTables controller found a DataTables provider with name \"%s\"", $name));
+
+        // Return the DataTables provider.
+        return $dtProvider;
+    }
+
+    /**
+     * Get a DataTables wrapper.
+     *
+     * @param DataTablesProviderInterface $dtProvider The DataTables provider.
+     * @return DataTablesWrapper Returns the DataTables wrapper.
+     */
+    private function getDataTablesWrapper(DataTablesProviderInterface $dtProvider) {
+
+        // Initialize the DataTables wrapper.
+        $dtWrapper = new DataTablesWrapper($dtProvider->getPrefix(), HTTPInterface::HTTP_METHOD_POST, "jquery_datatables_index", ["name" => $dtProvider->getName()]);
+
+        // Handle each column.
+        foreach ($dtProvider->getColumns() as $dtColumn) {
+
+            // Log a debug trace.
+            $this->getLogger()->debug(sprintf("DataTables provider add a DataTables column \"%s\"", $dtColumn->getData()));
+
+            // Add.
+            $dtWrapper->addColumn($dtColumn);
+        }
+
+        // Return the DataTables wrapper.
+        return $dtWrapper;
+    }
+
+    /**
+     * Get the logger.
+     *
+     * @return LoggerInterface Returns the logger.
+     */
+    private function getLogger() {
+        return $this->get("logger");
+    }
+
+    /**
      * Lists all entities.
      *
      * @param Request $request The request.
      * @param string $name The provider name.
      * @return Response Returns the response.
      * @throws UnregisteredDataTablesProviderException Throws an unregistered DataTables provider exception.
+     * @throws BadDataTablesRepositoryException Throws a bad DataTables repository exception.
      */
     public function indexAction(Request $request, $name) {
 
         // Get the provider.
-        $dtProvider = $this->get(DataTablesManager::SERVICE_NAME)->getProvider($name);
+        $dtProvider = $this->getDataTablesProvider($name);
 
-        // Initialize the DataTables wrapper.
-        $dtWrapper = new DataTablesWrapper($dtProvider->getPrefix(), HTTPInterface::HTTP_METHOD_POST, "jquery_datatables_index", ["name" => $name]);
-        foreach ($dtProvider->getColumns() as $dtColumn) {
-            $dtWrapper->addColumn($dtColumn);
-        }
+        // Get the wrapper.
+        $dtWrapper = $this->getDataTablesWrapper($dtProvider);
 
         // Check if the request is an XML HTTP request.
         if (true === $request->isXmlHttpRequest()) {
