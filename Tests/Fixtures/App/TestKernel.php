7ef56e44c4e40c97a442d199d0fa7e68d2f22dee
webeweb <support@webeweb.fr>
2018-06-05 09:24:19 +0200

    Update file headers

--- Tests/Fixtures/App/TestKernel.php
+++ Tests/Fixtures/App/TestKernel.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the edm-bundle package.
+ * This file is part of the jquery-datatables-bundle package.
  *
- * (c) 2017 WEBEWEB
+ * (c) 2018 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
