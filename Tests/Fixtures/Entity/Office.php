226d16a702d2a6b74c5e453e956ab91c9e7cb683
webeweb <support@webeweb.fr>
2018-07-31 09:04:52 +0200

    Add Office entity

---
+++ Tests/Fixtures/Entity/Office.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Entity;
+
+/**
+ * Office.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Entity
+ */
+class Office {
+
+    /**
+     * Id.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Name.
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the id.
+     *
+     * @return integer Returns the id.
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $name The name.
+     * @return Office Returns this office.
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+}
---
+++ Tests/Fixtures/Repository/OfficeRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Office repository.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Repository
+ * @final
+ */
+final class OfficeRepository extends EntityRepository {
+
+}
