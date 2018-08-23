d2b6592b5cbd6b1c1e2341a05575a854ca55dd85
webeweb <support@webeweb.fr>
2018-08-23 14:07:49 +0200

    Add DataTables mapping helper

---
+++ Helper/DataTablesMappingHelper.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Helper;
+
+use WBW\Bundle\JQuery\DataTablesBundle\API\DataTablesMapping;
+
+/**
+ * DataTables mapping helper.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Helper
+ */
+class DataTablesMappingHelper {
+
+    /**
+     * Get an alias.
+     *
+     * @param DataTablesMapping $mapping The mapping.
+     * @return string Returns the alias.
+     */
+    public static function getAlias(DataTablesMapping $mapping) {
+        if (null === $mapping->getColumn()) {
+            return null;
+        }
+        if (null === $mapping->getPrefix()) {
+            return $mapping->getColumn();
+        }
+        return implode(".", [$mapping->getPrefix(), $mapping->getColumn()]);
+    }
+
+    /**
+     * Get a param.
+     *
+     * @param DataTablesMapping $mapping The mapping.
+     * @return string Returns the param.
+     */
+    public static function getParam(DataTablesMapping $mapping) {
+
+        // Initialize the param.
+        $param = ":";
+        $param .= $mapping->getPrefix();
+        $param .= $mapping->getColumn();
+
+        // Return the param.
+        return $param;
+    }
+
+    /**
+     * Get a mapping where.
+     *
+     * @param DataTablesMapping $mapping The mapping.
+     * @return string Returns the where.
+     */
+    public static function getWhere(DataTablesMapping $mapping) {
+
+        // Initialize the where.
+        $where = self::getAlias($mapping);
+        $where .= " LIKE ";
+        $where .= self::getParam($mapping);
+
+        // Return the where.
+        return $where;
+    }
+
+}
---
+++ Tests/Helper/DataTablesMappingHelperTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Helper;
+
+use WBW\Bundle\JQuery\DataTablesBundle\API\DataTablesMapping;
+use WBW\Bundle\JQuery\DataTablesBundle\Helper\DataTablesMappingHelper;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Cases\AbstractJQueryDataTablesFrameworkTestCase;
+
+/**
+ * DataTables mapping helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Helper
+ * @final
+ */
+final class DataTablesMappingHelperTest extends AbstractJQueryDataTablesFrameworkTestCase {
+
+    /**
+     * Tests the getAlias() method.
+     *
+     * @return void
+     */
+    public function testGetAlias() {
+
+        $obj = new DataTablesMapping();
+
+        $this->assertNull(DataTablesMappingHelper::getAlias($obj));
+
+        $obj->setColumn("column");
+        $this->assertEquals("column", DataTablesMappingHelper::getAlias($obj));
+
+        $obj->setPrefix("prefix");
+        $this->assertEquals("prefix.column", DataTablesMappingHelper::getAlias($obj));
+    }
+
+    /**
+     * Tests the getParam() method.
+     *
+     * @return void
+     */
+    public function testGetParam() {
+
+        $obj = new DataTablesMapping();
+        $obj->setPrefix("prefix");
+        $obj->setColumn("column");
+
+        $this->assertEquals(":prefixcolumn", DataTablesMappingHelper::getParam($obj));
+    }
+
+    /**
+     * Tests the getWhere() method.
+     *
+     * @return void
+     */
+    public function testGetWhere() {
+
+        $obj = new DataTablesMapping();
+        $obj->setPrefix("prefix");
+        $obj->setColumn("column");
+
+        $this->assertEquals("prefix.column LIKE :prefixcolumn", DataTablesMappingHelper::getWhere($obj));
+    }
+
+}
