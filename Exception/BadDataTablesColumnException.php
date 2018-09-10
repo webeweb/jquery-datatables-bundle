c82ed1f3d5043785caea708ebe009f1359c4e4e2
webeweb <support@webeweb.fr>
2018-09-10 20:53:43 +0200

    Add Bad DataTables column exception

---
+++ Exception/BadDataTablesColumnException.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Exception;
+
+/**
+ * Bad DataTables column exception.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Exception
+ */
+class BadDataTablesColumnException extends AbstractDataTablesException {
+
+    /**
+     * Constructor.
+     *
+     * @param string $name The column name.
+     */
+    public function __construct($name) {
+        parent::__construct(sprintf("The DataTables column with name \"%s\" does not exist", $name));
+    }
+
+}
---
+++ Tests/Exception/BadDataTablesColumnExceptionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Exception;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Exception\BadDataTablesColumnException;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Cases\AbstractJQueryDataTablesFrameworkTestCase;
+
+/**
+ * Bad DataTables column exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Exception
+ * @final
+ */
+final class BadDataTablesColumnExceptionTest extends AbstractJQueryDataTablesFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BadDataTablesColumnException("exception");
+
+        $res = "The DataTables column with name \"exception\" does not exist";
+        $this->assertEquals($res, $obj->getMessage());
+    }
+
+}
