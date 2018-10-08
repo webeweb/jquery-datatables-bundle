52ce69951dd02bd735d73707a33581e54c24e83e
webeweb <support@webeweb.fr>
2018-10-08 20:33:31 +0200

    Update DataTables search unit test

--- Tests/API/DataTablesSearchTest.php
+++ Tests/API/DataTablesSearchTest.php
@@ -25,39 +25,14 @@ use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\TestFixtures;
 final class DataTablesSearchTest extends AbstractFrameworkTestCase {
 
     /**
-     * Tests the parse() method.
-     *
-     * @return void
+     * Tests the __construct() method.
      */
-    public function testParse() {
+    public function testConstruct() {
 
-        // Get the POST data.
-        $postData = TestFixtures::getPOSTData();
+        $obj = new DataTablesSearch();
 
-        // Set the POST data.
-        $postData["search"]["regex"] = "true";
-        $postData["search"]["value"] = "value";
-
-        // ===
-        $res = DataTablesSearch::parse($postData["search"]);
-        $this->assertTrue($res->getRegex());
-        $this->assertEquals("value", $res->getValue());
-
-        // Set an invalid search.
-        $postData["search"]["regex"] = "false";
-        unset($postData["search"]["value"]);
-
-        $res1 = DataTablesSearch::parse($postData["search"]);
-        $this->assertFalse($res1->getRegex());
-        $this->assertEquals("", $res1->getValue());
-
-        // Set an invalid search.
-        unset($postData["search"]["regex"]);
-        unset($postData["search"]["value"]);
-
-        $res2 = DataTablesSearch::parse($postData["search"]);
-        $this->assertFalse($res2->getRegex());
-        $this->assertEquals("", $res2->getValue());
+        $this->assertNull($obj->getRegex());
+        $this->assertNull($obj->getValue());
     }
 
 }
