88158b6cfe6e90dc335287f7995b7090089c916e
webeweb <support@webeweb.fr>
2018-10-15 12:07:23 +0200

    Add DataTables search interface unit tests

---
+++ Tests/API/DataTablesSearchInterfaceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\API;
+
+use WBW\Bundle\JQuery\DataTablesBundle\API\DataTablesSearchInterface;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * DataTables search interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\API
+ * @final
+ */
+final class DataTablesSearchInterfaceTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("regex", DataTablesSearchInterface::DATATABLES_PARAMETER_REGEX);
+        $this->assertEquals("value", DataTablesSearchInterface::DATATABLES_PARAMETER_VALUE);
+    }
+
+}
