1bbd11af011e2badbed38999af0cb200f4d5ec2d
webeweb <support@webeweb.fr>
2018-10-15 11:13:00 +0200

    Create a DataTables wrapper interface

---
+++ API/DataTablesWrapperInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\API;
+
+/**
+ * DataTables wrapper interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\API
+ */
+interface DataTablesWrapperInterface {
+
+}
