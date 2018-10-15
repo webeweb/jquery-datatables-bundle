c695833bb8b0067345d340302bca1bdf8ebb4334
webeweb <support@webeweb.fr>
2018-10-15 12:02:56 +0200

    Create DataTables order interface unit tests

---
+++ Tests/API/DataTablesOrderInterfaceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\API;
+
+use WBW\Bundle\JQuery\DataTablesBundle\API\DataTablesOrderInterface;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * DataTables order interface test.
+ *
+ * @author Camille A. <camille@ingeneo.eu>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\API
+ * @final
+ */
+final class DataTablesOrderInterfaceTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("asc", DataTablesOrderInterface::DATATABLES_DIR_ASC);
+        $this->assertEquals("desc", DataTablesOrderInterface::DATATABLES_DIR_DESC);
+        $this->assertEquals("column", DataTablesOrderInterface::DATATABLES_PARAMETER_COLUMN);
+        $this->assertEquals("dir", DataTablesOrderInterface::DATATABLES_PARAMETER_DIR);
+    }
+
+}
