5fb021872bd6b22f88d7840bab3db13eed184844
webeweb <support@webeweb.fr>
2018-10-25 11:52:15 +0200

    Rename var

--- Tests/Fixtures/TestFixtures.php
+++ Tests/Fixtures/TestFixtures.php
@@ -105,81 +105,81 @@ final class TestFixtures {
     public static function getPOSTData() {
 
         // Initialize.
-        $output = [];
+        $fixtures = [];
 
         // Name
-        $output["columns"][0]["data"]            = "name";
-        $output["columns"][0]["name"]            = "Name";
-        $output["columns"][0]["orderable"]       = "true";
-        $output["columns"][0]["search"]["regex"] = "false";
-        $output["columns"][0]["search"]["value"] = "";
-        $output["columns"][0]["searchable"]      = "true";
+        $fixtures["columns"][0]["data"]            = "name";
+        $fixtures["columns"][0]["name"]            = "Name";
+        $fixtures["columns"][0]["orderable"]       = "true";
+        $fixtures["columns"][0]["search"]["regex"] = "false";
+        $fixtures["columns"][0]["search"]["value"] = "";
+        $fixtures["columns"][0]["searchable"]      = "true";
 
         // Position
-        $output["columns"][1]["data"]            = "position";
-        $output["columns"][1]["name"]            = "Position";
-        $output["columns"][1]["orderable"]       = "true";
-        $output["columns"][1]["search"]["regex"] = "false";
-        $output["columns"][1]["search"]["value"] = "";
-        $output["columns"][1]["searchable"]      = "true";
+        $fixtures["columns"][1]["data"]            = "position";
+        $fixtures["columns"][1]["name"]            = "Position";
+        $fixtures["columns"][1]["orderable"]       = "true";
+        $fixtures["columns"][1]["search"]["regex"] = "false";
+        $fixtures["columns"][1]["search"]["value"] = "";
+        $fixtures["columns"][1]["searchable"]      = "true";
 
         // Office
-        $output["columns"][2]["data"]            = "office";
-        $output["columns"][2]["name"]            = "Office";
-        $output["columns"][2]["orderable"]       = "true";
-        $output["columns"][2]["search"]["regex"] = "false";
-        $output["columns"][2]["search"]["value"] = "";
-        $output["columns"][2]["searchable"]      = "true";
+        $fixtures["columns"][2]["data"]            = "office";
+        $fixtures["columns"][2]["name"]            = "Office";
+        $fixtures["columns"][2]["orderable"]       = "true";
+        $fixtures["columns"][2]["search"]["regex"] = "false";
+        $fixtures["columns"][2]["search"]["value"] = "";
+        $fixtures["columns"][2]["searchable"]      = "true";
 
         // Age
-        $output["columns"][3]["data"]            = "age";
-        $output["columns"][3]["name"]            = "Age";
-        $output["columns"][3]["orderable"]       = "true";
-        $output["columns"][3]["search"]["regex"] = "false";
-        $output["columns"][3]["search"]["value"] = "";
-        $output["columns"][3]["searchable"]      = "true";
+        $fixtures["columns"][3]["data"]            = "age";
+        $fixtures["columns"][3]["name"]            = "Age";
+        $fixtures["columns"][3]["orderable"]       = "true";
+        $fixtures["columns"][3]["search"]["regex"] = "false";
+        $fixtures["columns"][3]["search"]["value"] = "";
+        $fixtures["columns"][3]["searchable"]      = "true";
 
         // Start date
-        $output["columns"][4]["data"]            = "startDate";
-        $output["columns"][4]["name"]            = "Start date";
-        $output["columns"][4]["orderable"]       = "true";
-        $output["columns"][4]["search"]["regex"] = "false";
-        $output["columns"][4]["search"]["value"] = "";
-        $output["columns"][4]["searchable"]      = "true";
+        $fixtures["columns"][4]["data"]            = "startDate";
+        $fixtures["columns"][4]["name"]            = "Start date";
+        $fixtures["columns"][4]["orderable"]       = "true";
+        $fixtures["columns"][4]["search"]["regex"] = "false";
+        $fixtures["columns"][4]["search"]["value"] = "";
+        $fixtures["columns"][4]["searchable"]      = "true";
 
         // Salary
-        $output["columns"][5]["data"]            = "salary";
-        $output["columns"][5]["name"]            = "Salary";
-        $output["columns"][5]["orderable"]       = "true";
-        $output["columns"][5]["search"]["regex"] = "false";
-        $output["columns"][5]["search"]["value"] = "";
-        $output["columns"][5]["searchable"]      = "true";
+        $fixtures["columns"][5]["data"]            = "salary";
+        $fixtures["columns"][5]["name"]            = "Salary";
+        $fixtures["columns"][5]["orderable"]       = "true";
+        $fixtures["columns"][5]["search"]["regex"] = "false";
+        $fixtures["columns"][5]["search"]["value"] = "";
+        $fixtures["columns"][5]["searchable"]      = "true";
 
         // Actions.
-        $output["columns"][6]["data"]            = "actions";
-        $output["columns"][6]["name"]            = "Actions";
-        $output["columns"][6]["orderable"]       = "false";
-        $output["columns"][6]["search"]["regex"] = "false";
-        $output["columns"][6]["search"]["value"] = "";
-        $output["columns"][6]["searchable"]      = "false";
+        $fixtures["columns"][6]["data"]            = "actions";
+        $fixtures["columns"][6]["name"]            = "Actions";
+        $fixtures["columns"][6]["orderable"]       = "false";
+        $fixtures["columns"][6]["search"]["regex"] = "false";
+        $fixtures["columns"][6]["search"]["value"] = "";
+        $fixtures["columns"][6]["searchable"]      = "false";
 
         //
-        $output["draw"]   = "1";
-        $output["length"] = "10";
+        $fixtures["draw"]   = "1";
+        $fixtures["length"] = "10";
 
         // Order
-        $output["order"][0]["column"] = "0";
-        $output["order"][0]["dir"]    = "asc";
+        $fixtures["order"][0]["column"] = "0";
+        $fixtures["order"][0]["dir"]    = "asc";
 
         // Search
-        $output["search"]["regex"] = "false";
-        $output["search"]["value"] = "";
+        $fixtures["search"]["regex"] = "false";
+        $fixtures["search"]["value"] = "";
 
         // Start
-        $output["start"] = "0";
+        $fixtures["start"] = "0";
 
         // Return
-        return $output;
+        return $fixtures;
     }
 
     /**
