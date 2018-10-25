b07d0167ac6b8c0dddb64561813fdac16ef68f29
webeweb <support@webeweb.fr>
2018-10-25 15:22:07 +0200

    Add DataTables provider interface unit tests

---
+++ Tests/Provider/DataTablesProviderInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Provider;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Provider\DataTablesProviderInterface;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractFrameworkTestCase;
+
+/**
+ * DataTables provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Manager
+ * @final
+ */
+final class DataTablesProviderInterfaceTest extends AbstractFrameworkTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("webeweb.jquerydatatables.provider", DataTablesProviderInterface::TAG_NAME);
+    }
+
+}
