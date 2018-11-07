2e26e14fe2741fe9cc1e562e4c3cc020a223e508
webeweb <support@webeweb.fr>
2018-11-07 17:35:29 +0100

    Remove keyword "final"

--- Tests/Provider/DataTablesProviderInterfaceTest.php
+++ Tests/Provider/DataTablesProviderInterfaceTest.php
@@ -19,9 +19,8 @@ use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractFrameworkTestCase;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\JQuery\DataTablesBundle\Manager
- * @final
  */
-final class DataTablesProviderInterfaceTest extends AbstractFrameworkTestCase {
+class DataTablesProviderInterfaceTest extends AbstractFrameworkTestCase {
 
     /**
      * Tests the __construct() method.
