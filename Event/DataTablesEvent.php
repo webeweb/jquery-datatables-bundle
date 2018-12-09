98e7e23b560bf9171ba9f8cb5c8c232f876448cf
webeweb <support@webeweb.fr>
2018-12-09 15:44:22 +0100

    Add DataTables event

---
+++ Event/DataTablesEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Event;
+
+use WBW\Bundle\CoreBundle\Event\AbstractEvent;
+
+/**
+ * DataTables event.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Event
+ */
+class DataTablesEvent extends AbstractEvent {
+
+    /**
+     * Constructor.
+     *
+     * @param string $eventName The name.
+     */
+    public function __construct($eventName) {
+        parent::__construct($eventName);
+    }
+
+}
---
+++ Tests/Event/DataTablesEventTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Event;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Event\DataTablesEvent;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+
+/**
+ * DataTables event test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Event
+ */
+class DataTablesEventTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DataTablesEvent("eventName");
+
+        $this->assertEquals("eventName", $obj->getEventName());
+    }
+
+}
