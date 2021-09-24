d9eb6a22d1b71ff4e9776862a6fb6e8ac3036d98
webeweb <support@webeweb.fr>
2021-09-24 11:05:26 +0200

    Add abstract command

---
+++ Command/AbstractCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Command;
+
+use WBW\Bundle\CoreBundle\Command\AbstractCommand as BaseCommand;
+use WBW\Bundle\CoreBundle\Service\TranslatorTrait;
+use WBW\Bundle\JQuery\DataTablesBundle\Translation\TranslatorInterface;
+
+/**
+ * Abstract command.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Command
+ */
+abstract class AbstractCommand extends BaseCommand {
+
+    use TranslatorTrait {
+        setTranslator as public;
+    }
+
+    /**
+     * Translate.
+     *
+     * @param string $id The id.
+     * @param array $parameters The parameters.
+     * @param string|null $domain The domain.
+     * @param string|null $locale The locale.
+     * @return string Returns the translated id in case of success, id otherwise.
+     */
+    protected function translate(string $id, array $parameters = [], string $domain = null, string $locale = null): string {
+
+        if (null === $this->getTranslator()) {
+            return $id;
+        }
+
+        if (null === $domain) {
+            $domain = TranslatorInterface::DOMAIN;
+        }
+
+        return $this->getTranslator()->trans($id, $parameters, $domain, $locale);
+    }
+}
