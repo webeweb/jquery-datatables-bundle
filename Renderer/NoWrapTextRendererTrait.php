38c984e9f8b3134a048aa455130cbc846702ee5b
webeweb <support@webeweb.fr>
2022-02-04 17:41:28 +0100

    Add no wrap text renderer trait

---
+++ Renderer/NoWrapTextRendererTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Renderer;
+
+use WBW\Library\Types\Helper\StringHelper;
+
+/**
+ * No wrap text renderer trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Renderer
+ */
+trait NoWrapTextRendererTrait {
+
+    /**
+     * Render a no wrap text.
+     *
+     * @param string|null $str The string.
+     * @return string|null Returns the no wrap text.
+     */
+    protected function renderNoWrapText(?string $str): ?string {
+        if (null === $str || "" === $str) {
+            return null;
+        }
+        return StringHelper::domNode("span", $str, ["class" => "text-nowrap"]);
+    }
+}
---
+++ Tests/Renderer/NoWrapTextRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\TestNoWrapTextRendererTrait;
+
+/**
+ * No wrap text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class NoWrapTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the renderNoWrapText() method.
+     *
+     * @return void
+     */
+    public function testRenderNoWrapText(): void {
+
+        $obj = new TestNoWrapTextRendererTrait();
+
+        $this->assertNull( $obj->renderNoWrapText(null));
+        $this->assertNull( $obj->renderNoWrapText(""));
+        $this->assertEquals('<span class="text-nowrap">content</span>', $obj->renderNoWrapText("content"));
+    }
+}
