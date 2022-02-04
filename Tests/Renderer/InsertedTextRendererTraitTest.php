a7bec58c8e0a7b9426e896973a322f089b269ece
webeweb <support@webeweb.fr>
2022-02-04 18:14:07 +0100

    Add inserted text renderer trait

---
+++ Tests/Renderer/InsertedTextRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\TestInsertedTextRendererTrait;
+
+/**
+ * Inserted text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class InsertedTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the renderInsertedText() method.
+     *
+     * @return void
+     */
+    public function testRenderInsertedText(): void {
+
+        $obj = new TestInsertedTextRendererTrait();
+
+        $this->assertNull($obj->renderInsertedText(null));
+        $this->assertNull($obj->renderInsertedText(""));
+        $this->assertEquals('<ins>content</ins>', $obj->renderInsertedText("content"));
+    }
+}
