bf0232df6577b7820c59bc72a95525cc2f34fc7e
webeweb <support@webeweb.fr>
2022-02-04 15:05:19 +0100

    Add right-aligned renderer trait

---
+++ Tests/Renderer/RightAlignedRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\TestRightAlignedRendererTrait;
+
+/**
+ * Right-aligned renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class RightAlignedRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the renderRightAligned() method.
+     *
+     * @return void
+     */
+    public function testRenderRight(): void {
+
+        $obj = new TestRightAlignedRendererTrait();
+
+        $this->assertNull( $obj->renderRightAligned(null));
+        $this->assertNull( $obj->renderRightAligned(""));
+        $this->assertEquals('<span class="pull-right">content</span>', $obj->renderRightAligned("content"));
+    }
+}
