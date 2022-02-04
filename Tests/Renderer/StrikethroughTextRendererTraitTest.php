d8c6b2d2574abccad6ecb0c32ddc2b54a0badda8
webeweb <support@webeweb.fr>
2022-02-04 18:15:06 +0100

    Add strikethrough text renderer trait

---
+++ Tests/Renderer/StrikethroughTextRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\TestStrikethroughTextRendererTrait;
+
+/**
+ * Strikethrough text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class StrikethroughTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the renderStrikethroughText() method.
+     *
+     * @return void
+     */
+    public function testRenderStrikethroughText(): void {
+
+        $obj = new TestStrikethroughTextRendererTrait();
+
+        $this->assertNull($obj->renderStrikethroughText(null));
+        $this->assertNull($obj->renderStrikethroughText(""));
+        $this->assertEquals('<s>content</s>', $obj->renderStrikethroughText("content"));
+    }
+}
