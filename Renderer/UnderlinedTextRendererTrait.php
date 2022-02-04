7e14c44abd988926c88307d900423d68abedd431
webeweb <support@webeweb.fr>
2022-02-04 18:15:21 +0100

    Add underlined text renderer trait

---
+++ Renderer/UnderlinedTextRendererTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Renderer;
+
+use WBW\Library\Types\Helper\StringHelper;
+
+/**
+ * Underlined text renderer trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Renderer
+ */
+trait UnderlinedTextRendererTrait {
+
+    /**
+     * Render a underlined text.
+     *
+     * @param string|null $str The string.
+     * @return string|null Returns the underlined text.
+     */
+    protected function renderUnderlinedText(?string $str): ?string {
+        if (null === $str || "" === $str) {
+            return null;
+        }
+        return StringHelper::domNode("u", $str);
+    }
+}
---
+++ Tests/Renderer/UnderlinedTextRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\TestUnderlinedTextRendererTrait;
+
+/**
+ * Underlined text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class UnderlinedTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the renderUnderlinedText() method.
+     *
+     * @return void
+     */
+    public function testRenderUnderlinedText(): void {
+
+        $obj = new TestUnderlinedTextRendererTrait();
+
+        $this->assertNull($obj->renderUnderlinedText(null));
+        $this->assertNull($obj->renderUnderlinedText(""));
+        $this->assertEquals('<u>content</u>', $obj->renderUnderlinedText("content"));
+    }
+}
