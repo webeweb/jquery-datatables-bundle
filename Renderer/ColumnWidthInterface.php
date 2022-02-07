34ff11e3d224780e1c6425355830fa9faed99753
webeweb <support@webeweb.fr>
2022-02-07 08:47:35 +0100

    Add Column width interface

---
+++ Renderer/ColumnWidthInterface.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Renderer;
+
+/**
+ * Column width interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Renderer
+ */
+interface ColumnWidthInterface {
+
+    /**
+     * Column width "actions".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_ACTIONS = self::COLUMN_WIDTH_L;
+
+    /**
+     * Column width "date".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_DATE = self::COLUMN_WIDTH_M;
+
+    /**
+     * Column width "icon".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_ICON = self::COLUMN_WIDTH_XXS;
+
+    /**
+     * Column width "L".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_L = "160px";
+
+    /**
+     * Column width "label".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_LABEL = self::COLUMN_WIDTH_L;
+
+    /**
+     * Column width "M".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_M = "120px";
+
+    /**
+     * Column width "S".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_S = "80px";
+
+    /**
+     * Column width "thumbnail".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_THUMBNAIL = self::COLUMN_WIDTH_S;
+
+    /**
+     * Column width "unit".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_UNIT = self::COLUMN_WIDTH_XS;
+
+    /**
+     * Column width "XL".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_XL = "200px";
+
+    /**
+     * Column width "XS".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_XS = "40px";
+
+    /**
+     * Column width "XXL".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_XXL = "240px";
+
+    /**
+     * Column width "XXS".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_XXS = "20px";
+
+    /**
+     * Column width "XXXL".
+     *
+     * @var string
+     */
+    const COLUMN_WIDTH_XXXL = "280px";
+}
---
+++ Tests/Renderer/ColumnWidthInterfaceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer;
+
+use WBW\Bundle\JQuery\DataTablesBundle\Renderer\ColumnWidthInterface;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+
+/**
+ * Column width interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer
+ */
+class ColumnWidthInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("20px", ColumnWidthInterface::COLUMN_WIDTH_XXS);
+        $this->assertEquals("40px", ColumnWidthInterface::COLUMN_WIDTH_XS);
+        $this->assertEquals("80px", ColumnWidthInterface::COLUMN_WIDTH_S);
+        $this->assertEquals("120px", ColumnWidthInterface::COLUMN_WIDTH_M);
+        $this->assertEquals("160px", ColumnWidthInterface::COLUMN_WIDTH_L);
+        $this->assertEquals("200px", ColumnWidthInterface::COLUMN_WIDTH_XL);
+        $this->assertEquals("240px", ColumnWidthInterface::COLUMN_WIDTH_XXL);
+        $this->assertEquals("280px", ColumnWidthInterface::COLUMN_WIDTH_XXXL);
+
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_L, ColumnWidthInterface::COLUMN_WIDTH_ACTIONS);
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_M, ColumnWidthInterface::COLUMN_WIDTH_DATE);
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_XXS, ColumnWidthInterface::COLUMN_WIDTH_ICON);
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_L, ColumnWidthInterface::COLUMN_WIDTH_LABEL);
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_S, ColumnWidthInterface::COLUMN_WIDTH_THUMBNAIL);
+        $this->assertEquals(ColumnWidthInterface::COLUMN_WIDTH_XS, ColumnWidthInterface::COLUMN_WIDTH_UNIT);
+    }
+}
