d8e1dc8e6f8a488fbed8e8f58be360a25407ce1d
webeweb <support@webeweb.fr>
2022-03-24 15:18:38 +0100

    Add Enabled badge renderer trait

---
+++ Renderer/Assets/EnabledBadgeRendererTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Renderer\Assets;
+
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BadgeTwigExtension;
+use WBW\Bundle\CoreBundle\Translation\BaseTranslatorInterface;
+use WBW\Bundle\JQuery\DataTablesBundle\Translation\TranslatorInterface;
+
+/**
+ * Enabled badge renderer trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Renderer\Assets
+ */
+trait EnabledBadgeRendererTrait {
+
+    /**
+     * Get the badge Twig extension.
+     *
+     * @return BadgeTwigExtension|null Returns the badge Twig extension.
+     */
+    abstract public function getBadgeTwigExtension(): ?BadgeTwigExtension;
+
+    /**
+     * Get the translator.
+     *
+     * @return BaseTranslatorInterface|null Returns the translator.
+     */
+    abstract public function getTranslator();
+
+    /**
+     * Render an enabled badge.
+     *
+     * @param bool|null $enabled Enabled ?
+     * @return string|null Returns the rendered enabled badge.
+     */
+    protected function renderEnabledBadge(?bool $enabled): ?string {
+
+        if (null === $this->getTranslator() || null === $this->getBadgeTwigExtension()) {
+            return null;
+        }
+
+        $method  = sprintf("bootstrapBadge%sFunction", true === $enabled ? "Success" : "Danger");
+        $content = $this->getTranslator()->trans(true === $enabled ? "label.enabled" : "label.disabled", [], TranslatorInterface::DOMAIN);
+
+        return $this->getBadgeTwigExtension()->$method(["content" => $content]);
+    }
+}
---
+++ Tests/Renderer/Assets/EnabledBadgeRendererTraitTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer\Assets;
+
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BadgeTwigExtension;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\JQuery\DataTablesBundle\Tests\Fixtures\Renderer\Assets\TestEnabledBadgeRendererTrait;
+
+/**
+ * Enabled badge renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\JQuery\DataTablesBundle\Tests\Renderer\Assets
+ */
+class EnabledBadgeRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Badge Twig extension.
+     *
+     * @var BadgeTwigExtension
+     */
+    private $badgeTwigExtension;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Badge Twig extension mock.
+        $this->badgeTwigExtension = new BadgeTwigExtension($this->twigEnvironment);
+    }
+
+    /**
+     * Tests renderEnabledBadge()
+     *
+     * @return void
+     */
+    public function testRenderEnabledBadge(): void {
+
+        $obj = new TestEnabledBadgeRendererTrait();
+
+        $this->assertNull($obj->renderEnabledBadge(true));
+
+        $obj->setBadgeTwigExtension($this->badgeTwigExtension);
+        $this->assertNull($obj->renderEnabledBadge(true));
+
+        $obj->setTranslator($this->translator);
+        $this->assertNotNull($obj->renderEnabledBadge(true));
+    }
+
+    /**
+     * Tests renderEnabledBadge()
+     *
+     * @return void
+     */
+    public function testRenderEnabledBadgeWithFalse(): void {
+
+        $obj = new TestEnabledBadgeRendererTrait();
+        $obj->setBadgeTwigExtension($this->badgeTwigExtension);
+        $obj->setTranslator($this->translator);
+
+        $res = file_get_contents(__DIR__ . "/EnabledBadgeRendererTraitTest.testRenderEnabledBadgeWithFalse.html.txt");
+        $this->assertEquals($res, $obj->renderEnabledBadge(false));
+    }
+
+    /**
+     * Tests renderEnabledBadge()
+     *
+     * @return void
+     */
+    public function testRenderEnabledBadgeWithTrue(): void {
+
+        $obj = new TestEnabledBadgeRendererTrait();
+        $obj->setBadgeTwigExtension($this->badgeTwigExtension);
+        $obj->setTranslator($this->translator);
+
+        $res = file_get_contents(__DIR__ . "/EnabledBadgeRendererTraitTest.testRenderEnabledBadgeWithTrue.html.txt");
+        $this->assertEquals($res, $obj->renderEnabledBadge(true));
+    }
+}
