44eab8ddc0b238ec5f3c88f46c78bb8c465eba5b
webeweb <support@webeweb.fr>
2024-03-27 18:21:15 +0100

    Update Column with interface: - improve constants visibility

--- Renderer/ColumnWidthInterface.php
+++ Renderer/ColumnWidthInterface.php
@@ -24,96 +24,96 @@ interface ColumnWidthInterface {
      *
      * @var string
      */
-    const COLUMN_WIDTH_ACTIONS = self::COLUMN_WIDTH_L;
+    public const COLUMN_WIDTH_ACTIONS = self::COLUMN_WIDTH_L;
 
     /**
      * Column width "date".
      *
      * @var string
      */
-    const COLUMN_WIDTH_DATE = self::COLUMN_WIDTH_M;
+    public const COLUMN_WIDTH_DATE = self::COLUMN_WIDTH_M;
 
     /**
      * Column width "icon".
      *
      * @var string
      */
-    const COLUMN_WIDTH_ICON = self::COLUMN_WIDTH_XXS;
+    public const COLUMN_WIDTH_ICON = self::COLUMN_WIDTH_XXS;
 
     /**
      * Column width "L".
      *
      * @var string
      */
-    const COLUMN_WIDTH_L = "160px";
+    public const COLUMN_WIDTH_L = "160px";
 
     /**
      * Column width "label".
      *
      * @var string
      */
-    const COLUMN_WIDTH_LABEL = self::COLUMN_WIDTH_L;
+    public const COLUMN_WIDTH_LABEL = self::COLUMN_WIDTH_L;
 
     /**
      * Column width "M".
      *
      * @var string
      */
-    const COLUMN_WIDTH_M = "120px";
+    public const COLUMN_WIDTH_M = "120px";
 
     /**
      * Column width "S".
      *
      * @var string
      */
-    const COLUMN_WIDTH_S = "80px";
+    public const COLUMN_WIDTH_S = "80px";
 
     /**
      * Column width "thumbnail".
      *
      * @var string
      */
-    const COLUMN_WIDTH_THUMBNAIL = self::COLUMN_WIDTH_S;
+    public const COLUMN_WIDTH_THUMBNAIL = self::COLUMN_WIDTH_S;
 
     /**
      * Column width "unit".
      *
      * @var string
      */
-    const COLUMN_WIDTH_UNIT = self::COLUMN_WIDTH_XS;
+    public const COLUMN_WIDTH_UNIT = self::COLUMN_WIDTH_XS;
 
     /**
      * Column width "XL".
      *
      * @var string
      */
-    const COLUMN_WIDTH_XL = "200px";
+    public const COLUMN_WIDTH_XL = "200px";
 
     /**
      * Column width "XS".
      *
      * @var string
      */
-    const COLUMN_WIDTH_XS = "40px";
+    public const COLUMN_WIDTH_XS = "40px";
 
     /**
      * Column width "XXL".
      *
      * @var string
      */
-    const COLUMN_WIDTH_XXL = "240px";
+    public const COLUMN_WIDTH_XXL = "240px";
 
     /**
      * Column width "XXS".
      *
      * @var string
      */
-    const COLUMN_WIDTH_XXS = "20px";
+    public const COLUMN_WIDTH_XXS = "20px";
 
     /**
      * Column width "XXXL".
      *
      * @var string
      */
-    const COLUMN_WIDTH_XXXL = "280px";
+    public const COLUMN_WIDTH_XXXL = "280px";
 }
