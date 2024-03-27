69bcfe1efad691437514d4268bc3fd2af0541a2c
webeweb <support@webeweb.fr>
2024-03-27 18:21:35 +0100

    Update DataTables Twig extension: - improve constants visibility

--- Twig/Extension/DataTablesTwigExtension.php
+++ Twig/Extension/DataTablesTwigExtension.php
@@ -31,7 +31,7 @@ class DataTablesTwigExtension extends AbstractDataTablesTwigExtension {
      *
      * @var string
      */
-    const SERVICE_NAME = "wbw.jquery.datatables.twig.extension";
+    public const SERVICE_NAME = "wbw.jquery.datatables.twig.extension";
 
     /**
      * Get the Twig filters.
