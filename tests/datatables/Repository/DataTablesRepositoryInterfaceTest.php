73960472176d9c2bf11d750561d27df9b40213b5
webeweb <support@webeweb.fr>
2024-03-29 11:27:19 +0100

    Move DataTables repository interface

---
+++ tests/datatables/Repository/DataTablesRepositoryInterfaceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Tests\Repository;
+
+use WBW\Bundle\DataTablesBundle\Repository\DataTablesRepositoryInterface;
+use WBW\Bundle\DataTablesBundle\Tests\AbstractTestCase;
+
+/**
+ * DataTables repository interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Tests\Repository
+ */
+class DataTablesRepositoryInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals(10000, DataTablesRepositoryInterface::REPOSITORY_LIMIT);
+    }
+}
