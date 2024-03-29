51bd6344b0cd6839aa0a86f259eab4fb4475990e
webeweb <support@webeweb.fr>
2024-03-29 10:09:06 +0100

    Clean up abstract test case

--- tests/common/AbstractTestCase.php
+++ tests/common/AbstractTestCase.php
@@ -12,37 +12,14 @@
 namespace WBW\Bundle\CommonBundle\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 /**
  * Abstract test case.
  *
  * @author webeweb <https://github.com/webeweb>
  * @package WBW\Bundle\CommonBundle\Tests
+ * @abstract
  */
 abstract class AbstractTestCase extends BaseTestCase {
 
-    /**
-     * Container builder.
-     *
-     * @var ContainerBuilder|null
-     */
-    protected $containerBuilder;
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function setUp(): void {
-        parent::setUp();
-
-        // Set a Parameter bag mock.
-        $parameterBag = new ParameterBag([
-            "kernel.environment" => "test",
-            "kernel.project_dir" => realpath(__DIR__ . "/../fixtures/app"),
-        ]);
-
-        // Set a Container builder with only the necessary.
-        $this->containerBuilder = new ContainerBuilder($parameterBag);
-    }
 }
