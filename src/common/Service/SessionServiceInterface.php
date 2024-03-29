d0601f8bc1f09063920696f64d6671290686c7b5
webeweb <support@webeweb.fr>
2024-03-29 10:47:46 +0100

    Copy Session service interface (from core-bundle)

---
+++ src/common/Service/SessionServiceInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Service;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Throwable;
+
+/**
+ * Session service interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Service
+ */
+interface SessionServiceInterface {
+
+    /**
+     * Get the session.
+     *
+     * @return SessionInterface|null Returns the session.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function getSession(): ?SessionInterface;
+}
