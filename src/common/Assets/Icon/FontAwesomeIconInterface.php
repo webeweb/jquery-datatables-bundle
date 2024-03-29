f46e5f21b636a191a801771c6666bfce3ca77428
webeweb <support@webeweb.fr>
2024-03-29 17:11:25 +0100

    Copy Font Awesome icon interface (from core-bundle)

---
+++ src/common/Assets/Icon/FontAwesomeIconInterface.php
@@ -0,0 +1,267 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Assets\Icon;
+
+use WBW\Library\Symfony\Assets\IconInterface;
+
+/**
+ * Font Awesome icon interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Assets\Icon
+ */
+interface FontAwesomeIconInterface extends IconInterface {
+
+    /**
+     * Font Awesome animation "pulse".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_ANIMATION_PULSE = "pulse";
+
+    /**
+     * Font Awesome animation "spin".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_ANIMATION_SPIN = "spin";
+
+    /**
+     * Font Awesome font "default".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT = "";
+
+    /**
+     * Font Awesome font "bold".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT_BOLD = "b";
+
+    /**
+     * Font Awesome font "duotone".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT_DUOTONE = "d";
+
+    /**
+     * Font Awesome font "light".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT_LIGHT = "l";
+
+    /**
+     * Font Awesome font "regular".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT_REGULAR = "r";
+
+    /**
+     * Font Awesome font "solid".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_FONT_SOLID = "s";
+
+    /**
+     * Font Awesome pull "left".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_PULL_LEFT = "left";
+
+    /**
+     * Font Awesome pull "right".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_PULL_RIGHT = "right";
+
+    /**
+     * Font Awesome size "10x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_10X = "10x";
+
+    /**
+     * Font Awesome size "2x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_2X = "2x";
+
+    /**
+     * Font Awesome size "3x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_3X = "3x";
+
+    /**
+     * Font Awesome size "4x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_4X = "4x";
+
+    /**
+     * Font Awesome size "5x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_5X = "5x";
+
+    /**
+     * Font Awesome size "6x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_6X = "6x";
+
+    /**
+     * Font Awesome size "7x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_7X = "7x";
+
+    /**
+     * Font Awesome size "8x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_8X = "8x";
+
+    /**
+     * Font Awesome size "9x".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_9X = "9x";
+
+    /**
+     * Font Awesome size "lg".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_LG = "lg";
+
+    /**
+     * Font Awesome size "sm".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_SM = "sm";
+
+    /**
+     * Font Awesome size "xs".
+     *
+     * @var string
+     */
+    public const FONT_AWESOME_SIZE_XS = "xs";
+
+    /**
+     * Get the animation.
+     *
+     * @return string|null Returns the animation.
+     */
+    public function getAnimation(): ?string;
+
+    /**
+     * Get the bordered.
+     *
+     * @return bool|null Returns the bordered.
+     */
+    public function getBordered(): ?bool;
+
+    /**
+     * Get the fixed width.
+     *
+     * @return bool|null Returns the fixed width.
+     */
+    public function getFixedWidth(): ?bool;
+
+    /**
+     * Get the font.
+     *
+     * @return string|null Returns the font.
+     */
+    public function getFont(): ?string;
+
+    /**
+     * Get the pull.
+     *
+     * @return string|null Returns the pull.
+     */
+    public function getPull(): ?string;
+
+    /**
+     * Get the size.
+     *
+     * @return string|null Returns the size.
+     */
+    public function getSize(): ?string;
+
+    /**
+     * Set the animation.
+     *
+     * @param string|null $animation The animation.
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setAnimation(?string $animation): FontAwesomeIconInterface;
+
+    /**
+     * Set the bordered.
+     *
+     * @param bool|null $bordered Bordered ?
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setBordered(?bool $bordered): FontAwesomeIconInterface;
+
+    /**
+     * Set the fixed width.
+     *
+     * @param bool|null $fixedWidth Fixed width ?
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setFixedWidth(?bool $fixedWidth): FontAwesomeIconInterface;
+
+    /**
+     * Set the font.
+     *
+     * @param string|null $font The font.
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setFont(?string $font): FontAwesomeIconInterface;
+
+    /**
+     * Set the pull.
+     *
+     * @param string|null $pull The pull.
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setPull(?string $pull): FontAwesomeIconInterface;
+
+    /**
+     * Set the size.
+     *
+     * @param string|null $size The size.
+     * @return FontAwesomeIconInterface Returns this icon.
+     */
+    public function setSize(?string $size): FontAwesomeIconInterface;
+}
---
+++ tests/common/Assets/Icon/FontAwesomeIconInterfaceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Assets\Icon;
+
+use WBW\Bundle\CommonBundle\Assets\Icon\FontAwesomeIconInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Font Awesome icon interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Assets\Icon
+ */
+class FontAwesomeIconInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("pulse", FontAwesomeIconInterface::FONT_AWESOME_ANIMATION_PULSE);
+        $this->assertEquals("spin", FontAwesomeIconInterface::FONT_AWESOME_ANIMATION_SPIN);
+
+        $this->assertEquals("", FontAwesomeIconInterface::FONT_AWESOME_FONT);
+        $this->assertEquals("b", FontAwesomeIconInterface::FONT_AWESOME_FONT_BOLD);
+        $this->assertEquals("d", FontAwesomeIconInterface::FONT_AWESOME_FONT_DUOTONE);
+        $this->assertEquals("l", FontAwesomeIconInterface::FONT_AWESOME_FONT_LIGHT);
+        $this->assertEquals("r", FontAwesomeIconInterface::FONT_AWESOME_FONT_REGULAR);
+        $this->assertEquals("s", FontAwesomeIconInterface::FONT_AWESOME_FONT_SOLID);
+
+        $this->assertEquals("left", FontAwesomeIconInterface::FONT_AWESOME_PULL_LEFT);
+        $this->assertEquals("right", FontAwesomeIconInterface::FONT_AWESOME_PULL_RIGHT);
+
+        $this->assertEquals("2x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_2X);
+        $this->assertEquals("3x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_3X);
+        $this->assertEquals("4x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_4X);
+        $this->assertEquals("5x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_5X);
+        $this->assertEquals("6x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_6X);
+        $this->assertEquals("7x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_7X);
+        $this->assertEquals("8x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_8X);
+        $this->assertEquals("9x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_9X);
+        $this->assertEquals("10x", FontAwesomeIconInterface::FONT_AWESOME_SIZE_10X);
+        $this->assertEquals("lg", FontAwesomeIconInterface::FONT_AWESOME_SIZE_LG);
+        $this->assertEquals("sm", FontAwesomeIconInterface::FONT_AWESOME_SIZE_SM);
+        $this->assertEquals("xs", FontAwesomeIconInterface::FONT_AWESOME_SIZE_XS);
+    }
+}
