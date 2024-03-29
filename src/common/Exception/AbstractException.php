1ba85a2e4942254dedf9b9ce10c9c96e26876b9b
webeweb <support@webeweb.fr>
2024-03-29 11:17:33 +0100

    Copy abstract exception (from core-bundle)

---
+++ src/common/Exception/AbstractException.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Exception;
+
+use Exception as BaseException;
+
+/**
+ * Abstract exception.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CoreBundle\Exception
+ * @abstract
+ */
+abstract class AbstractException extends BaseException {
+
+}
