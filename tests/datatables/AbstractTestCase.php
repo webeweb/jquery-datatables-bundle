612df6e4862baa1252416bcad988c79210babf9f
webeweb <support@webeweb.fr>
2024-03-29 09:09:36 +0100

    Add abstract test case

---
+++ tests/datatables/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends BaseTestCase {
+
+}
