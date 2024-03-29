da5115f7d485304213736194bac519880d1e2b7f
webeweb <support@webeweb.fr>
2024-03-29 17:14:38 +0100

    Move Material Design iconic font

---
+++ src/common/Assets/Icon/MaterialDesignIconicFontIcon.php
@@ -0,0 +1,298 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Assets\Icon;
+
+use WBW\Library\Symfony\Assets\AbstractIcon;
+
+/**
+ * Material Design Iconic Font icon.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Assets\Icon
+ */
+class MaterialDesignIconicFontIcon extends AbstractIcon implements MaterialDesignIconicFontIconInterface {
+
+    /**
+     * Border.
+     *
+     * @var string|null
+     */
+    private $border;
+
+    /**
+     * Fixed width.
+     *
+     * @var bool|null
+     */
+    private $fixedWidth;
+
+    /**
+     * Flip.
+     *
+     * @var string|null
+     */
+    private $flip;
+
+    /**
+     * Pull.
+     *
+     * @var string|null
+     */
+    private $pull;
+
+    /**
+     * Rotate.
+     *
+     * @var string|null
+     */
+    private $rotate;
+
+    /**
+     * Size.
+     *
+     * @var string|null
+     */
+    private $size;
+
+    /**
+     * Spin.
+     *
+     * @var string|null
+     */
+    private $spin;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+
+        $this->setFixedWidth(false);
+    }
+
+    /**
+     * Enumerate the borders.
+     *
+     * @return string[] Returns the borders enumeration.
+     */
+    public static function enumBorders(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_BORDER,
+            self::MATERIAL_DESIGN_ICONIC_FONT_BORDER_CIRCLE,
+        ];
+    }
+
+    /**
+     * Enumerate the flips.
+     *
+     * @return string[] Returns the flips enumeration.
+     */
+    public static function enumFlips(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_FLIP_HORIZONTAL,
+            self::MATERIAL_DESIGN_ICONIC_FONT_FLIP_VERTICAL,
+        ];
+    }
+
+    /**
+     * Enumerate the pulls.
+     *
+     * @return string[] Returns the pulls enumeration.
+     */
+    public static function enumPulls(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_PULL_LEFT,
+            self::MATERIAL_DESIGN_ICONIC_FONT_PULL_RIGHT,
+        ];
+    }
+
+    /**
+     * Enumerate the rotates.
+     *
+     * @return string[] Returns the rotates enumeration.
+     */
+    public static function enumRotates(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_90,
+            self::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_180,
+            self::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_270,
+        ];
+    }
+
+    /**
+     * Enumerate the sizes.
+     *
+     * @return string[] Returns the sizes enumeration.
+     */
+    public static function enumSizes(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_SIZE_LG,
+            self::MATERIAL_DESIGN_ICONIC_FONT_SIZE_2X,
+            self::MATERIAL_DESIGN_ICONIC_FONT_SIZE_3X,
+            self::MATERIAL_DESIGN_ICONIC_FONT_SIZE_4X,
+            self::MATERIAL_DESIGN_ICONIC_FONT_SIZE_5X,
+        ];
+    }
+
+    /**
+     * Enumerate the spins.
+     *
+     * @return string[] Returns the spins enumeration.
+     */
+    public static function enumSpins(): array {
+
+        return [
+            self::MATERIAL_DESIGN_ICONIC_FONT_SPIN,
+            self::MATERIAL_DESIGN_ICONIC_FONT_SPIN_REVERSE,
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getBorder(): ?string {
+        return $this->border;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFixedWidth(): ?bool {
+        return $this->fixedWidth;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFlip(): ?string {
+        return $this->flip;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPull(): ?string {
+        return $this->pull;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRotate(): ?string {
+        return $this->rotate;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSize(): ?string {
+        return $this->size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSpin(): ?string {
+        return $this->spin;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setBorder(?string $border): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($border, static::enumBorders())) {
+            $border = null;
+        }
+
+        $this->border = $border;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setFixedWidth(?bool $fixedWidth): MaterialDesignIconicFontIconInterface {
+        $this->fixedWidth = $fixedWidth;
+        return $this;
+    }
+
+    /**
+     *{@inheritDoc}
+     */
+    public function setFlip(?string $flip): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($flip, static::enumFlips())) {
+            $flip = null;
+        }
+
+        $this->flip = $flip;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setPull(?string $pull): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($pull, static::enumPulls())) {
+            $pull = null;
+        }
+
+        $this->pull = $pull;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setRotate(?string $rotate): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($rotate, static::enumRotates())) {
+            $rotate = null;
+        }
+
+        $this->rotate = $rotate;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setSize(?string $size): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($size, static::enumSizes())) {
+            $size = null;
+        }
+
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setSpin(?string $spin): MaterialDesignIconicFontIconInterface {
+
+        if (false === in_array($spin, static::enumSpins())) {
+            $spin = null;
+        }
+
+        $this->spin = $spin;
+        return $this;
+    }
+}
---
+++ tests/common/Assets/Icon/MaterialDesignIconicFontIconTest.php
@@ -0,0 +1,306 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Assets\Icon;
+
+use WBW\Bundle\CommonBundle\Assets\Icon\MaterialDesignIconicFontIcon;
+use WBW\Bundle\CommonBundle\Assets\Icon\MaterialDesignIconicFontIconInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Material Design Iconic Font icon test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Assets\Icon
+ */
+class MaterialDesignIconicFontIconTest extends AbstractTestCase {
+
+    /**
+     * Test enumBorders()
+     *
+     * @return void.
+     */
+    public function testEnumBorders(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_BORDER,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_BORDER_CIRCLE,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumBorders());
+    }
+
+    /**
+     * Test enumFlips()
+     *
+     * @return void
+     */
+    public function testEnumFlips(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_FLIP_HORIZONTAL,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_FLIP_VERTICAL,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumFlips());
+    }
+
+    /**
+     * Test enumPulls()
+     *
+     * @return void
+     */
+    public function testEnumPulls(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_PULL_LEFT,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_PULL_RIGHT,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumPulls());
+    }
+
+    /**
+     * Test enumRotates()
+     *
+     * @return void
+     */
+    public function testEnumRotates(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_90,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_180,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_270,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumRotates());
+    }
+
+    /**
+     * Test enumSizes()
+     *
+     * @return void
+     */
+    public function testEnumSizes(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_LG,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_2X,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_3X,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_4X,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_5X,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumSizes());
+    }
+
+    /**
+     * Test enumSpins()
+     *
+     * @return void
+     */
+    public function testEnumSpins(): void {
+
+        $exp = [
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SPIN,
+            MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SPIN_REVERSE,
+        ];
+
+        $this->assertEquals($exp, MaterialDesignIconicFontIcon::enumSpins());
+    }
+
+    /**
+     * Test setBorder()
+     *
+     * @return void
+     */
+    public function testSetBorder(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setBorder(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_BORDER);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_BORDER, $obj->getBorder());
+    }
+
+    /**
+     * Test setBorder()
+     *
+     * @return void
+     */
+    public function testSetBorderWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setBorder("exception");
+        $this->assertNotEquals("exception", $obj->getBorder());
+    }
+
+    /**
+     * Test setFixedWidth()
+     *
+     * @return void
+     */
+    public function testSetFixedWidth(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setFixedWidth(true);
+        $this->assertTrue($obj->getFixedWidth());
+    }
+
+    /**
+     * Test setFlip()
+     *
+     * @return void
+     */
+    public function testSetFlip(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setFlip(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_FLIP_HORIZONTAL);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_FLIP_HORIZONTAL, $obj->getFlip());
+    }
+
+    /**
+     * Test setFlip()
+     *
+     * @return void
+     */
+    public function testSetFlipWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setFlip("exception");
+        $this->assertNotEquals("exception", $obj->getFlip());
+    }
+
+    /**
+     * Test setPull()
+     *
+     * @return void
+     */
+    public function testSetPull(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setPull(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_PULL_LEFT);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_PULL_LEFT, $obj->getPull());
+    }
+
+    /**
+     * Test setPull()
+     *
+     * @return void
+     */
+    public function testSetPullWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setPull("exception");
+        $this->assertNotEquals("exception", $obj->getPull());
+    }
+
+    /**
+     * Test setRotate()
+     *
+     * @return void
+     */
+    public function testSetRotate(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setRotate(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_90);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_ROTATE_90, $obj->getRotate());
+    }
+
+    /**
+     * Test setRotate()
+     *
+     * @return void
+     */
+    public function testSetRotateWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setRotate("exception");
+        $this->assertNotEquals("exception", $obj->getRotate());
+    }
+
+    /**
+     * Test setSize()
+     *
+     * @return void
+     */
+    public function testSetSize(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSize(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_LG);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SIZE_LG, $obj->getSize());
+    }
+
+    /**
+     * Test setSize()
+     *
+     * @return void
+     */
+    public function testSetSizeWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSize("exception");
+        $this->assertNotEquals("exception", $obj->getSize());
+    }
+
+    /**
+     * Test setSpin()
+     *
+     * @return void
+     */
+    public function testSetSpin(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSpin(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SPIN);
+        $this->assertEquals(MaterialDesignIconicFontIconInterface::MATERIAL_DESIGN_ICONIC_FONT_SPIN, $obj->getSpin());
+    }
+
+    /**
+     * Test setSpin()
+     *
+     * @return void
+     */
+    public function testSetSpinWithBadArgument(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $obj->setSpin("exception");
+        $this->assertNotEquals("exception", $obj->getSpin());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new MaterialDesignIconicFontIcon();
+
+        $this->assertNull($obj->getBorder());
+        $this->assertFalse($obj->getFixedWidth());
+        $this->assertNull($obj->getFlip());
+        $this->assertNull($obj->getPull());
+        $this->assertNull($obj->getRotate());
+        $this->assertNull($obj->getSize());
+        $this->assertNull($obj->getSpin());
+    }
+}
