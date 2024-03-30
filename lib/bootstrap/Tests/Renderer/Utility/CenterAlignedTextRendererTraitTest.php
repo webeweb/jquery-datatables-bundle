1291c1aee6a0066ae5f9f4bb461322ce2e4ce28f
webeweb <support@webeweb.fr>
2024-03-30 16:32:23 +0100

    Copy Center aligned text renderer (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Renderer/Utility/CenterAlignedTextRendererTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Renderer\Utility;
+
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Renderer\Utility\TestCenterAlignedTextRendererTrait;
+
+/**
+ * Center-aligned text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Renderer\Utility
+ */
+class CenterAlignedTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Test renderCenterAlignedText()
+     *
+     * @return void
+     */
+    public function testRenderCenterAlignedText(): void {
+
+        $obj = new TestCenterAlignedTextRendererTrait();
+
+        $this->assertNull($obj->renderCenterAlignedText(null));
+        $this->assertNull($obj->renderCenterAlignedText(""));
+        $this->assertEquals('<span class="text-center">content</span>', $obj->renderCenterAlignedText("content"));
+    }
+}
