8e7cca7b5ff231e96f3c77144b7fe80d6d1a19e9
webeweb <support@webeweb.fr>
2024-03-30 14:20:10 +0100

    Copy Badge renderer (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Renderer/BadgeRendererTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Renderer;
+
+use WBW\Bundle\BootstrapBundle\Factory\BadgeFactory;
+use WBW\Bundle\BootstrapBundle\Model\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Renderer\BadgeRenderer;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Badge renderer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Renderer
+ */
+class BadgeRendererTest extends AbstractTestCase {
+
+    /**
+     * Badge.
+     *
+     * @var BadgeInterface|null
+     */
+    private $badge;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Badge mock.
+        $this->badge = BadgeFactory::parseDangerBadge([
+            "content" => "content",
+            "pill"    => true,
+            "class"   => "class",
+        ]);
+    }
+
+    /**
+     * Test renderContent()
+     *
+     * @return void
+     */
+    public function testRenderContent(): void {
+
+        $res = BadgeRenderer::renderContent($this->badge);
+        $this->assertEquals("content", $res);
+    }
+
+    /**
+     * Test renderPill()
+     *
+     * @return void
+     */
+    public function testRenderPill(): void {
+
+        $res = BadgeRenderer::renderPill($this->badge);
+        $this->assertEquals("badge-pill", $res);
+    }
+
+    /**
+     * Test renderType()
+     *
+     * @return void
+     */
+    public function testRenderType(): void {
+
+        $res = BadgeRenderer::renderType($this->badge);
+        $this->assertEquals("badge-danger", $res);
+    }
+}
