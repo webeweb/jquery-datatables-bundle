36d9228d50d3fd4b9ebf1afe076e78da6fdd5957
webeweb <support@webeweb.fr>
2024-03-30 13:52:29 +0100

    Copy Warning alert (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/Alert/WarningAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\Alert;
+
+use WBW\Bundle\BootstrapBundle\Model\Alert\WarningAlert;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\AlertInterface as BaseAlertInterface;
+
+/**
+ * Warning alert test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\Alert
+ */
+class WarningAlertTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new WarningAlert();
+
+        $this->assertEquals(BaseAlertInterface::ALERT_TYPE_WARNING, $obj->getType());
+    }
+}
