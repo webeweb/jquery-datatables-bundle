b11fda902475de5fcaacca695cca58e301ff63cc
webeweb <support@webeweb.fr>
2024-03-30 17:03:27 +0100

    Copy Font Awesome icon renderer (from core-bundle)

---
+++ lib/bootstrap/Renderer/Extend/Icon/FontAwesomeIconRenderer.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\FontAwesomeIconInterface;
+use WBW\Library\Symfony\Renderer\Assets\IconRenderer;
+
+/**
+ * Font Awesome icon renderer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon
+ */
+class FontAwesomeIconRenderer extends IconRenderer {
+
+    /**
+     * Render an animation.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered animation in case of success, null otherwise.
+     */
+    public static function renderAnimation(FontAwesomeIconInterface $icon): ?string {
+        return null !== $icon->getAnimation() ? "fa-" . $icon->getAnimation() : null;
+    }
+
+    /**
+     * Render a bordered.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered bordered in case of success, null otherwise.
+     */
+    public static function renderBordered(FontAwesomeIconInterface $icon): ?string {
+        return true === $icon->getBordered() ? "fa-border" : null;
+    }
+
+    /**
+     * Render a fixed width.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered fixed width in case of success, null otherwise.
+     */
+    public static function renderFixedWidth(FontAwesomeIconInterface $icon): ?string {
+        return true === $icon->getFixedWidth() ? "fa-fw" : null;
+    }
+
+    /**
+     * Render a font.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string Returns the rendered font.
+     */
+    public static function renderFont(FontAwesomeIconInterface $icon): ?string {
+        return "fa" . $icon->getFont();
+    }
+
+    /**
+     * Render a name.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered name in case of success, false otherwise.
+     */
+    public static function renderName(FontAwesomeIconInterface $icon): ?string {
+        return null !== $icon->getName() ? "fa-" . $icon->getName() : null;
+    }
+
+    /**
+     * Render a pull.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered pull in case of success, null otherwise.
+     */
+    public static function renderPull(FontAwesomeIconInterface $icon): ?string {
+        return null !== $icon->getPull() ? "fa-pull-" . $icon->getPull() : null;
+    }
+
+    /**
+     * Render a size.
+     *
+     * @param FontAwesomeIconInterface $icon The icon.
+     * @return string|null Returns the rendered size in case of success, null otherwise.
+     */
+    public static function renderSize(FontAwesomeIconInterface $icon): ?string {
+        return null !== $icon->getSize() ? "fa-" . $icon->getSize() : null;
+    }
+}
---
+++ lib/bootstrap/Tests/Renderer/Extend/Icon/FontAwesomeIconRendererTest.php
@@ -0,0 +1,129 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Renderer\Extend\Icon;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\FontAwesomeIconInterface;
+use WBW\Bundle\BootstrapBundle\Factory\Extend\IconFactory;
+use WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon\FontAwesomeIconRenderer;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Font Awesome icon renderer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Renderer\Extend\Icon
+ */
+class FontAwesomeIconRendererTest extends AbstractTestCase {
+
+    /**
+     * Icon.
+     *
+     * @var FontAwesomeIconInterface|null
+     */
+    private $icon;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Font Awesome icon mock.
+        $this->icon = IconFactory::parseFontAwesomeIcon([
+            "name"       => "home",
+            "style"      => "color: #000000;",
+            "animation"  => "spin",
+            "bordered"   => true,
+            "fixedWidth" => true,
+            "font"       => "s",
+            "pull"       => "left",
+            "size"       => "lg",
+        ]);
+    }
+
+    /**
+     * Test renderAnimation()
+     *
+     * @return void
+     */
+    public function testRenderAnimation(): void {
+
+        $res = FontAwesomeIconRenderer::renderAnimation($this->icon);
+        $this->assertEquals("fa-spin", $res);
+    }
+
+    /**
+     * Test renderBordered()
+     *
+     * @return void
+     */
+    public function testRenderBordered(): void {
+
+        $res = FontAwesomeIconRenderer::renderBordered($this->icon);
+        $this->assertEquals("fa-border", $res);
+    }
+
+    /**
+     * Test renderFixedWidth()
+     *
+     * @return void
+     */
+    public function testRenderFixedWidth(): void {
+
+        $res = FontAwesomeIconRenderer::renderFixedWidth($this->icon);
+        $this->assertEquals("fa-fw", $res);
+    }
+
+    /**
+     * Test renderFont()
+     *
+     * @return void
+     */
+    public function testRenderFont(): void {
+
+        $res = FontAwesomeIconRenderer::renderFont($this->icon);
+        $this->assertEquals("fas", $res);
+    }
+
+    /**
+     * Test renderName()
+     *
+     * @return void
+     */
+    public function testRenderName(): void {
+
+        $res = FontAwesomeIconRenderer::renderName($this->icon);
+        $this->assertEquals("fa-home", $res);
+    }
+
+    /**
+     * Test renderPull()
+     *
+     * @return void
+     */
+    public function testRenderPull(): void {
+
+        $res = FontAwesomeIconRenderer::renderPull($this->icon);
+        $this->assertEquals("fa-pull-left", $res);
+    }
+
+    /**
+     * Test renderSize()
+     *
+     * @return void
+     */
+    public function testRenderSize(): void {
+
+        $res = FontAwesomeIconRenderer::renderSize($this->icon);
+        $this->assertEquals("fa-lg", $res);
+    }
+}
