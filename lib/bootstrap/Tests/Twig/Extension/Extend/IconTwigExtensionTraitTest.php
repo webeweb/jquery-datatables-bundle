06f62fbcc3b5ad7ff4a87c0a41d8eda55bf7befe
webeweb <support@webeweb.fr>
2024-03-30 15:55:14 +0100

    Copy Icon Twig extension trait (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/Extend/IconTwigExtensionTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend;
+
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Extend\TestIconTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\IconTwigExtension;
+
+/**
+ * Icon Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend
+ */
+class IconTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setIconTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetIconTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set an Icon Twig extension mock.
+        $iconTwigExtension = new IconTwigExtension($twigEnvironment);
+
+        $obj = new TestIconTwigExtensionTrait();
+
+        $obj->setIconTwigExtension($iconTwigExtension);
+        $this->assertSame($iconTwigExtension, $obj->getIconTwigExtension());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Extend/IconTwigExtensionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Extend;
+
+/**
+ * Icon Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Extend
+ */
+trait IconTwigExtensionTrait {
+
+    /**
+     * Icon Twig extension.
+     *
+     * @var IconTwigExtension|null
+     */
+    private $iconTwigExtension;
+
+    /**
+     * Get the icon Twig extension.
+     *
+     * @return IconTwigExtension Returns the icon Twig extension.
+     */
+    public function getIconTwigExtension(): ?IconTwigExtension {
+        return $this->iconTwigExtension;
+    }
+
+    /**
+     * Set the icon Twig extension.
+     *
+     * @param IconTwigExtension|null $iconTwigExtension The icon Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setIconTwigExtension(?IconTwigExtension $iconTwigExtension): self {
+        $this->iconTwigExtension = $iconTwigExtension;
+        return $this;
+    }
+}
