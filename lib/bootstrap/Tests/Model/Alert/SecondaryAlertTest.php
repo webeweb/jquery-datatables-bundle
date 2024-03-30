5b48d9c664f7d789160585c5b83b063ce736614e
webeweb <support@webeweb.fr>
2024-03-30 13:52:11 +0100

    Copy Secondary alert (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/Alert/SecondaryAlertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\Alert;
+
+use WBW\Bundle\BootstrapBundle\Model\Alert\SecondaryAlert;
+use WBW\Bundle\BootstrapBundle\Model\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Secondary alert test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\Alert
+ */
+class SecondaryAlertTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new SecondaryAlert();
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_SECONDARY, $obj->getType());
+    }
+}
