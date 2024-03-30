49eecd66d2134f6d3b48ed98bbd8a2600d31a3b8
webeweb <support@webeweb.fr>
2024-03-30 14:15:40 +0100

    Copy Progress bar factory (from bootstrap-bundle)

---
+++ lib/bootstrap/Factory/ProgressBarFactory.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\BasicProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\DangerProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\InfoProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\SuccessProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\WarningProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBarInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Progress bar factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Factory
+ */
+class ProgressBarFactory {
+
+    /**
+     * Create a new basic progress bar.
+     *
+     * @return ProgressBarInterface Returns the success progress bar.
+     */
+    public static function newBasicProgressBar(): ProgressBarInterface {
+        return new BasicProgressBar();
+    }
+
+    /**
+     * Create a new danger progress bar.
+     *
+     * @return ProgressBarInterface Returns the danger progress bar.
+     */
+    public static function newDangerProgressBar(): ProgressBarInterface {
+        return new DangerProgressBar();
+    }
+
+    /**
+     * Create a new info progress bar.
+     *
+     * @return ProgressBarInterface Returns the info progress bar.
+     */
+    public static function newInfoProgressBar(): ProgressBarInterface {
+        return new InfoProgressBar();
+    }
+
+    /**
+     * Create a new success progress bar.
+     *
+     * @return ProgressBarInterface Returns the success progress bar.
+     */
+    public static function newSuccessProgressBar(): ProgressBarInterface {
+        return new SuccessProgressBar();
+    }
+
+    /**
+     * Create a new warning progress bar.
+     *
+     * @return ProgressBarInterface Returns the warning progress bar.
+     */
+    public static function newWarningProgressBar(): ProgressBarInterface {
+        return new WarningProgressBar();
+    }
+
+    /**
+     * Parse a basic progress bar.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the basic progress bar.
+     */
+    public static function parseBasicProgressBar(array $args): ProgressBarInterface {
+        return static::parseProgressBar(static::newBasicProgressBar(), $args);
+    }
+
+    /**
+     * Parse a danger progress bar.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the danger progress bar.
+     */
+    public static function parseDangerProgressBar(array $args): ProgressBarInterface {
+        return static::parseProgressBar(static::newDangerProgressBar(), $args);
+    }
+
+    /**
+     * Parse an info progress bar.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the info progress bar.
+     */
+    public static function parseInfoProgressBar(array $args): ProgressBarInterface {
+        return static::parseProgressBar(static::newInfoProgressBar(), $args);
+    }
+
+    /**
+     * Parse a progress bar.
+     *
+     * @param ProgressBarInterface $progressBar The progress bar.
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the progress bar.
+     */
+    protected static function parseProgressBar(ProgressBarInterface $progressBar, array $args): ProgressBarInterface {
+
+        $progressBar->setAnimated(ArrayHelper::get($args, "animated", false));
+        $progressBar->setContent(ArrayHelper::get($args, "content"));
+        $progressBar->setHeight(ArrayHelper::get($args, "height"));
+        $progressBar->setMax(ArrayHelper::get($args, "max", 100));
+        $progressBar->setMin(ArrayHelper::get($args, "min", 0));
+        $progressBar->setStriped(ArrayHelper::get($args, "striped", false));
+        $progressBar->setValue(ArrayHelper::get($args, "value", 50));
+
+        return $progressBar;
+    }
+
+    /**
+     * Parse a success progress bar.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the success progress bar.
+     */
+    public static function parseSuccessProgressBar(array $args): ProgressBarInterface {
+        return static::parseProgressBar(static::newSuccessProgressBar(), $args);
+    }
+
+    /**
+     * Parse a warning progress bar.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ProgressBarInterface Returns the warning progress bar.
+     */
+    public static function parseWarningProgressBar(array $args): ProgressBarInterface {
+        return static::parseProgressBar(static::newWarningProgressBar(), $args);
+    }
+}
---
+++ lib/bootstrap/Tests/Factory/ProgressBarFactoryTest.php
@@ -0,0 +1,214 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\BasicProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\DangerProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\InfoProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\SuccessProgressBar;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\WarningProgressBar;
+use WBW\Bundle\BootstrapBundle\Factory\ProgressBarFactory;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\ProgressBarInterface as BaseProgressBarInterface;
+
+/**
+ * Progress bar factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Factory
+ */
+class ProgressBarFactoryTest extends AbstractTestCase {
+
+    /**
+     * Arguments.
+     *
+     * @var array<string,mixed>|null
+     */
+    private $args;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set an arguments mock.
+        $this->args = [
+            "content"  => "content",
+            "value"    => 25,
+            "min"      => 0,
+            "max"      => 1000,
+            "animated" => true,
+            "striped"  => true,
+        ];
+    }
+
+    /**
+     * Test newBasicProgressBar()
+     *
+     * @return void
+     */
+    public function testNewBasicProgressBar(): void {
+
+        $obj = ProgressBarFactory::newBasicProgressBar();
+
+        $this->assertInstanceOf(BasicProgressBar::class, $obj);
+    }
+
+    /**
+     * Test newDangerProgressBar()
+     *
+     * @return void
+     */
+    public function testNewDangerProgressBar(): void {
+
+        $obj = ProgressBarFactory::newDangerProgressBar();
+
+        $this->assertInstanceOf(DangerProgressBar::class, $obj);
+    }
+
+    /**
+     * Test newInfoProgressBar()
+     *
+     * @return void
+     */
+    public function testNewInfoProgressBar(): void {
+
+        $obj = ProgressBarFactory::newInfoProgressBar();
+
+        $this->assertInstanceOf(InfoProgressBar::class, $obj);
+    }
+
+    /**
+     * Test newSuccessProgressBar()
+     *
+     * @return void
+     */
+    public function testNewSuccessProgressBar(): void {
+
+        $obj = ProgressBarFactory::newSuccessProgressBar();
+
+        $this->assertInstanceOf(SuccessProgressBar::class, $obj);
+    }
+
+    /**
+     * Test newWarningProgressBar()
+     *
+     * @return void
+     */
+    public function testNewWarningProgressBar(): void {
+
+        $obj = ProgressBarFactory::newWarningProgressBar();
+
+        $this->assertInstanceOf(WarningProgressBar::class, $obj);
+    }
+
+    /**
+     * Test parseBasicProgressBar()
+     *
+     * @return void
+     */
+    public function testParseBasicProgressBar(): void {
+
+        $obj = ProgressBarFactory::parseBasicProgressBar($this->args);
+
+        $this->assertInstanceOf(BasicProgressBar::class, $obj);
+
+        $this->assertEquals($this->args["animated"], $obj->getAnimated());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["max"], $obj->getMax());
+        $this->assertEquals($this->args["min"], $obj->getMin());
+        $this->assertEquals($this->args["striped"], $obj->getStriped());
+        $this->assertEquals($this->args["value"], $obj->getValue());
+        $this->assertNull($obj->getType());
+    }
+
+    /**
+     * Test parseDangerProgressBar()
+     *
+     * @return void
+     */
+    public function testParseDangerProgressBar(): void {
+
+        $obj = ProgressBarFactory::parseDangerProgressBar($this->args);
+
+        $this->assertInstanceOf(DangerProgressBar::class, $obj);
+
+        $this->assertEquals($this->args["animated"], $obj->getAnimated());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["max"], $obj->getMax());
+        $this->assertEquals($this->args["min"], $obj->getMin());
+        $this->assertEquals($this->args["striped"], $obj->getStriped());
+        $this->assertEquals($this->args["value"], $obj->getValue());
+        $this->assertEquals(BaseProgressBarInterface::PROGRESS_BAR_TYPE_DANGER, $obj->getType());
+    }
+
+    /**
+     * Test parseInfoProgressBar()
+     *
+     * @return void
+     */
+    public function testParseInfoProgressBar(): void {
+
+        $obj = ProgressBarFactory::parseInfoProgressBar($this->args);
+
+        $this->assertInstanceOf(InfoProgressBar::class, $obj);
+
+        $this->assertEquals($this->args["animated"], $obj->getAnimated());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["max"], $obj->getMax());
+        $this->assertEquals($this->args["min"], $obj->getMin());
+        $this->assertEquals($this->args["striped"], $obj->getStriped());
+        $this->assertEquals($this->args["value"], $obj->getValue());
+        $this->assertEquals(BaseProgressBarInterface::PROGRESS_BAR_TYPE_INFO, $obj->getType());
+    }
+
+    /**
+     * Test parseSuccessProgressBar()
+     *
+     * @return void
+     */
+    public function testParseSuccessProgressBar(): void {
+
+        $obj = ProgressBarFactory::parseSuccessProgressBar($this->args);
+
+        $this->assertInstanceOf(SuccessProgressBar::class, $obj);
+
+        $this->assertEquals($this->args["animated"], $obj->getAnimated());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["max"], $obj->getMax());
+        $this->assertEquals($this->args["min"], $obj->getMin());
+        $this->assertEquals($this->args["striped"], $obj->getStriped());
+        $this->assertEquals($this->args["value"], $obj->getValue());
+        $this->assertEquals(BaseProgressBarInterface::PROGRESS_BAR_TYPE_SUCCESS, $obj->getType());
+    }
+
+    /**
+     * Test parseWarningProgressBar()
+     *
+     * @return void
+     */
+    public function testParseWarningProgressBar(): void {
+
+        $obj = ProgressBarFactory::parseWarningProgressBar($this->args);
+
+        $this->assertInstanceOf(WarningProgressBar::class, $obj);
+
+        $this->assertEquals($this->args["animated"], $obj->getAnimated());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["max"], $obj->getMax());
+        $this->assertEquals($this->args["min"], $obj->getMin());
+        $this->assertEquals($this->args["striped"], $obj->getStriped());
+        $this->assertEquals($this->args["value"], $obj->getValue());
+        $this->assertEquals(BaseProgressBarInterface::PROGRESS_BAR_TYPE_WARNING, $obj->getType());
+    }
+}
