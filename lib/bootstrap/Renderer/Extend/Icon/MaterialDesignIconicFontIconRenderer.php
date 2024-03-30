cf849a5403477fd493175ad89909b8ca1c1125d6
webeweb <support@webeweb.fr>
2024-03-30 17:03:52 +0100

    Copy Material Design iconic font renderer (from core-bundle)

---
+++ lib/bootstrap/Renderer/Extend/Icon/MaterialDesignIconicFontIconRenderer.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\MaterialDesignIconicFontIconInterface;
+use WBW\Library\Symfony\Renderer\Assets\IconRenderer;
+
+/**
+ * Material Design Iconic Font icon renderer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon
+ */
+class MaterialDesignIconicFontIconRenderer extends IconRenderer {
+
+    /**
+     * Render a border.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered border in case of success, null otherwise.
+     */
+    public static function renderBorder(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getBorder() ? "zmdi-hc-" . $icon->getBorder() : null;
+    }
+
+    /**
+     * Render a fixed width.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered fixed width in case of success, null otherwise.
+     */
+    public static function renderFixedWidth(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return true === $icon->getFixedWidth() ? "zmdi-hc-fw" : null;
+    }
+
+    /**
+     * Render a flip.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered flip.
+     */
+    public static function renderFlip(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getFlip() ? "zmdi-hc-flip-" . $icon->getFlip() : null;
+    }
+
+    /**
+     * Render a name.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered name in case of success, false otherwise.
+     */
+    public static function renderName(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getName() ? "zmdi-" . $icon->getName() : null;
+    }
+
+    /**
+     * Render a pull.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered pull in case of success, null otherwise.
+     */
+    public static function renderPull(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getPull() ? "pull-" . $icon->getPull() : null;
+    }
+
+    /**
+     * Render a rotate.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered rotate in case of success, null otherwise.
+     */
+    public static function renderRotate(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getRotate() ? "zmdi-hc-rotate-" . $icon->getRotate() : null;
+    }
+
+    /**
+     * Render a size.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered size in case of success, null otherwise.
+     */
+    public static function renderSize(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getSize() ? "zmdi-hc-" . $icon->getSize() : null;
+    }
+
+    /**
+     * Render a spin.
+     *
+     * @param MaterialDesignIconicFontIconInterface $icon The icon.
+     * @return string|null Returns the rendered spin in case of success, null otherwise.
+     */
+    public static function renderSpin(MaterialDesignIconicFontIconInterface $icon): ?string {
+        return null !== $icon->getSpin() ? "zmdi-hc-" . $icon->getSpin() : null;
+    }
+}
---
+++ lib/bootstrap/Tests/Renderer/Extend/Icon/MaterialDesignIconicFontIconRendererTest.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Renderer\Extend\Icon;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\MaterialDesignIconicFontIconInterface;
+use WBW\Bundle\BootstrapBundle\Factory\Extend\IconFactory;
+use WBW\Bundle\BootstrapBundle\Renderer\Extend\Icon\MaterialDesignIconicFontIconRenderer;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Material Design Iconic Font icon renderer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Renderer\Extend\Icon
+ */
+class MaterialDesignIconicFontIconRendererTest extends AbstractTestCase {
+
+    /**
+     * Icon.
+     *
+     * @var MaterialDesignIconicFontIconInterface|null
+     */
+    private $icon;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Material Design iconic font icon mock.
+        $this->icon = IconFactory::parseMaterialDesignIconicFontIcon([
+            "name"       => "home",
+            "style"      => "color: #000000;",
+            "border"     => "border",
+            "fixedWidth" => true,
+            "flip"       => "horizontal",
+            "pull"       => "left",
+            "rotate"     => "90",
+            "size"       => "lg",
+            "spin"       => "spin",
+        ]);
+    }
+
+    /**
+     * Test renderBorder()
+     *
+     * @return void
+     */
+    public function testRenderBorder(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderBorder($this->icon);
+        $this->assertEquals("zmdi-hc-border", $res);
+    }
+
+    /**
+     * Test renderFixedWidth()
+     *
+     * @return void
+     */
+    public function testRenderFixedWidth(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderFixedWidth($this->icon);
+        $this->assertEquals("zmdi-hc-fw", $res);
+    }
+
+    /**
+     * Test renderFlip()
+     *
+     * @return void
+     */
+    public function testRenderFlip(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderFlip($this->icon);
+        $this->assertEquals("zmdi-hc-flip-horizontal", $res);
+    }
+
+    /**
+     * Test renderName()
+     *
+     * @return void
+     */
+    public function testRenderName(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderName($this->icon);
+        $this->assertEquals("zmdi-home", $res);
+    }
+
+    /**
+     * Test renderPull()
+     *
+     * @return void
+     */
+    public function testRenderPull(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderPull($this->icon);
+        $this->assertEquals("pull-left", $res);
+    }
+
+    /**
+     * Test renderRotate()
+     *
+     * @return void
+     */
+    public function testRenderRotate(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderRotate($this->icon);
+        $this->assertEquals("zmdi-hc-rotate-90", $res);
+    }
+
+    /**
+     * Test renderSize()
+     *
+     * @return void
+     */
+    public function testRenderSize(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderSize($this->icon);
+        $this->assertEquals("zmdi-hc-lg", $res);
+    }
+
+    /**
+     * Test renderSpin()
+     *
+     * @return void
+     */
+    public function testRenderSpin(): void {
+
+        $res = MaterialDesignIconicFontIconRenderer::renderSpin($this->icon);
+        $this->assertEquals("zmdi-hc-spin", $res);
+    }
+}
