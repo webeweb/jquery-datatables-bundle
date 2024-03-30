138225e5cdc66bda387e90b86035d7cf38b5f999
webeweb <support@webeweb.fr>
2024-03-30 13:47:05 +0100

    Copy abstract button (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Fixtures/Model/TestButton.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Fixtures\Model;
+
+use WBW\Bundle\BootstrapBundle\Model\AbstractButton;
+
+/**
+ * Test button.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Fixtures\Model
+ */
+class TestButton extends AbstractButton {
+
+    /**
+     * Constructor.
+     *
+     * @param string|null $type The type.
+     */
+    public function __construct(?string $type) {
+        parent::__construct($type);
+    }
+}
