a8844b5ee34287f1f069e4f2e477c9ad478b9d37
webeweb <support@webeweb.fr>
2024-03-30 13:59:23 +0100

    Copy Danger badge (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/Badge/DangerBadgeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\Badge;
+
+use WBW\Bundle\BootstrapBundle\Model\Badge\DangerBadge;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\BadgeInterface as BaseBadgeInterface;
+
+/**
+ * Danger badge test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\Badge
+ */
+class DangerBadgeTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DangerBadge();
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_DANGER, $obj->getType());
+    }
+}
