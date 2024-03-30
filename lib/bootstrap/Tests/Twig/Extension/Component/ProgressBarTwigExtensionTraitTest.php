33e389c9f8101c448a6247f9a73cb1cb4b57bc5c
webeweb <support@webeweb.fr>
2024-03-30 15:20:46 +0100

    Copy Progress bar Twig extension trait (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/Component/ProgressBarTwigExtensionTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestProgressBarTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ProgressBarTwigExtension;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Progress bar Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class ProgressBarTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setProgressBarTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetProgressBarTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set a Progress bar Twig extension mock.
+        $progressBarTwigExtension = new ProgressBarTwigExtension($twigEnvironment);
+
+        $obj = new TestProgressBarTwigExtensionTrait();
+
+        $obj->setProgressBarTwigExtension($progressBarTwigExtension);
+        $this->assertSame($progressBarTwigExtension, $obj->getProgressBarTwigExtension());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Component/ProgressBarTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Progress bar Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait ProgressBarTwigExtensionTrait {
+
+    /**
+     * Progress bar Twig extension.
+     *
+     * @var ProgressBarTwigExtension|null
+     */
+    private $progressBarTwigExtension;
+
+    /**
+     * Get the progress bar Twig extension.
+     *
+     * @return ProgressBarTwigExtension|null Returns the progress bar Twig extension.
+     */
+    public function getProgressBarTwigExtension(): ?ProgressBarTwigExtension {
+        return $this->progressBarTwigExtension;
+    }
+
+    /**
+     * Set the progress bar Twig extension.
+     *
+     * @param ProgressBarTwigExtension|null $progressBarTwigExtension The progress bar Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setProgressBarTwigExtension(?ProgressBarTwigExtension $progressBarTwigExtension): self {
+        $this->progressBarTwigExtension = $progressBarTwigExtension;
+        return $this;
+    }
+}
