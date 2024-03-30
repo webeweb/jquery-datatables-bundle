e1d6e3012ce4dfe8d9c6796fad5c00f9afd2e269
webeweb <support@webeweb.fr>
2024-03-30 14:15:13 +0100

    Copy Alert factory (from bootstrap-bundle)

---
+++ lib/bootstrap/Factory/AlertFactory.php
@@ -0,0 +1,199 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Alert\DangerAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\DarkAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\InfoAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\LightAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\PrimaryAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\SecondaryAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\SuccessAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\WarningAlert;
+use WBW\Bundle\BootstrapBundle\Model\AlertInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Alert factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Factory
+ */
+class AlertFactory {
+
+    /**
+     * Create a new danger alert.
+     *
+     * @return AlertInterface Returns the danger alert.
+     */
+    public static function newDangerAlert(): AlertInterface {
+        return new DangerAlert();
+    }
+
+    /**
+     * Create a new default alert.
+     *
+     * @return AlertInterface Returns the default alert.
+     */
+    public static function newDarkAlert(): AlertInterface {
+        return new DarkAlert();
+    }
+
+    /**
+     * Create a new info alert.
+     *
+     * @return AlertInterface Returns the info alert.
+     */
+    public static function newInfoAlert(): AlertInterface {
+        return new InfoAlert();
+    }
+
+    /**
+     * Create a new light alert.
+     *
+     * @return AlertInterface Returns the light alert.
+     */
+    public static function newLightAlert(): AlertInterface {
+        return new LightAlert();
+    }
+
+    /**
+     * Create a new primary alert.
+     *
+     * @return AlertInterface Returns the primary alert.
+     */
+    public static function newPrimaryAlert(): AlertInterface {
+        return new PrimaryAlert();
+    }
+
+    /**
+     * Create a new secondary alert.
+     *
+     * @return AlertInterface Returns the secondary alert.
+     */
+    public static function newSecondaryAlert(): AlertInterface {
+        return new SecondaryAlert();
+    }
+
+    /**
+     * Create a new success alert.
+     *
+     * @return AlertInterface Returns the success alert.
+     */
+    public static function newSuccessAlert(): AlertInterface {
+        return new SuccessAlert();
+    }
+
+    /**
+     * Create a new warning alert.
+     *
+     * @return AlertInterface Returns the warning alert.
+     */
+    public static function newWarningAlert(): AlertInterface {
+        return new WarningAlert();
+    }
+
+    /**
+     * Parse an alert.
+     *
+     * @param AlertInterface $alert The alert.
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the alert.
+     */
+    protected static function parseAlert(AlertInterface $alert, array $args): AlertInterface {
+
+        $alert->setContent(ArrayHelper::get($args, "content"));
+        $alert->setDismissible(ArrayHelper::get($args, "dismissible", false));
+
+        return $alert;
+    }
+
+    /**
+     * Parse a danger alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the danger alert.
+     */
+    public static function parseDangerAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newDangerAlert(), $args);
+    }
+
+    /**
+     * Parse a dark alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the dark alert.
+     */
+    public static function parseDarkAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newDarkAlert(), $args);
+    }
+
+    /**
+     * Parse an info alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the info alert.
+     */
+    public static function parseInfoAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newInfoAlert(), $args);
+    }
+
+    /**
+     * Parse a light alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the light alert.
+     */
+    public static function parseLightAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newLightAlert(), $args);
+    }
+
+    /**
+     * Parse a primary alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the primary alert.
+     */
+    public static function parsePrimaryAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newPrimaryAlert(), $args);
+    }
+
+    /**
+     * Parse a secondary alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the secondary alert.
+     */
+    public static function parseSecondaryAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newSecondaryAlert(), $args);
+    }
+
+    /**
+     * Parse a success alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the success alert.
+     */
+    public static function parseSuccessAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newSuccessAlert(), $args);
+    }
+
+    /**
+     * Parse a warning alert.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return AlertInterface Returns the warning alert.
+     */
+    public static function parseWarningAlert(array $args): AlertInterface {
+        return static::parseAlert(static::newWarningAlert(), $args);
+    }
+}
---
+++ lib/bootstrap/Tests/Factory/AlertFactoryTest.php
@@ -0,0 +1,286 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Alert\DangerAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\DarkAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\InfoAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\LightAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\PrimaryAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\SecondaryAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\SuccessAlert;
+use WBW\Bundle\BootstrapBundle\Model\Alert\WarningAlert;
+use WBW\Bundle\BootstrapBundle\Model\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Factory\AlertFactory;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\AlertInterface as BaseAlertInterface;
+
+/**
+ * Alert factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Factory
+ */
+class AlertFactoryTest extends AbstractTestCase {
+
+    /**
+     * Arguments.
+     *
+     * @var array<string,mixed>|null
+     */
+    private $args;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set an arguments mock.
+        $this->args = [
+            "content"     => "content",
+            "dismissible" => true,
+        ];
+    }
+
+    /**
+     * Test newDangerAlert()
+     *
+     * @return void
+     */
+    public function testNewDangerAlert(): void {
+
+        $obj = AlertFactory::newDangerAlert();
+
+        $this->assertInstanceOf(DangerAlert::class, $obj);
+    }
+
+    /**
+     * Test newDarkAlert()
+     *
+     * @return void
+     */
+    public function testNewDarkAlert(): void {
+
+        $obj = AlertFactory::newDarkAlert();
+
+        $this->assertInstanceOf(DarkAlert::class, $obj);
+    }
+
+    /**
+     * Test newInfoAlert()
+     *
+     * @return void
+     */
+    public function testNewInfoAlert(): void {
+
+        $obj = AlertFactory::newInfoAlert();
+
+        $this->assertInstanceOf(InfoAlert::class, $obj);
+    }
+
+    /**
+     * Test newLightAlert()
+     *
+     * @return void
+     */
+    public function testNewLightAlert(): void {
+
+        $obj = AlertFactory::newLightAlert();
+
+        $this->assertInstanceOf(LightAlert::class, $obj);
+    }
+
+    /**
+     * Test newPrimaryAlert()
+     *
+     * @return void
+     */
+    public function testNewPrimaryAlert(): void {
+
+        $obj = AlertFactory::newPrimaryAlert();
+
+        $this->assertInstanceOf(PrimaryAlert::class, $obj);
+    }
+
+    /**
+     * Test newSecondaryAlert()
+     *
+     * @return void
+     */
+    public function testNewSecondaryAlert(): void {
+
+        $obj = AlertFactory::newSecondaryAlert();
+
+        $this->assertInstanceOf(SecondaryAlert::class, $obj);
+    }
+
+    /**
+     * Test newSuccessAlert()
+     *
+     * @return void
+     */
+    public function testNewSuccessAlert(): void {
+
+        $obj = AlertFactory::newSuccessAlert();
+
+        $this->assertInstanceOf(SuccessAlert::class, $obj);
+    }
+
+    /**
+     * Test newWarningAlert()
+     *
+     * @return void
+     */
+    public function testNewWarningAlert(): void {
+
+        $obj = AlertFactory::newWarningAlert();
+
+        $this->assertInstanceOf(WarningAlert::class, $obj);
+    }
+
+    /**
+     * Test parseDangerAlert()
+     *
+     * @return void
+     */
+    public function testParseDangerAlert(): void {
+
+        $obj = AlertFactory::parseDangerAlert($this->args);
+
+        $this->assertInstanceOf(DangerAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(BaseAlertInterface::ALERT_TYPE_DANGER, $obj->getType());
+    }
+
+    /**
+     * Test parseDarkAlert()
+     *
+     * @return void
+     */
+    public function testParseDarkAlert(): void {
+
+        $obj = AlertFactory::parseDarkAlert($this->args);
+
+        $this->assertInstanceOf(DarkAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_DARK, $obj->getType());
+    }
+
+    /**
+     * Test parseInfoAlert()
+     *
+     * @return void
+     */
+    public function testParseInfoAlert(): void {
+
+        $obj = AlertFactory::parseInfoAlert($this->args);
+
+        $this->assertInstanceOf(InfoAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(BaseAlertInterface::ALERT_TYPE_INFO, $obj->getType());
+    }
+
+    /**
+     * Test parseLightAlert()
+     *
+     * @return void
+     */
+    public function testParseLightAlert(): void {
+
+        $obj = AlertFactory::parseLightAlert($this->args);
+
+        $this->assertInstanceOf(LightAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_LIGHT, $obj->getType());
+    }
+
+    /**
+     * Test parsePrimaryAlert()
+     *
+     * @return void
+     */
+    public function testParsePrimaryAlert(): void {
+
+        $obj = AlertFactory::parsePrimaryAlert($this->args);
+
+        $this->assertInstanceOf(PrimaryAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_PRIMARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSecondaryAlert()
+     *
+     * @return void
+     */
+    public function testParseSecondaryAlert(): void {
+
+        $obj = AlertFactory::parseSecondaryAlert($this->args);
+
+        $this->assertInstanceOf(SecondaryAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(AlertInterface::ALERT_TYPE_SECONDARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSuccessAlert()
+     *
+     * @return void
+     */
+    public function testParseSuccessAlert(): void {
+
+        $obj = AlertFactory::parseSuccessAlert($this->args);
+
+        $this->assertInstanceOf(SuccessAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(BaseAlertInterface::ALERT_TYPE_SUCCESS, $obj->getType());
+    }
+
+    /**
+     * Test parseWarningAlert()
+     *
+     * @return void
+     */
+    public function testParseWarningAlert(): void {
+
+        $obj = AlertFactory::parseWarningAlert($this->args);
+
+        $this->assertInstanceOf(WarningAlert::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["dismissible"], $obj->getDismissible());
+
+        $this->assertEquals(BaseAlertInterface::ALERT_TYPE_WARNING, $obj->getType());
+    }
+}
