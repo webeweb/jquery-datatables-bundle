2454ab76af9f67985c0f7dd658f1feab97384174
webeweb <support@webeweb.fr>
2024-03-30 10:52:11 +0100

    Copy Toast event (from core-bundle)

---
+++ lib/common/Event/ToastEvent.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Event;
+
+use WBW\Library\Symfony\Assets\ToastInterface;
+use WBW\Library\Symfony\Assets\ToastTrait;
+
+/**
+ * Toast event.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Event
+ */
+class ToastEvent extends AbstractEvent {
+
+    use ToastTrait;
+
+    /**
+     * Event "danger".
+     *
+     * @var string
+     */
+    public const DANGER = "wbw.common.event.toast.danger";
+
+    /**
+     * Event "info".
+     *
+     * @var string
+     */
+    public const INFO = "wbw.common.event.toast.info";
+
+    /**
+     * Event "success".
+     *
+     * @var string
+     */
+    public const SUCCESS = "wbw.common.event.toast.success";
+
+    /**
+     * Event "warning".
+     *
+     * @var string
+     */
+    public const WARNING = "wbw.common.event.toast.warning";
+
+    /**
+     * Constructor.
+     *
+     * @param string $eventName The event name.
+     * @param ToastInterface $toast The toast.
+     */
+    public function __construct(string $eventName, ToastInterface $toast) {
+        parent::__construct($eventName);
+
+        $this->setToast($toast);
+    }
+}
---
+++ lib/common/Tests/Event/ToastEventTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Event;
+
+use WBW\Bundle\CommonBundle\Event\ToastEvent;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\ToastInterface;
+
+/**
+ * Toast event test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Event
+ */
+class ToastEventTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__constructor(): void {
+
+        $this->assertEquals("wbw.common.event.toast.danger", ToastEvent::DANGER);
+        $this->assertEquals("wbw.common.event.toast.info", ToastEvent::INFO);
+        $this->assertEquals("wbw.common.event.toast.success", ToastEvent::SUCCESS);
+        $this->assertEquals("wbw.common.event.toast.warning", ToastEvent::WARNING);
+
+        // Set a Toast mock.
+        $toast = $this->getMockBuilder(ToastInterface::class)->getMock();
+
+        $obj = new ToastEvent("eventName", $toast);
+
+        $this->assertEquals("eventName", $obj->getEventName());
+        $this->assertSame($toast, $obj->getToast());
+    }
+}
