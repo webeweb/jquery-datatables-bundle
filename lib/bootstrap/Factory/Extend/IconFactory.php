043d109fbeb4aafe31110e094f72e35a66d6898c
webeweb <support@webeweb.fr>
2024-03-30 16:57:07 +0100

    Copy Icon factory (from core-bundle)

---
+++ lib/bootstrap/Factory/Extend/IconFactory.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Factory\Extend;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\FontAwesomeIcon;
+use WBW\Bundle\BootstrapBundle\Extend\Icon\FontAwesomeIconInterface;
+use WBW\Bundle\BootstrapBundle\Extend\Icon\MaterialDesignIconicFontIcon;
+use WBW\Bundle\BootstrapBundle\Extend\Icon\MaterialDesignIconicFontIconInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Icon factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Factory
+ */
+class IconFactory {
+
+    /**
+     * Create a Font Awesome icon.
+     *
+     * @return FontAwesomeIconInterface Returns the Font Awesome icon.
+     */
+    public static function newFontAwesomeIcon(): FontAwesomeIconInterface {
+        return new FontAwesomeIcon();
+    }
+
+    /**
+     * Create a Material Design Iconic Font icon.
+     *
+     * @return MaterialDesignIconicFontIconInterface Returns the Material Design Iconic Font icon.
+     */
+    public static function newMaterialDesignIconicFontIcon(): MaterialDesignIconicFontIconInterface {
+        return new MaterialDesignIconicFontIcon();
+    }
+
+    /**
+     * Parse a Font Awesome icon.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return FontAwesomeIconInterface Returns the parsed Font Awesome icon.
+     */
+    public static function parseFontAwesomeIcon(array $args): FontAwesomeIconInterface {
+
+        $icon = static::newFontAwesomeIcon();
+
+        $icon->setName(ArrayHelper::get($args, "name", "home"));
+        $icon->setStyle(ArrayHelper::get($args, "style"));
+
+        $icon->setAnimation(ArrayHelper::get($args, "animation"));
+        $icon->setBordered(ArrayHelper::get($args, "bordered", false));
+        $icon->setFixedWidth(ArrayHelper::get($args, "fixedWidth", false));
+        $icon->setFont(ArrayHelper::get($args, "font"));
+        $icon->setPull(ArrayHelper::get($args, "pull"));
+        $icon->setSize(ArrayHelper::get($args, "size"));
+
+        return $icon;
+    }
+
+    /**
+     * Parse a Material Design Iconic Font icon.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return MaterialDesignIconicFontIconInterface Returns the parsed Material Design Iconic Font icon.
+     */
+    public static function parseMaterialDesignIconicFontIcon(array $args): MaterialDesignIconicFontIconInterface {
+
+        $icon = static::newMaterialDesignIconicFontIcon();
+
+        $icon->setName(ArrayHelper::get($args, "name", "home"));
+        $icon->setStyle(ArrayHelper::get($args, "style"));
+
+        $icon->setBorder(ArrayHelper::get($args, "border", false));
+        $icon->setFixedWidth(ArrayHelper::get($args, "fixedWidth", false));
+        $icon->setFlip(ArrayHelper::get($args, "flip"));
+        $icon->setPull(ArrayHelper::get($args, "pull"));
+        $icon->setRotate(ArrayHelper::get($args, "rotate"));
+        $icon->setSize(ArrayHelper::get($args, "size"));
+        $icon->setSpin(ArrayHelper::get($args, "spin"));
+
+        return $icon;
+    }
+}
---
+++ lib/bootstrap/Tests/Factory/Extend/IconFactoryTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Factory\Extend;
+
+use WBW\Bundle\BootstrapBundle\Extend\Icon\FontAwesomeIconInterface;
+use WBW\Bundle\BootstrapBundle\Extend\Icon\MaterialDesignIconicFontIconInterface;
+use WBW\Bundle\BootstrapBundle\Factory\Extend\IconFactory;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Icon factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Factory\Extend
+ */
+class IconFactoryTest extends AbstractTestCase {
+
+    /**
+     * Test newFontAwesomeIcon()
+     *
+     * @return void
+     */
+    public function testNewFontAwesomeIcon(): void {
+
+        $obj = IconFactory::newFontAwesomeIcon();
+        $this->assertInstanceOf(FontAwesomeIconInterface::class, $obj);
+    }
+
+    /**
+     * Test newMaterialDesignIconicFontIcon()
+     *
+     * @return void
+     */
+    public function testNewMaterialDesignIconicFontIcon(): void {
+
+        $obj = IconFactory::newMaterialDesignIconicFontIcon();
+        $this->assertInstanceOf(MaterialDesignIconicFontIconInterface::class, $obj);
+    }
+
+    /**
+     * Test parseFontAwesomeIcon()
+     *
+     * @return void
+     */
+    public function testParseFontAwesomeIcon(): void {
+
+        // Set the Arguments mock.
+        $arg = [
+            "name"       => "home",
+            "style"      => "color: #000000;",
+            "animation"  => "spin",
+            "bordered"   => true,
+            "fixedWidth" => true,
+            "font"       => "s",
+            "pull"       => "left",
+            "size"       => "lg",
+        ];
+
+        $obj = IconFactory::parseFontAwesomeIcon($arg);
+        $this->assertInstanceOf(FontAwesomeIconInterface::class, $obj);
+
+        $this->assertEquals($arg["name"], $obj->getName());
+        $this->assertEquals($arg["style"], $obj->getStyle());
+
+        $this->assertEquals($arg["animation"], $obj->getAnimation());
+        $this->assertEquals($arg["bordered"], $obj->getBordered());
+        $this->assertEquals($arg["fixedWidth"], $obj->getFixedWidth());
+        $this->assertEquals($arg["font"], $obj->getFont());
+        $this->assertEquals($arg["pull"], $obj->getPull());
+        $this->assertEquals($arg["size"], $obj->getSize());
+    }
+
+    /**
+     * Test parseMaterialDesignIconicFontIcon()
+     *
+     * @return void
+     */
+    public function testParseMaterialDesignIconicFontIcon(): void {
+
+        // Set the Arguments mock.
+        $arg = [
+            "name"       => "home",
+            "style"      => "color: #000000;",
+            "border"     => "border",
+            "fixedWidth" => true,
+            "flip"       => "horizontal",
+            "pull"       => "left",
+            "rotate"     => "90",
+            "size"       => "lg",
+            "spin"       => "spin",
+        ];
+
+        $obj = IconFactory::parseMaterialDesignIconicFontIcon($arg);
+        $this->assertNotNull($obj);
+        $this->assertInstanceOf(MaterialDesignIconicFontIconInterface::class, $obj);
+
+        $this->assertEquals($arg["name"], $obj->getName());
+        $this->assertEquals($arg["style"], $obj->getStyle());
+
+        $this->assertEquals($arg["border"], $obj->getBorder());
+        $this->assertEquals($arg["fixedWidth"], $obj->getFixedWidth());
+        $this->assertEquals($arg["flip"], $obj->getFlip());
+        $this->assertEquals($arg["pull"], $obj->getPull());
+        $this->assertEquals($arg["rotate"], $obj->getRotate());
+        $this->assertEquals($arg["size"], $obj->getSize());
+        $this->assertEquals($arg["spin"], $obj->getSpin());
+    }
+}
