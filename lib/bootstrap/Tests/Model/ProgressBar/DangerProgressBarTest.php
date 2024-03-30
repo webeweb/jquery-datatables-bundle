e1629766e7eb54d5acbdefb66861ee18d91da8f3
webeweb <support@webeweb.fr>
2024-03-30 14:09:24 +0100

    Copy Danger progress bar (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/ProgressBar/DangerProgressBarTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\ProgressBar;
+
+use WBW\Bundle\BootstrapBundle\Model\ProgressBar\DangerProgressBar;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\ProgressBarInterface as BaseProgressBarInterface;
+
+/**
+ * Danger progress bar test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\ProgressBar
+ */
+class DangerProgressBarTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DangerProgressBar();
+
+        $this->assertEquals(BaseProgressBarInterface::PROGRESS_BAR_TYPE_DANGER, $obj->getType());
+    }
+}
