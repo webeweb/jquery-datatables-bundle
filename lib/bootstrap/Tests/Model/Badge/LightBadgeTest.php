471ce43009af8c5aab14dddcaf726b532a001434
webeweb <support@webeweb.fr>
2024-03-30 13:59:57 +0100

    Copy Light badge (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/Badge/LightBadgeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\Badge;
+
+use WBW\Bundle\BootstrapBundle\Model\Badge\LightBadge;
+use WBW\Bundle\BootstrapBundle\Model\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Light badge test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\Badge
+ */
+class LightBadgeTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new LightBadge();
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_LIGHT, $obj->getType());
+    }
+}
