0260a467c0d85f4eb6aef6eca2e0883e0f83c0a3
webeweb <support@webeweb.fr>
2024-03-30 13:35:55 +0100

    Copy JSON serializer (from bootstrap-bundle)

---
+++ lib/bootstrap/Serializer/JsonSerializer.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Serializer;
+
+use WBW\Bundle\BootstrapBundle\Model\AlertInterface;
+use WBW\Bundle\BootstrapBundle\Model\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Model\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Model\ProgressBarInterface;
+use WBW\Library\Serializer\SerializerKeys as BaseSerializerKeys;
+
+/**
+ * JSON serializer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Serializer
+ */
+class JsonSerializer {
+
+    /**
+     * Serialize an alert.
+     *
+     * @param AlertInterface $model The model.
+     * @return array Returns the serialized model.
+     */
+    public static function serializeAlert(AlertInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            SerializerKeys::DISMISSIBLE => $model->getDismissible(),
+            SerializerKeys::PREFIX      => $model->getPrefix(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a badge.
+     *
+     * @param BadgeInterface $model The model.
+     * @return array Returns the serialized model.
+     */
+    public static function serializeBadge(BadgeInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            SerializerKeys::PILL        => $model->getPill(),
+            SerializerKeys::PREFIX      => $model->getPrefix(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a button.
+     *
+     * @param ButtonInterface $model The model.
+     * @return array Returns the serialized model.
+     */
+    public static function serializeButton(ButtonInterface $model): array {
+
+        return [
+            BaseSerializerKeys::ACTIVE   => $model->getActive(),
+            SerializerKeys::BLOCK        => $model->getBlock(),
+            BaseSerializerKeys::DATA     => $model->getData(),
+            BaseSerializerKeys::DISABLED => $model->getDisabled(),
+            BaseSerializerKeys::CONTENT  => $model->getContent(),
+            SerializerKeys::OUTLINE      => $model->getOutline(),
+            SerializerKeys::PREFIX       => $model->getPrefix(),
+            BaseSerializerKeys::SIZE     => $model->getSize(),
+            BaseSerializerKeys::TITLE    => $model->getTitle(),
+            BaseSerializerKeys::TYPE     => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a progress bar.
+     *
+     * @param ProgressBarInterface $model The model.
+     * @return array Returns the serialized model.
+     */
+    public static function serializeProgressBar(ProgressBarInterface $model): array {
+
+        return [
+            SerializerKeys::ANIMATED    => $model->getAnimated(),
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::HEIGHT  => $model->getHeight(),
+            BaseSerializerKeys::MAX     => $model->getMax(),
+            BaseSerializerKeys::MIN     => $model->getMin(),
+            SerializerKeys::PREFIX      => $model->getPrefix(),
+            SerializerKeys::STRIPED     => $model->getStriped(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+            BaseSerializerKeys::VALUE   => $model->getValue(),
+        ];
+    }
+}
