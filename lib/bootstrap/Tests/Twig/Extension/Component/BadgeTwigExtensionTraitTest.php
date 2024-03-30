5a97ff43b74a7460b39cd6544ec47009e7c57756
webeweb <support@webeweb.fr>
2024-03-30 15:20:37 +0100

    Copy Badge Twig extension trait (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/Component/BadgeTwigExtensionTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestBadgeTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BadgeTwigExtension;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Badge Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class BadgeTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setBadgeTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetBadgeTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set a Badge Twig extension mock.
+        $badgeTwigExtension = new BadgeTwigExtension($twigEnvironment);
+
+        $obj = new TestBadgeTwigExtensionTrait();
+
+        $obj->setBadgeTwigExtension($badgeTwigExtension);
+        $this->assertSame($badgeTwigExtension, $obj->getBadgeTwigExtension());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Component/BadgeTwigExtensionTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Badge Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait BadgeTwigExtensionTrait {
+
+    /**
+     * Badge Twig extension.
+     *
+     * @var BadgeTwigExtension|null
+     */
+    private $badgeTwigExtension;
+
+    /**
+     * Get the badge Twig extension.
+     *
+     * @return BadgeTwigExtension|null Returns the badge Twig extension.
+     */
+    public function getBadgeTwigExtension(): ?BadgeTwigExtension {
+        return $this->badgeTwigExtension;
+    }
+
+    /**
+     * Set the badge Twig extension.
+     *
+     * @param BadgeTwigExtension|null $badgeTwigExtension The badge Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setBadgeTwigExtension(?BadgeTwigExtension $badgeTwigExtension): self {
+        $this->badgeTwigExtension = $badgeTwigExtension;
+        return $this;
+    }
+}
