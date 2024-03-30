6203f1e0ccc2338586abed9cd0e4f0b95e0e9013
webeweb <support@webeweb.fr>
2024-03-30 17:26:00 +0100

    Copy Material Design iconic font Twig extension trait (from core-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/Extend/MaterialDesignIconicFontTwigExtensionTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend;
+
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Extend\TestMaterialDesignIconicFontTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\MaterialDesignIconicFontTwigExtension;
+
+/**
+ * Material Design Iconic font Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend
+ */
+class MaterialDesignIconicFontTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setMaterialDesignIconicFontTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetMaterialDesignIconicFontTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set a Material Design Iconic font Twig extension mock.
+        $materialDesignIconicFontTwigExtension = new MaterialDesignIconicFontTwigExtension($twigEnvironment);
+
+        $obj = new TestMaterialDesignIconicFontTwigExtensionTrait();
+
+        $obj->setMaterialDesignIconicFontTwigExtension($materialDesignIconicFontTwigExtension);
+        $this->assertSame($materialDesignIconicFontTwigExtension, $obj->getMaterialDesignIconicFontTwigExtension());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Extend/MaterialDesignIconicFontTwigExtensionTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Extend;
+
+/**
+ * Material Design Iconic font Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Extend
+ */
+trait MaterialDesignIconicFontTwigExtensionTrait {
+
+    /**
+     * Material Design Iconic font Twig extension.
+     *
+     * @var MaterialDesignIconicFontTwigExtension|null
+     */
+    private $materialDesignIconicFontTwigExtension;
+
+    /**
+     * Get the Material Design Iconic font Twig extension.
+     *
+     * @return MaterialDesignIconicFontTwigExtension|null Returns the Material Design Iconic font Twig extension.
+     */
+    public function getMaterialDesignIconicFontTwigExtension(): ?MaterialDesignIconicFontTwigExtension {
+        return $this->materialDesignIconicFontTwigExtension;
+    }
+
+    /**
+     * Set the Material Design Iconic font Twig extension.
+     *
+     * @param MaterialDesignIconicFontTwigExtension|null $materialDesignIconicFontTwigExtension The Material Design Iconic font Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setMaterialDesignIconicFontTwigExtension(?MaterialDesignIconicFontTwigExtension $materialDesignIconicFontTwigExtension): self {
+        $this->materialDesignIconicFontTwigExtension = $materialDesignIconicFontTwigExtension;
+        return $this;
+    }
+}
