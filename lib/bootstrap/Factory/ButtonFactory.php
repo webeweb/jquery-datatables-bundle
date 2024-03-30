92d16ddf3da06df9c3fd9ecd61e9f3e732c6583a
webeweb <support@webeweb.fr>
2024-03-30 14:15:30 +0100

    Copy Button factory (from bootstrap-bundle)

---
+++ lib/bootstrap/Factory/ButtonFactory.php
@@ -0,0 +1,245 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Button\DangerButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\DarkButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\DefaultButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\InfoButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\LightButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\LinkButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\PrimaryButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\SecondaryButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\SuccessButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\WarningButton;
+use WBW\Bundle\BootstrapBundle\Model\ButtonInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Button factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Factory
+ */
+class ButtonFactory {
+
+    /**
+     * Create a new danger button.
+     *
+     * @return ButtonInterface Returns the danger button.
+     */
+    public static function newDangerButton(): ButtonInterface {
+        return new DangerButton();
+    }
+
+    /**
+     * Create a new dark button.
+     *
+     * @return ButtonInterface Returns the dark button.
+     */
+    public static function newDarkButton(): ButtonInterface {
+        return new DarkButton();
+    }
+
+    /**
+     * Create a new default button.
+     *
+     * @return ButtonInterface Returns the default button.
+     */
+    public static function newDefaultButton(): ButtonInterface {
+        return new DefaultButton();
+    }
+
+    /**
+     * Create a new info button.
+     *
+     * @return ButtonInterface Returns the info button.
+     */
+    public static function newInfoButton(): ButtonInterface {
+        return new InfoButton();
+    }
+
+    /**
+     * Create a new light button.
+     *
+     * @return ButtonInterface Returns the light button.
+     */
+    public static function newLightButton(): ButtonInterface {
+        return new LightButton();
+    }
+
+    /**
+     * Create a new link button.
+     *
+     * @return ButtonInterface Returns the link button.
+     */
+    public static function newLinkButton(): ButtonInterface {
+        return new LinkButton();
+    }
+
+    /**
+     * Create a new primary button.
+     *
+     * @return ButtonInterface Returns the primary button.
+     */
+    public static function newPrimaryButton(): ButtonInterface {
+        return new PrimaryButton();
+    }
+
+    /**
+     * Create a new secondary button.
+     *
+     * @return ButtonInterface Returns the secondary button.
+     */
+    public static function newSecondaryButton(): ButtonInterface {
+        return new SecondaryButton();
+    }
+
+    /**
+     * Create a new success button.
+     *
+     * @return ButtonInterface Returns the success button.
+     */
+    public static function newSuccessButton(): ButtonInterface {
+        return new SuccessButton();
+    }
+
+    /**
+     * Create a new warning button.
+     *
+     * @return ButtonInterface Returns the warning button.
+     */
+    public static function newWarningButton(): ButtonInterface {
+        return new WarningButton();
+    }
+
+    /**
+     * Parse a button.
+     *
+     * @param ButtonInterface $button The button.
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the button.
+     */
+    protected static function parseButton(ButtonInterface $button, array $args): ButtonInterface {
+
+        $button->setActive(ArrayHelper::get($args, "active", false));
+        $button->setBlock(ArrayHelper::get($args, "block", false));
+        $button->setContent(ArrayHelper::get($args, "content"));
+        $button->setData(ArrayHelper::get($args, "data", []));
+        $button->setDisabled(ArrayHelper::get($args, "disabled", false));
+        $button->setOutline(ArrayHelper::get($args, "outline", false));
+        $button->setSize(ArrayHelper::get($args, "size"));
+        $button->setTitle(ArrayHelper::get($args, "title"));
+
+        return $button;
+    }
+
+    /**
+     * Parse a danger button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the danger button.
+     */
+    public static function parseDangerButton(array $args): ButtonInterface {
+        return static::parseButton(static::newDangerButton(), $args);
+    }
+
+    /**
+     * Parse a dark button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the dark button.
+     */
+    public static function parseDarkButton(array $args): ButtonInterface {
+        return static::parseButton(static::newDarkButton(), $args);
+    }
+
+    /**
+     * Parse a default button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the default button.
+     */
+    public static function parseDefaultButton(array $args): ButtonInterface {
+        return static::parseButton(static::newDefaultButton(), $args);
+    }
+
+    /**
+     * Parse a info button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the info button.
+     */
+    public static function parseInfoButton(array $args): ButtonInterface {
+        return static::parseButton(static::newInfoButton(), $args);
+    }
+
+    /**
+     * Parse a light button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the light button.
+     */
+    public static function parseLightButton(array $args): ButtonInterface {
+        return static::parseButton(static::newLightButton(), $args);
+    }
+
+    /**
+     * Parse a link button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the link button.
+     */
+    public static function parseLinkButton(array $args): ButtonInterface {
+        return static::parseButton(static::newLinkButton(), $args);
+    }
+
+    /**
+     * Parse a primary button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the primary button.
+     */
+    public static function parsePrimaryButton(array $args): ButtonInterface {
+        return static::parseButton(static::newPrimaryButton(), $args);
+    }
+
+    /**
+     * Parse a secondary button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the secondary button.
+     */
+    public static function parseSecondaryButton(array $args): ButtonInterface {
+        return static::parseButton(static::newSecondaryButton(), $args);
+    }
+
+    /**
+     * Parse a success button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the success button.
+     */
+    public static function parseSuccessButton(array $args): ButtonInterface {
+        return static::parseButton(static::newSuccessButton(), $args);
+    }
+
+    /**
+     * Parse a warning button.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return ButtonInterface Returns the warning button.
+     */
+    public static function parseWarningButton(array $args): ButtonInterface {
+        return static::parseButton(static::newWarningButton(), $args);
+    }
+}
---
+++ lib/bootstrap/Tests/Factory/ButtonFactoryTest.php
@@ -0,0 +1,404 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Button\DangerButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\DarkButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\DefaultButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\InfoButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\LightButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\LinkButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\PrimaryButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\SecondaryButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\SuccessButton;
+use WBW\Bundle\BootstrapBundle\Model\Button\WarningButton;
+use WBW\Bundle\BootstrapBundle\Model\ButtonInterface;
+use WBW\Bundle\BootstrapBundle\Factory\ButtonFactory;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\ButtonInterface as BaseButtonInterface;
+
+/**
+ * Button factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Factory
+ */
+class ButtonFactoryTest extends AbstractTestCase {
+
+    /**
+     * Arguments.
+     *
+     * @var array<string,mixed>|null
+     */
+    private $args;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set an arguments mock.
+        $this->args = [
+            "content"  => "content",
+            "title"    => "title",
+            "size"     => "lg",
+            "block"    => true,
+            "active"   => true,
+            "disabled" => true,
+            "outline"  => true,
+            "data"     => [
+                "id" => 1,
+            ],
+        ];
+    }
+
+    /**
+     * Test newDangerButton()
+     *
+     * @return void
+     */
+    public function testNewDangerButton(): void {
+
+        $obj = ButtonFactory::newDangerButton();
+
+        $this->assertInstanceOf(DangerButton::class, $obj);
+    }
+
+    /**
+     * Test newDarkButton()
+     *
+     * @return void
+     */
+    public function testNewDarkButton(): void {
+
+        $obj = ButtonFactory::newDarkButton();
+
+        $this->assertInstanceOf(DarkButton::class, $obj);
+    }
+
+    /**
+     * Test newDefaultButton()
+     *
+     * @return void
+     */
+    public function testNewDefaultButton(): void {
+
+        $obj = ButtonFactory::newDefaultButton();
+
+        $this->assertInstanceOf(DefaultButton::class, $obj);
+    }
+
+    /**
+     * Test newInfoButton()
+     *
+     * @return void
+     */
+    public function testNewInfoButton(): void {
+
+        $obj = ButtonFactory::newInfoButton();
+
+        $this->assertInstanceOf(InfoButton::class, $obj);
+    }
+
+    /**
+     * Test newLightButton()
+     *
+     * @return void
+     */
+    public function testNewLightButton(): void {
+
+        $obj = ButtonFactory::newLightButton();
+
+        $this->assertInstanceOf(LightButton::class, $obj);
+    }
+
+    /**
+     * Test newLinkButton()
+     *
+     * @return void
+     */
+    public function testNewLinkButton(): void {
+
+        $obj = ButtonFactory::newLinkButton();
+
+        $this->assertInstanceOf(LinkButton::class, $obj);
+    }
+
+    /**
+     * Test newPrimaryButton()
+     *
+     * @return void
+     */
+    public function testNewPrimaryButton(): void {
+
+        $obj = ButtonFactory::newPrimaryButton();
+
+        $this->assertInstanceOf(PrimaryButton::class, $obj);
+    }
+
+    /**
+     * Test newSecondaryButton()
+     *
+     * @return void
+     */
+    public function testNewSecondaryButton(): void {
+
+        $obj = ButtonFactory::newSecondaryButton();
+
+        $this->assertInstanceOf(SecondaryButton::class, $obj);
+    }
+
+    /**
+     * Test newSuccessButton()
+     *
+     * @return void
+     */
+    public function testNewSuccessButton(): void {
+
+        $obj = ButtonFactory::newSuccessButton();
+
+        $this->assertInstanceOf(SuccessButton::class, $obj);
+    }
+
+    /**
+     * Test newWarningButton()
+     *
+     * @return void
+     */
+    public function testNewWarningButton(): void {
+
+        $obj = ButtonFactory::newWarningButton();
+
+        $this->assertInstanceOf(WarningButton::class, $obj);
+    }
+
+    /**
+     * Test parseDangerButton()
+     *
+     * @return void
+     */
+    public function testParseDangerButton(): void {
+
+        $obj = ButtonFactory::parseDangerButton($this->args);
+
+        $this->assertInstanceOf(DangerButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(BaseButtonInterface::BUTTON_TYPE_DANGER, $obj->getType());
+    }
+
+    /**
+     * Test parseDarkButton()
+     *
+     * @return void
+     */
+    public function testParseDarkButton(): void {
+
+        $obj = ButtonFactory::parseDarkButton($this->args);
+
+        $this->assertInstanceOf(DarkButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_DARK, $obj->getType());
+    }
+
+    /**
+     * Test parseDefaultButton()
+     *
+     * @return void
+     */
+    public function testParseDefaultButton(): void {
+
+        $obj = ButtonFactory::parseDefaultButton($this->args);
+
+        $this->assertInstanceOf(DefaultButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_DEFAULT, $obj->getType());
+    }
+
+    /**
+     * Test parseInfoButton()
+     *
+     * @return void
+     */
+    public function testParseInfoButton(): void {
+
+        $obj = ButtonFactory::parseInfoButton($this->args);
+
+        $this->assertInstanceOf(InfoButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(BaseButtonInterface::BUTTON_TYPE_INFO, $obj->getType());
+    }
+
+    /**
+     * Test parseLightButton()
+     *
+     * @return void
+     */
+    public function testParseLightButton(): void {
+
+        $obj = ButtonFactory::parseLightButton($this->args);
+
+        $this->assertInstanceOf(LightButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_LIGHT, $obj->getType());
+    }
+
+    /**
+     * Test parseLinkButton()
+     *
+     * @return void
+     */
+    public function testParseLinkButton(): void {
+
+        $obj = ButtonFactory::parseLinkButton($this->args);
+
+        $this->assertInstanceOf(LinkButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_LINK, $obj->getType());
+    }
+
+    /**
+     * Test parsePrimaryButton()
+     *
+     * @return void
+     */
+    public function testParsePrimaryButton(): void {
+
+        $obj = ButtonFactory::parsePrimaryButton($this->args);
+
+        $this->assertInstanceOf(PrimaryButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_PRIMARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSecondaryButton()
+     *
+     * @return void
+     */
+    public function testParseSecondaryButton(): void {
+
+        $obj = ButtonFactory::parseSecondaryButton($this->args);
+
+        $this->assertInstanceOf(SecondaryButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(ButtonInterface::BUTTON_TYPE_SECONDARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSuccessButton()
+     *
+     * @return void
+     */
+    public function testParseSuccessButton(): void {
+
+        $obj = ButtonFactory::parseSuccessButton($this->args);
+
+        $this->assertInstanceOf(SuccessButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(BaseButtonInterface::BUTTON_TYPE_SUCCESS, $obj->getType());
+    }
+
+    /**
+     * Test parseWarningButton()
+     *
+     * @return void
+     */
+    public function testParseWarningButton(): void {
+
+        $obj = ButtonFactory::parseWarningButton($this->args);
+
+        $this->assertInstanceOf(WarningButton::class, $obj);
+
+        $this->assertEquals($this->args["active"], $obj->getActive());
+        $this->assertEquals($this->args["block"], $obj->getBlock());
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["data"], $obj->getData());
+        $this->assertEquals($this->args["disabled"], $obj->getDisabled());
+        $this->assertEquals($this->args["outline"], $obj->getOutline());
+        $this->assertEquals($this->args["size"], $obj->getSize());
+        $this->assertEquals($this->args["title"], $obj->getTitle());
+        $this->assertEquals(BaseButtonInterface::BUTTON_TYPE_WARNING, $obj->getType());
+    }
+}
