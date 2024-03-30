7d9b998bb051eb0aed1251d18db2d3f9f284d065
webeweb <support@webeweb.fr>
2024-03-30 15:25:18 +0100

    Update Badge Twig extension test: - fix PHPStan warnings

--- lib/bootstrap/Tests/Twig/Extension/Component/BadgeTwigExtensionTest.php
+++ lib/bootstrap/Tests/Twig/Extension/Component/BadgeTwigExtensionTest.php
@@ -37,7 +37,7 @@ class BadgeTwigExtensionTest extends AbstractTestCase {
     /**
      * {@inheritDoc}
      */
-    protected function setUp() {
+    protected function setUp(): void {
         parent::setUp();
 
         // Set a Twig environment mock.
