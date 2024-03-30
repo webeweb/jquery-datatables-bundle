5054c18fa38a81ef439ca9da7a1bc2956560c10b
webeweb <support@webeweb.fr>
2024-03-30 13:34:53 +0100

    Move Serializer keys (from bootstrap-bundle)

---
+++ lib/bootstrap/Serializer/SerializerKeys.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Serializer;
+
+use WBW\Library\Serializer\SerializerKeys as BaseSerializerKeys;
+
+/**
+ * Serializer keys.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Serializer
+ */
+interface SerializerKeys extends BaseSerializerKeys {
+
+    /**
+     * Serializer key "animated".
+     *
+     * @var string
+     */
+    public const ANIMATED = "animated";
+
+    /**
+     * Serializer key "block".
+     *
+     * @var string
+     */
+    public const BLOCK = "block";
+
+    /**
+     * Serializer key "dismissible".
+     *
+     * @var string
+     */
+    public const DISMISSIBLE = "dismissible";
+
+    /**
+     * Serializer key "outline".
+     *
+     * @var string
+     */
+    public const OUTLINE = "outline";
+
+    /**
+     * Serializer key "pill".
+     *
+     * @var string
+     */
+    public const PILL = "pill";
+
+    /**
+     * Serializer key "prefix".
+     *
+     * @var string
+     */
+    public const PREFIX = "prefix";
+
+    /**
+     * Serializer key "striped".
+     *
+     * @var string
+     */
+    public const STRIPED = "striped";
+}
---
+++ lib/bootstrap/Tests/Serializer/SerializerKeysTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Serializer;
+
+use WBW\Bundle\BootstrapBundle\Serializer\SerializerKeys;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Serializer keys test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Serializer
+ */
+class SerializerKeysTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("animated", SerializerKeys::ANIMATED);
+        $this->assertEquals("block", SerializerKeys::BLOCK);
+        $this->assertEquals("dismissible", SerializerKeys::DISMISSIBLE);
+        $this->assertEquals("outline", SerializerKeys::OUTLINE);
+        $this->assertEquals("pill", SerializerKeys::PILL);
+        $this->assertEquals("prefix", SerializerKeys::PREFIX);
+        $this->assertEquals("striped", SerializerKeys::STRIPED);
+    }
+}
