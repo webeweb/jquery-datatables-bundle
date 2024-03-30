2d72e96ec5a814ad4217ad22e2e0d7c6779c8855
webeweb <support@webeweb.fr>
2024-03-30 13:59:48 +0100

    Copy Info badge (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Model/Badge/InfoBadgeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model\Badge;
+
+use WBW\Bundle\BootstrapBundle\Model\Badge\InfoBadge;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\BadgeInterface as BaseBadgeInterface;
+
+/**
+ * Info badge test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model\Badge
+ */
+class InfoBadgeTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new InfoBadge();
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_INFO, $obj->getType());
+    }
+}
