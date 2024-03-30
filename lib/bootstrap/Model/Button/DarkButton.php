b9895fa1153ecdd964347d9231db62382025066d
webeweb <support@webeweb.fr>
2024-03-30 14:05:38 +0100

    Copy Dark button (from bootstrap-bundle)

---
+++ lib/bootstrap/Model/Button/DarkButton.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Model\Button;
+
+use WBW\Bundle\BootstrapBundle\Model\AbstractButton;
+
+/**
+ * Dark button.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Model\Button
+ */
+class DarkButton extends AbstractButton {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::BUTTON_TYPE_DARK);
+    }
+}
