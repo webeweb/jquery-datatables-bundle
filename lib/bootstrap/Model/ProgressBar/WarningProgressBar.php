235712f1df2c82cf67d2c322d78f08431a2768f5
webeweb <support@webeweb.fr>
2024-03-30 14:09:49 +0100

    Copy Warning progress bar (from bootstrap-bundle)

---
+++ lib/bootstrap/Model/ProgressBar/WarningProgressBar.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Model\ProgressBar;
+
+use WBW\Bundle\BootstrapBundle\Model\AbstractProgressBar;
+
+/**
+ * Warning progress bar.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Model\ProgressBar
+ */
+class WarningProgressBar extends AbstractProgressBar {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::PROGRESS_BAR_TYPE_WARNING);
+    }
+}
