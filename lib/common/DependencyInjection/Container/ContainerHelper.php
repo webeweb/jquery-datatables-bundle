7c27bc13aa022b4330f39bc9dc9f1ab48c9692a6
webeweb <support@webeweb.fr>
2024-03-30 11:59:02 +0100

    Copy Container helper (from core-bundle)

---
+++ lib/common/DependencyInjection/Container/ContainerHelper.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\DependencyInjection\Container;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Container helper.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\DependencyInjection\Container
+ */
+class ContainerHelper {
+
+    /**
+     * Set a container parameter.
+     *
+     * @param ContainerInterface $container The container.
+     * @param array<string,mixed> $config The configuration.
+     * @param string $alias The alias.
+     * @param string $key The key.
+     * @param bool $tree Tree ?
+     * @return void
+     */
+    public static function setParameter(ContainerInterface $container, array $config, string $alias, string $key, bool $tree = true): void {
+
+        if (false === array_key_exists($key, $config)) {
+            return;
+        }
+
+        $item = $config[$key];
+        $name = "$alias.$key";
+
+        if (true === $tree || false === is_array($item) || false === ArrayHelper::isObject($item)) {
+            $container->setParameter($name, $item);
+            return;
+        }
+
+        foreach ($item as $k => $v) {
+            static::setParameter($container, $item, $name, $k, $tree);
+        }
+    }
+
+    /**
+     * Set the container parameters.
+     *
+     * @param ContainerInterface $container The container.
+     * @param array<string,mixed> $config The configuration.
+     * @return void
+     */
+    public static function setParameters(ContainerInterface $container, array $config): void {
+
+        foreach ($config as $k => $v) {
+            $container->setParameter($k, $v);
+        }
+    }
+}
---
+++ lib/common/Tests/DependencyInjection/Container/ContainerHelperTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\DependencyInjection\Container;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use WBW\Bundle\CommonBundle\DependencyInjection\Container\ContainerHelper;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Container helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\DependencyInjection\Container
+ */
+class ContainerHelperTest extends AbstractTestCase {
+
+    /**
+     * Test setParameter()
+     *
+     * @return void
+     */
+    public function testSetParameter(): void {
+
+        $arg = [
+            "one" => 1,
+            "two" => [
+                "one" => 1,
+                "two" => 2,
+            ],
+        ];
+
+        $obj = new ContainerBuilder();
+
+        ContainerHelper::setParameter($obj, $arg, "alias", "zero");
+        $this->assertCount(0, $obj->getParameterBag()->all());
+
+        ContainerHelper::setParameter($obj, $arg, "alias", "one");
+        $this->assertCount(1, $obj->getParameterBag()->all());
+
+        $this->assertEquals(1, $obj->getParameter("alias.one"));
+
+        ContainerHelper::setParameter($obj, $arg, "alias", "two", false);
+        $this->assertCount(3, $obj->getParameterBag()->all());
+
+        $this->assertEquals(1, $obj->getParameter("alias.one"));
+        $this->assertEquals(1, $obj->getParameter("alias.two.one"));
+        $this->assertEquals(2, $obj->getParameter("alias.two.two"));
+    }
+
+    /**
+     * Test setParameters()
+     *
+     * @return void
+     */
+    public function testSetParameters(): void {
+
+        $arg = [
+            "one" => 1,
+            "two" => 2,
+        ];
+
+        $obj = new ContainerBuilder();
+
+        ContainerHelper::setParameters($obj, $arg);
+        $this->assertCount(2, $obj->getParameterBag()->all());
+
+        $this->assertEquals(1, $obj->getParameter("one"));
+        $this->assertEquals(2, $obj->getParameter("two"));
+    }
+}
