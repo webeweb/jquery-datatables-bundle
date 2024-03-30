0f17a5863160e0d2e6bcdea12749377f616999ea
webeweb <support@webeweb.fr>
2024-03-30 11:04:23 +0100

    Copy Notification event listener (from core-bundle)

---
+++ lib/common/EventListener/NotificationEventListener.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\EventListener;
+
+use Throwable;
+use WBW\Bundle\CommonBundle\Event\NotificationEvent;
+use WBW\Bundle\CommonBundle\Service\SessionServiceInterface;
+use WBW\Bundle\CommonBundle\Service\SessionServiceTrait;
+
+/**
+ * Notification event listener.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\EventListener
+ */
+class NotificationEventListener {
+
+    use SessionServiceTrait;
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.common.event_listener.notification";
+
+    /**
+     * Constructor.
+     *
+     * @param SessionServiceInterface $sessionService The session service.
+     */
+    public function __construct(SessionServiceInterface $sessionService) {
+        $this->setSessionService($sessionService);
+    }
+
+    /**
+     * On notify.
+     *
+     * @param NotificationEvent $event The event.
+     * @return NotificationEvent Returns the event.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function onNotify(NotificationEvent $event): NotificationEvent {
+        $this->getSessionService()->getSession()->getFlashBag()->add($event->getNotification()->getType(), $event->getNotification()->getContent());
+        return $event;
+    }
+}
---
+++ lib/common/Tests/EventListener/NotificationEventListenerTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Throwable;
+use WBW\Bundle\CommonBundle\Event\NotificationEvent;
+use WBW\Bundle\CommonBundle\EventListener\NotificationEventListener;
+use WBW\Bundle\CommonBundle\Service\SessionServiceInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NotificationInterface;
+
+/**
+ * Notification event listener.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\EventListener
+ */
+class NotificationEventListenerTest extends AbstractTestCase {
+
+    /**
+     * Session service.
+     *
+     * @var SessionServiceInterface|null
+     */
+    private $sessionService;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Session service mock.
+        $this->sessionService = $this->getMockBuilder(SessionServiceInterface::class)->getMock();
+    }
+
+    /**
+     * Test onNotify()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testOnNotify(): void {
+
+        // Set a Session mock.
+        $session = $this->getMockBuilder(Session::class)->getMock();
+        $session->expects($this->any())->method("getFlashBag")->willReturn(new FlashBag());
+
+        // Set the Session service mock.
+        $this->sessionService->expects($this->any())->method("getSession")->willReturn($session);
+
+        // Set a Notification mock.
+        $notification = $this->getMockBuilder(NotificationInterface::class)->getMock();
+        $notification->expects($this->any())->method("getType")->willReturn("type");
+
+        $evt = new NotificationEvent("test", $notification);
+
+        $obj = new NotificationEventListener($this->sessionService);
+
+        $this->assertSame($evt, $obj->onNotify($evt));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.common.event_listener.notification", NotificationEventListener::SERVICE_NAME);
+
+        $obj = new NotificationEventListener($this->sessionService);
+
+        $this->assertSame($this->sessionService, $obj->getSessionService());
+    }
+}
