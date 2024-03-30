6a294501d7ae6b21c52507cae31446c4abaa14b5
webeweb <support@webeweb.fr>
2024-03-30 14:15:21 +0100

    Copy Badge factory (from bootstrap-bundle)

---
+++ lib/bootstrap/Factory/BadgeFactory.php
@@ -0,0 +1,219 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Badge\DangerBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\DarkBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\DefaultBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\InfoBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\LightBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\PrimaryBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\SecondaryBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\SuccessBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\WarningBadge;
+use WBW\Bundle\BootstrapBundle\Model\BadgeInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Badge factory.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Factory
+ */
+class BadgeFactory {
+
+    /**
+     * Create a new danger badge.
+     *
+     * @return BadgeInterface Returns the danger badge.
+     */
+    public static function newDangerBadge(): BadgeInterface {
+        return new DangerBadge();
+    }
+
+    /**
+     * Create a new dark badge.
+     *
+     * @return BadgeInterface Returns the dark badge.
+     */
+    public static function newDarkBadge(): BadgeInterface {
+        return new DarkBadge();
+    }
+
+    /**
+     * Create a new default badge.
+     *
+     * @return BadgeInterface Returns the default badge.
+     */
+    public static function newDefaultBadge(): BadgeInterface {
+        return new DefaultBadge();
+    }
+
+    /**
+     * Create a new info badge.
+     *
+     * @return BadgeInterface Returns the info badge.
+     */
+    public static function newInfoBadge(): BadgeInterface {
+        return new InfoBadge();
+    }
+
+    /**
+     * Create a new light badge.
+     *
+     * @return BadgeInterface Returns the light badge.
+     */
+    public static function newLightBadge(): BadgeInterface {
+        return new LightBadge();
+    }
+
+    /**
+     * Create a new primary badge.
+     *
+     * @return BadgeInterface Returns the primary badge.
+     */
+    public static function newPrimaryBadge(): BadgeInterface {
+        return new PrimaryBadge();
+    }
+
+    /**
+     * Create a new secondary badge.
+     *
+     * @return BadgeInterface Returns the secondary badge.
+     */
+    public static function newSecondaryBadge(): BadgeInterface {
+        return new SecondaryBadge();
+    }
+
+    /**
+     * Create a new success badge.
+     *
+     * @return BadgeInterface Returns the success badge.
+     */
+    public static function newSuccessBadge(): BadgeInterface {
+        return new SuccessBadge();
+    }
+
+    /**
+     * Create a new warning badge.
+     *
+     * @return BadgeInterface Returns the warning badge.
+     */
+    public static function newWarningBadge(): BadgeInterface {
+        return new WarningBadge();
+    }
+
+    /**
+     * Parse a badge.
+     *
+     * @param BadgeInterface $badge The badge.
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the badge.
+     */
+    protected static function parseBadge(BadgeInterface $badge, array $args): BadgeInterface {
+
+        $badge->setContent(ArrayHelper::get($args, "content"));
+        $badge->setPill(ArrayHelper::get($args, "pill", false));
+
+        return $badge;
+    }
+
+    /**
+     * Parse a danger badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the danger badge.
+     */
+    public static function parseDangerBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newDangerBadge(), $args);
+    }
+
+    /**
+     * Parse a dark badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the dark badge.
+     */
+    public static function parseDarkBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newDarkBadge(), $args);
+    }
+
+    /**
+     * Parse a default badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the default badge.
+     */
+    public static function parseDefaultBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newDefaultBadge(), $args);
+    }
+
+    /**
+     * Parse an info badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the info badge.
+     */
+    public static function parseInfoBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newInfoBadge(), $args);
+    }
+
+    /**
+     * Parse a light badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the light badge.
+     */
+    public static function parseLightBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newLightBadge(), $args);
+    }
+
+    /**
+     * Parse a primary badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the primary badge.
+     */
+    public static function parsePrimaryBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newPrimaryBadge(), $args);
+    }
+
+    /**
+     * Parse a secondary badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the secondary badge.
+     */
+    public static function parseSecondaryBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newSecondaryBadge(), $args);
+    }
+
+    /**
+     * Parse a success badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the success badge.
+     */
+    public static function parseSuccessBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newSuccessBadge(), $args);
+    }
+
+    /**
+     * Parse a warning badge.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return BadgeInterface Returns the warning badge.
+     */
+    public static function parseWarningBadge(array $args): BadgeInterface {
+        return static::parseBadge(static::newWarningBadge(), $args);
+    }
+}
---
+++ lib/bootstrap/Tests/Factory/BadgeFactoryTest.php
@@ -0,0 +1,316 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Factory;
+
+use WBW\Bundle\BootstrapBundle\Model\Badge\DangerBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\DarkBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\DefaultBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\InfoBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\LightBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\PrimaryBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\SecondaryBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\SuccessBadge;
+use WBW\Bundle\BootstrapBundle\Model\Badge\WarningBadge;
+use WBW\Bundle\BootstrapBundle\Model\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Factory\BadgeFactory;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\BadgeInterface as BaseBadgeInterface;
+
+/**
+ * Badge factory test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Factory
+ */
+class BadgeFactoryTest extends AbstractTestCase {
+
+    /**
+     * Arguments.
+     *
+     * @var array<string,mixed>|null
+     */
+    private $args;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set an arguments mock.
+        $this->args = [
+            "content" => "content",
+            "pill"    => true,
+        ];
+    }
+
+    /**
+     * Test newDangerBadge()
+     *
+     * @return void
+     */
+    public function testNewDangerBadge(): void {
+
+        $obj = BadgeFactory::newDangerBadge();
+
+        $this->assertInstanceOf(DangerBadge::class, $obj);
+    }
+
+    /**
+     * Test newDarkBadge()
+     *
+     * @return void
+     */
+    public function testNewDarkBadge(): void {
+
+        $obj = BadgeFactory::newDarkBadge();
+
+        $this->assertInstanceOf(DarkBadge::class, $obj);
+    }
+
+    /**
+     * Test newDefaultBadge()
+     *
+     * @return void
+     */
+    public function testNewDefaultBadge(): void {
+
+        $obj = BadgeFactory::newDefaultBadge();
+
+        $this->assertInstanceOf(DefaultBadge::class, $obj);
+    }
+
+    /**
+     * Test newInfoBadge()
+     *
+     * @return void
+     */
+    public function testNewInfoBadge(): void {
+
+        $obj = BadgeFactory::newInfoBadge();
+
+        $this->assertInstanceOf(InfoBadge::class, $obj);
+    }
+
+    /**
+     * Test newLightBadge()
+     *
+     * @return void
+     */
+    public function testNewLightBadge(): void {
+
+        $obj = BadgeFactory::newLightBadge();
+
+        $this->assertInstanceOf(LightBadge::class, $obj);
+    }
+
+    /**
+     * Test newPrimaryBadge()
+     *
+     * @return void
+     */
+    public function testNewPrimaryBadge(): void {
+
+        $obj = BadgeFactory::newPrimaryBadge();
+
+        $this->assertInstanceOf(PrimaryBadge::class, $obj);
+    }
+
+    /**
+     * Test newSecondaryBadge()
+     *
+     * @return void
+     */
+    public function testNewSecondaryBadge(): void {
+
+        $obj = BadgeFactory::newSecondaryBadge();
+
+        $this->assertInstanceOf(SecondaryBadge::class, $obj);
+    }
+
+    /**
+     * Test newSuccessBadge()
+     *
+     * @return void
+     */
+    public function testNewSuccessBadge(): void {
+
+        $obj = BadgeFactory::newSuccessBadge();
+
+        $this->assertInstanceOf(SuccessBadge::class, $obj);
+    }
+
+    /**
+     * Test newWarningBadge()
+     *
+     * @return void
+     */
+    public function testNewWarningBadge(): void {
+
+        $obj = BadgeFactory::newWarningBadge();
+
+        $this->assertInstanceOf(WarningBadge::class, $obj);
+    }
+
+    /**
+     * Test parseDangerBadge()
+     *
+     * @return void
+     */
+    public function testParseDangerBadge(): void {
+
+        $obj = BadgeFactory::parseDangerBadge($this->args);
+
+        $this->assertInstanceOf(DangerBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_DANGER, $obj->getType());
+    }
+
+    /**
+     * Test parseDarkBadge()
+     *
+     * @return void
+     */
+    public function testParseDarkBadge(): void {
+
+        $obj = BadgeFactory::parseDarkBadge($this->args);
+
+        $this->assertInstanceOf(DarkBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_DARK, $obj->getType());
+    }
+
+    /**
+     * Test parseDefaultBadge()
+     *
+     * @return void
+     */
+    public function testParseDefaultBadge(): void {
+
+        $obj = BadgeFactory::parseDefaultBadge($this->args);
+
+        $this->assertInstanceOf(DefaultBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertNull($obj->getType());
+    }
+
+    /**
+     * Test parseInfoBadge()
+     *
+     * @return void
+     */
+    public function testParseInfoBadge(): void {
+
+        $obj = BadgeFactory::parseInfoBadge($this->args);
+
+        $this->assertInstanceOf(InfoBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_INFO, $obj->getType());
+    }
+
+    /**
+     * Test parseLightBadge()
+     *
+     * @return void
+     */
+    public function testParseLightBadge(): void {
+
+        $obj = BadgeFactory::parseLightBadge($this->args);
+
+        $this->assertInstanceOf(LightBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_LIGHT, $obj->getType());
+    }
+
+    /**
+     * Test parsePrimaryBadge()
+     *
+     * @return void
+     */
+    public function testParsePrimaryBadge(): void {
+
+        $obj = BadgeFactory::parsePrimaryBadge($this->args);
+
+        $this->assertInstanceOf(PrimaryBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_PRIMARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSecondaryBadge()
+     *
+     * @return void
+     */
+    public function testParseSecondaryBadge(): void {
+
+        $obj = BadgeFactory::parseSecondaryBadge($this->args);
+
+        $this->assertInstanceOf(SecondaryBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BadgeInterface::BADGE_TYPE_SECONDARY, $obj->getType());
+    }
+
+    /**
+     * Test parseSuccessBadge()
+     *
+     * @return void
+     */
+    public function testParseSuccessBadge(): void {
+
+        $obj = BadgeFactory::parseSuccessBadge($this->args);
+
+        $this->assertInstanceOf(SuccessBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_SUCCESS, $obj->getType());
+    }
+
+    /**
+     * Test parseWarningBadge()
+     *
+     * @return void
+     */
+    public function testParseWarningBadge(): void {
+
+        $obj = BadgeFactory::parseWarningBadge($this->args);
+
+        $this->assertInstanceOf(WarningBadge::class, $obj);
+
+        $this->assertEquals($this->args["content"], $obj->getContent());
+        $this->assertEquals($this->args["pill"], $obj->getPill());
+
+        $this->assertEquals(BaseBadgeInterface::BADGE_TYPE_WARNING, $obj->getType());
+    }
+}
