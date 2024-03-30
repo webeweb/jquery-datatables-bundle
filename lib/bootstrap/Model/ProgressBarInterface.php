5a1dad96e7c79b87699ef34daf2e3e6fef97da54
webeweb <support@webeweb.fr>
2024-03-30 12:58:43 +0100

    Move Progress bar interface (from bootstrap-bundle)

---
+++ lib/bootstrap/Model/ProgressBarInterface.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Model;
+
+use WBW\Library\Symfony\Assets\ProgressBarInterface as BaseProgressBarInterface;
+
+/**
+ * Progress bar interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Model
+ */
+interface ProgressBarInterface extends BaseProgressBarInterface {
+
+    /**
+     * Get the animated.
+     *
+     * @return bool|null Returns the animated.
+     */
+    public function getAnimated(): ?bool;
+
+    /**
+     * Get the height.
+     *
+     * @return int|null Returns the height.
+     */
+    public function getHeight(): ?int;
+
+    /**
+     * Get the max.
+     *
+     * @return int|null Returns the max.
+     */
+    public function getMax(): ?int;
+
+    /**
+     * Get the min.
+     *
+     * @return int|null Returns the min.
+     */
+    public function getMin(): ?int;
+
+    /**
+     * Get the prefix.
+     *
+     * @return string|null Returns the prefix.
+     */
+    public function getPrefix(): ?string;
+
+    /**
+     * Get the striped.
+     *
+     * @return bool|null Returns the striped.
+     */
+    public function getStriped(): ?bool;
+
+    /**
+     * Get the value.
+     *
+     * @return int|null Returns the value.
+     */
+    public function getValue(): ?int;
+
+    /**
+     * Set the animated.
+     *
+     * @param bool|null $animated The animated.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setAnimated(?bool $animated): ProgressBarInterface;
+
+    /**
+     * Set the height.
+     *
+     * @param int|null $height The height.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setHeight(?int $height);
+
+    /**
+     * Set the max.
+     *
+     * @param int|null $max The max.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setMax(?int $max);
+
+    /**
+     * Set the min.
+     *
+     * @param int|null $min The min.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setMin(?int $min);
+
+    /**
+     * Set the striped.
+     *
+     * @param bool|null $striped The striped.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setStriped(?bool $striped): ProgressBarInterface;
+
+    /**
+     * Set the value.
+     *
+     * @param int|null $value The value.
+     * @return ProgressBarInterface Returns this progress bar.
+     */
+    public function setValue(?int $value);
+}
---
+++ lib/bootstrap/Tests/Model/ProgressBarInterfaceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Model;
+
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\WBWBootstrapBundle;
+use WBW\Library\Symfony\Assets\ProgressBarInterface as BaseProgressBarInterface;
+
+/**
+ * Progress bar interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Model
+ */
+class ProgressBarInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals(WBWBootstrapBundle::BOOTSTRAP_TYPE_DANGER, BaseProgressBarInterface::PROGRESS_BAR_TYPE_DANGER);
+        $this->assertEquals(WBWBootstrapBundle::BOOTSTRAP_TYPE_INFO, BaseProgressBarInterface::PROGRESS_BAR_TYPE_INFO);
+        $this->assertEquals(WBWBootstrapBundle::BOOTSTRAP_TYPE_SUCCESS, BaseProgressBarInterface::PROGRESS_BAR_TYPE_SUCCESS);
+        $this->assertEquals(WBWBootstrapBundle::BOOTSTRAP_TYPE_WARNING, BaseProgressBarInterface::PROGRESS_BAR_TYPE_WARNING);
+    }
+}
