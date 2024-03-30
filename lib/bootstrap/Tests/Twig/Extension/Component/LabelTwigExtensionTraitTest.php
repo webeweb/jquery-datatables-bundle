f33fbe9c8022b2beb7e54733ec1581796fba2b66
webeweb <support@webeweb.fr>
2024-03-30 16:00:45 +0100

    Copy Label Twig extension trait (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/Component/LabelTwigExtensionTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component;
+
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Twig\Extension\Component\TestLabelTwigExtensionTrait;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelTwigExtension;
+
+/**
+ * Label Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Component
+ */
+class LabelTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setLabelTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetLabelTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set a Label Twig extension mock.
+        $labelTwigExtension = new LabelTwigExtension($twigEnvironment);
+
+        $obj = new TestLabelTwigExtensionTrait();
+
+        $obj->setLabelTwigExtension($labelTwigExtension);
+        $this->assertSame($labelTwigExtension, $obj->getLabelTwigExtension());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Component/LabelTwigExtensionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Component;
+
+/**
+ * Label Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Component
+ */
+trait LabelTwigExtensionTrait {
+
+    /**
+     * Label Twig extension.
+     *
+     * @var LabelTwigExtension|null
+     */
+    private $labelTwigExtension;
+
+    /**
+     * Get the label Twig extension.
+     *
+     * @return LabelTwigExtension|null Returns the label Twig extension.
+     */
+    public function getLabelTwigExtension(): ?LabelTwigExtension {
+        return $this->labelTwigExtension;
+    }
+
+    /**
+     * Set the label Twig extension.
+     *
+     * @param LabelTwigExtension|null $labelTwigExtension The label Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setLabelTwigExtension(?LabelTwigExtension $labelTwigExtension): self {
+        $this->labelTwigExtension = $labelTwigExtension;
+        return $this;
+    }
+}
