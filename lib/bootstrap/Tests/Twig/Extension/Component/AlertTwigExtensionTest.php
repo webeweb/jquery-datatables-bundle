ff95249bffcb7251356361e60e5bd5ae9a0475c2
webeweb <support@webeweb.fr>
2024-03-30 15:25:10 +0100

    Update Alert Twig extension test: - fix PHPStan warnings

--- lib/bootstrap/Tests/Twig/Extension/Component/AlertTwigExtensionTest.php
+++ lib/bootstrap/Tests/Twig/Extension/Component/AlertTwigExtensionTest.php
@@ -36,7 +36,7 @@ class AlertTwigExtensionTest extends AbstractTestCase {
     /**
      * {@inheritDoc}
      */
-    protected function setUp() {
+    protected function setUp(): void {
         parent::setUp();
 
         // Set a Twig environment mock.
