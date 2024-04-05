d5b65605b3d5573fefb88437b77771f5a9a7bcba
webeweb <support@webeweb.fr>
2024-04-05 15:38:32 +0200

    Copy User helper (from core-bundle)

---
+++ lib/common/Security/Core/User/UserHelper.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Security\Core\User;
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User helper.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Security\Core\User
+ */
+class UserHelper {
+
+    /**
+     * Get the identifier.
+     *
+     * @param UserInterface $user The user.
+     * @return string|null Returns the identifier.
+     */
+    public static function getIdentifier(UserInterface $user): ?string {
+
+        // TODO: Remove when dropping support for Symfony 5
+        if (Kernel::MAJOR_VERSION < 6) {
+            return $user->getUsername();
+        }
+
+        return $user->getUserIdentifier();
+    }
+
+    /**
+     * Determine if the connected user entity has roles.
+     *
+     * @param UserInterface|null $user The user.
+     * @param array<mixed>|string|null $roles The role or roles.
+     * @param bool $or OR ? If true, matches a role cause a break and the method returns true.
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public static function hasRoles(?UserInterface $user, $roles, bool $or = true): bool {
+
+        if (null === $user || null === $roles) {
+            return false;
+        }
+
+        if (true === is_string($roles)) {
+            $roles = [$roles];
+        }
+
+        $result = 1 <= count($roles);
+
+        foreach ($roles as $role) {
+
+            $buffer = in_array($role, $user->getRoles());
+
+            if (true === $buffer && true === $or) {
+                $result = $buffer;
+                break;
+            }
+
+            $result = $result && $buffer;
+        }
+
+        return $result;
+    }
+}
---
+++ lib/common/Tests/Security/Core/User/UserHelperTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Security\Core\User;
+
+use WBW\Bundle\CommonBundle\Model\User;
+use WBW\Bundle\CommonBundle\Security\Core\User\UserHelper;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Model\TestUser;
+
+/**
+ * User helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Security\Core\User
+ */
+class UserHelperTest extends AbstractTestCase {
+
+    /**
+     * Test getIdentifier()
+     *
+     * @return void
+     */
+    public function testGetIdentifier(): void {
+
+        // Set a User mock.
+        $user = new User("username");
+
+        $this->assertEquals("username", UserHelper::getIdentifier($user));
+    }
+
+    /**
+     * Test hasRoles()
+     *
+     * @return void
+     */
+    public function testHasRoles(): void {
+
+        // Set a User mock.
+        $user = new User("github", "github", ["ROLE_USER"]);
+
+        $this->assertFalse(UserHelper::hasRoles(null, null));
+        $this->assertFalse(UserHelper::hasRoles(null, ""));
+
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_SUPER_ADMIN"));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_SUPER_ADMIN", false));
+
+        $this->assertTrue(UserHelper::hasRoles($user, "ROLE_USER"));
+        $this->assertTrue(UserHelper::hasRoles($user, "ROLE_USER", false));
+
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_GITHUB"));
+        $this->assertFalse(UserHelper::hasRoles($user, "ROLE_GITHUB", false));
+
+        $this->assertTrue(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_USER"]));
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_USER"], false));
+
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"]));
+        $this->assertFalse(UserHelper::hasRoles($user, ["ROLE_SUPER_ADMIN", "ROLE_GITHUB"], false));
+    }
+}
