34f056ebb8ad6024d2f18ae08f28ef068d1c2149
webeweb <support@webeweb.fr>
2024-04-05 16:21:55 +0200

    Copy Assets Twig extension (from boostrap-bundle)

---
+++ lib/bootstrap/Tests/Twig/Extension/AssetsTwigExtensionTest.php
@@ -0,0 +1,212 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension;
+
+use Twig\Environment;
+use Twig\Extension\ExtensionInterface;
+use Twig\Node\Node;
+use Twig\TwigFunction;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\AssetsTwigExtension;
+
+/**
+ * Assets Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension
+ */
+class AssetsTwigExtensionTest extends AbstractTestCase {
+
+    /**
+     * Twig environment.
+     *
+     * @var Environment|null
+     */
+    private $twigEnvironment;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Twig environment mock.
+        $this->twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * Test bootstrapRenderIconFunction()
+     *
+     * @return void
+     */
+    public function testBootstrapRenderIconRender(): void {
+
+        $obj = new AssetsTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->bootstrapRenderIconFunction(null));
+        $this->assertNull($obj->bootstrapRenderIconFunction("::"));
+    }
+
+    /**
+     * Test getFilters()
+     *
+     * @return void
+     */
+    public function testGetFilters(): void {
+
+        $obj = new AssetsTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFilters();
+        $this->assertCount(0, $res);
+    }
+
+    /**
+     * Test getFunctions()
+     *
+     * @return void
+     */
+    public function testGetFunctions(): void {
+
+        $obj = new AssetsTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(2, $res);
+
+        $i = -1;
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("bootstrapRenderIcon", $res[$i]->getName());
+        $this->assertEquals([$obj, "bootstrapRenderIconFunction"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("bsRenderIcon", $res[$i]->getName());
+        $this->assertEquals([$obj, "bootstrapRenderIconFunction"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIcon(): void {
+
+        $this->assertNull(AssetsTwigExtension::renderIcon($this->twigEnvironment, "::"));
+        $this->assertNull(AssetsTwigExtension::renderIcon($this->twigEnvironment, null));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithBootstrapIcon(): void {
+
+        $res = '<i class="bi bi-house"></i>';
+
+        $this->assertEquals($res, AssetsTwigExtension::renderIcon($this->twigEnvironment, "bi:house"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithDefault(): void {
+
+        $res = '<span class="glyphicon glyphicon-home" aria-hidden="true"></span>';
+
+        $this->assertEquals($res, AssetsTwigExtension::renderIcon($this->twigEnvironment, "home"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithFontAwesome(): void {
+
+        $this->assertEquals('<i class="fa fa-home"></i>', AssetsTwigExtension::renderIcon($this->twigEnvironment, "fa:home"));
+        $this->assertEquals('<i class="fas fa-home"></i>', AssetsTwigExtension::renderIcon($this->twigEnvironment, "fas:home"));
+        $this->assertEquals('<i class="far fa-home"></i>', AssetsTwigExtension::renderIcon($this->twigEnvironment, "far:home"));
+        $this->assertEquals('<i class="fal fa-home"></i>', AssetsTwigExtension::renderIcon($this->twigEnvironment, "fal:home"));
+        $this->assertEquals('<i class="fad fa-home"></i>', AssetsTwigExtension::renderIcon($this->twigEnvironment, "fad:home"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithGlyphicon(): void {
+
+        $res = '<span class="glyphicon glyphicon-home" aria-hidden="true"></span>';
+
+        $this->assertEquals($res, AssetsTwigExtension::renderIcon($this->twigEnvironment, "b:home"));
+        $this->assertEquals($res, AssetsTwigExtension::renderIcon($this->twigEnvironment, "g:home"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithMaterialDesignIconicFont(): void {
+
+        $exp = '<i class="zmdi zmdi-home"></i>';
+
+        $this->assertEquals($exp, AssetsTwigExtension::renderIcon($this->twigEnvironment, "zmdi:home"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithMeteocons(): void {
+
+        $exp = '<i class="meteocons" data-meteocons="A"></i>';
+
+        $this->assertEquals($exp, AssetsTwigExtension::renderIcon($this->twigEnvironment, "mc:A"));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIconWithOther(): void {
+
+        $res = '<i class="fa fa-home"></i>';
+
+        $this->assertEquals($res, AssetsTwigExtension::renderIcon($this->twigEnvironment, "fa:home"));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.bootstrap.twig.extension.assets", AssetsTwigExtension::SERVICE_NAME);
+
+        $obj = new AssetsTwigExtension($this->twigEnvironment);
+
+        $this->assertInstanceOf(ExtensionInterface::class, $obj);
+
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/AssetsTwigExtension.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension;
+
+use Twig\Environment;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\FontAwesomeTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\GlyphiconTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\IconTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\MaterialDesignIconicFontTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\MeteoconsTwigExtension;
+
+/**
+ * Assets Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension
+ */
+class AssetsTwigExtension extends AbstractTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.bootstrap.twig.extension.assets";
+
+    /**
+     * Display an icon.
+     *
+     * @param string|null $name The name.
+     * @param string|null $style The style.
+     * @return string|null Returns the icon.
+     */
+    public function bootstrapRenderIconFunction(?string $name, string $style = null): ?string {
+        return static::renderIcon($this->getTwigEnvironment(), $name, $style);
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return TwigFilter[] Returns the Twig filters.
+     */
+    public function getFilters(): array {
+        return [];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return TwigFunction[] Returns the Twig functions.
+     */
+    public function getFunctions(): array {
+
+        return [
+            new TwigFunction("bootstrapRenderIcon", [$this, "bootstrapRenderIconFunction"], ["is_safe" => ["html"]]),
+            new TwigFunction("bsRenderIcon", [$this, "bootstrapRenderIconFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Render an icon.
+     *
+     * @param Environment $twigEnvironment The Twig environment.
+     * @param string|null $name The name.
+     * @param string|null $style The style.
+     * @return string|null Returns the rendered icon.
+     */
+    public static function renderIcon(Environment $twigEnvironment, ?string $name, string $style = null): ?string {
+
+        if (null === $name) {
+            return null;
+        }
+
+        $handler = explode(":", $name);
+        if (1 === count($handler)) {
+            array_unshift($handler, "g");
+        }
+        if (2 !== count($handler)) {
+            return null;
+        }
+
+        switch ($handler[0]) {
+
+            default:
+            case "b": // Bootstrap
+            case "g": // Glyphicon
+                $output = (new GlyphiconTwigExtension($twigEnvironment))->renderIcon($handler[1], $style);
+                break;
+
+            case "bi": // Bootstrap icons
+                $output = (new IconTwigExtension($twigEnvironment))->renderIcon($handler[1], $style);
+                break;
+
+            case "fa": // Font Awesome
+            case "fas":
+            case "far":
+            case "fal":
+            case "fab":
+            case "fad":
+                $output = (new FontAwesomeTwigExtension($twigEnvironment))->renderIcon($handler[1], $style);
+                $output = str_replace('class="fa', 'class="' . $handler[0], $output);
+                break;
+
+            case "mc": // Meteocons
+                $output = (new MeteoconsTwigExtension($twigEnvironment))->renderIcon($handler[1], $style);
+                break;
+
+            case "zmdi": // Material Design Iconic Font
+                $output = (new MaterialDesignIconicFontTwigExtension($twigEnvironment))->renderIcon($handler[1], $style);
+                break;
+        }
+
+        return $output;
+    }
+}
