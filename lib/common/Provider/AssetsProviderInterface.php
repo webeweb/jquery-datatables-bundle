d0f972ae9387c4fa02593abcb590469ce923c3fd
webeweb <support@webeweb.fr>
2024-04-05 08:32:48 +0200

    Copy Assets provider interface (from core-bundle)

---
+++ lib/common/Provider/AssetsProviderInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider;
+
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use WBW\Library\Symfony\Provider\ProviderInterface;
+
+/**
+ * Assets provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider
+ */
+interface AssetsProviderInterface extends ProviderInterface, BundleInterface {
+
+    /**
+     * Asset relative directory.
+     *
+     * @var string
+     */
+    const ASSETS_RELATIVE_DIRECTORY = "/Resources/assets";
+
+    /**
+     * Get the assets relative directory.
+     *
+     * @return string Returns the assets relative directory.
+     */
+    public function getAssetsRelativeDirectory(): string;
+}
