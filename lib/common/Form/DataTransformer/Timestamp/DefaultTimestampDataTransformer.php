9ccc6f889b9d2d437ec15a40cf06bea93fb09736
webeweb <support@webeweb.fr>
2024-04-05 14:17:13 +0200

    Copy Default timestamp data transformer (from core-bundle)

---
+++ lib/common/Form/DataTransformer/Timestamp/DefaultTimestampDataTransformer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Form\DataTransformer\Timestamp;
+
+use WBW\Bundle\CommonBundle\Form\DataTransformer\AbstractTimestampDataTransformer;
+
+/**
+ * Default timestamp data transformer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Form\DataTransformer\Timestamp
+ */
+class DefaultTimestampDataTransformer extends AbstractTimestampDataTransformer {
+
+    /**
+     * Constructor.
+     *
+     * @param string $format The format.
+     * @param string|null $timezone The timezone.
+     */
+    public function __construct(string $format = "Y-m-d H:i:s", ?string $timezone = "UTC") {
+        parent::__construct($format, $timezone);
+    }
+}
