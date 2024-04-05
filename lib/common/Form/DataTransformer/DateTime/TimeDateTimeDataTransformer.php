e8e72be9913d80e542426fb9bc20c7f4c14e42ff
webeweb <support@webeweb.fr>
2024-04-05 14:16:54 +0200

    Copy Time date/time data transformer (from core-bundle)

---
+++ lib/common/Form/DataTransformer/DateTime/TimeDateTimeDataTransformer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Form\DataTransformer\DateTime;
+
+use WBW\Bundle\CommonBundle\Form\DataTransformer\AbstractDateTimeDataTransformer;
+
+/**
+ * Time date/time data transformer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Form\DataTransformer\DateTime
+ */
+class TimeDateTimeDataTransformer extends AbstractDateTimeDataTransformer {
+
+    /**
+     * Constructor.
+     *
+     * @param string $format The format.
+     * @param string|null $timezone The timezone.
+     */
+    public function __construct(string $format = "H:i:s", ?string $timezone = "UTC") {
+        parent::__construct($format, $timezone);
+    }
+}
---
+++ lib/common/Tests/Form/DataTransformer/DateTime/TimeDateTimeDataTransformerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Form\DataTransformer\DateTime;
+
+use WBW\Bundle\CommonBundle\Form\DataTransformer\DateTime\TimeDateTimeDataTransformer;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Time date/time data transformer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Form\DataTransformer\DateTime
+ */
+class TimeDateTimeDataTransformerTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new TimeDateTimeDataTransformer();
+
+        $this->assertEquals("H:i:s", $obj->getFormat());
+        $this->assertEquals("UTC", $obj->getTimezone());
+    }
+}
