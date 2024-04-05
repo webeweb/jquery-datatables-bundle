850bb34ba950a698e22dcea3002d7c9719f14047
webeweb <support@webeweb.fr>
2024-04-05 13:31:26 +0200

    Copy Console helper (from core-bundle)

---
+++ lib/common/Command/CommandHelper.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use WBW\Library\System\Helper\SystemHelper;
+
+/**
+ * Command helper.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Command
+ */
+class CommandHelper {
+
+    /**
+     * Format a checkbox.
+     *
+     * @param bool|null $checked Checked ?
+     * @return string Returns the checkbox.
+     */
+    public static function formatCheckbox(?bool $checked): string {
+
+        if (true === $checked) {
+            return sprintf("<fg=green;options=bold>%s</>", SystemHelper::isWindows() ? "OK" : "\xE2\x9C\x94");
+        }
+
+        return sprintf("<fg=yellow;options=bold>%s</>", SystemHelper::isWindows() ? "KO" : "!");
+    }
+
+    /**
+     * Format a help.
+     *
+     * @param string $content The content
+     * @return string
+     */
+    public static function formatHelp(string $content): string {
+
+        $template = <<< EOT
+The <info>%command.name%</info> command {{ content }}.
+
+    <info>php %command.full_name%</info>
+
+
+EOT;
+
+        return str_replace("{{ content }}", $content, $template);
+    }
+
+    /**
+     * Create a Symfony style.
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return StyleInterface Returns the Symfony style.
+     */
+    public static function newSymfonyStyle(InputInterface $input, OutputInterface $output): StyleInterface {
+        return new SymfonyStyle($input, $output);
+    }
+}
---
+++ lib/common/Tests/Command/CommandHelperTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Command;
+
+use Symfony\Component\Console\Formatter\OutputFormatterInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use WBW\Bundle\CommonBundle\Command\CommandHelper;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Command helper test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Command
+ */
+class CommandHelperTest extends AbstractTestCase {
+
+    /**
+     * Test formatCheckbox()
+     *
+     * @return void
+     */
+    public function testFormatCheckbox(): void {
+
+        // Determines the operating system.
+        if ("\\" !== DIRECTORY_SEPARATOR) {
+
+            $this->assertEquals("<fg=green;options=bold>\xE2\x9C\x94</>", CommandHelper::formatCheckbox(true));
+            $this->assertEquals("<fg=yellow;options=bold>!</>", CommandHelper::formatCheckbox(false));
+            $this->assertEquals("<fg=yellow;options=bold>!</>", CommandHelper::formatCheckbox(null));
+        } else {
+
+            $this->assertEquals("<fg=green;options=bold>OK</>", CommandHelper::formatCheckbox(true));
+            $this->assertEquals("<fg=yellow;options=bold>WARNING</>", CommandHelper::formatCheckbox(false));
+            $this->assertEquals("<fg=yellow;options=bold>WARNING</>", CommandHelper::formatCheckbox(null));
+        }
+    }
+
+    /**
+     * Test formatHelp()
+     *
+     * @return void
+     */
+    public function testFormatHelp(): void {
+
+        $exp = <<< EOT
+The <info>%command.name%</info> command content.
+
+    <info>php %command.full_name%</info>
+
+
+EOT;
+
+        $this->assertEquals($exp, CommandHelper::formatHelp("content"));
+    }
+
+    /**
+     * Test newSymfonyStyle()
+     *
+     * @return void
+     */
+    public function testNewSymfonyStyle(): void {
+
+        // Set an Output formatter mock.
+        $outputFormatter = $this->getMockBuilder(OutputFormatterInterface::class)->getMock();
+
+        // Set an Input mock.
+        $input = $this->getMockBuilder(InputInterface::class)->getMock();
+
+        // Set an Output mock.
+        $output = $this->getMockBuilder(OutputInterface::class)->getMock();
+        $output->expects($this->any())->method("getFormatter")->willReturn($outputFormatter);
+
+        $res = CommandHelper::newSymfonyStyle($input, $output);
+        $this->assertInstanceOf(StyleInterface::class, $res);
+    }
+}
