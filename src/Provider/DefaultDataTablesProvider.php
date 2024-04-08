edd22f7ffe0cd63f52751853295cdd94c521cf0d
webeweb <support@webeweb.fr>
2024-04-08 15:35:48 +0200

    Copy abstract DataTables provider

---
+++ src/Provider/DefaultDataTablesProvider.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Provider;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+use WBW\Bundle\CommonBundle\Translation\TranslatorTrait;
+use WBW\Bundle\DataTablesBundle\Factory\DataTablesFactory;
+use WBW\Bundle\DataTablesBundle\Helper\DataTablesEntityHelper;
+use WBW\Bundle\DataTablesBundle\Helper\DataTablesWrapperHelper;
+use WBW\Bundle\DataTablesBundle\Model\DataTablesOptionsInterface;
+use WBW\Bundle\DataTablesBundle\Model\DataTablesResponseInterface;
+
+/**
+ * Default DataTables provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Provider
+ * @abstract
+ */
+abstract class DefaultDataTablesProvider implements DataTablesProviderInterface {
+
+    use TranslatorTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param TranslatorInterface $translator The translator.
+     */
+    public function __construct(TranslatorInterface $translator) {
+        $this->setTranslator($translator);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCsvExporter(): ?DataTablesCsvExporterInterface {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getEditor(): ?DataTablesEditorInterface {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMethod(): ?string {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOptions(): DataTablesOptionsInterface {
+
+        $url = ["url" => DataTablesWrapperHelper::getLanguageURL("French")];
+
+        $dtOptions = DataTablesFactory::newOptions();
+        $dtOptions->addOption("language", $url);
+        $dtOptions->addOption("order", [[0, "asc"]]);
+        $dtOptions->addOption("responsive", true);
+        $dtOptions->addOption("search", ["return" => true]);
+
+        $dtOptions->addOption("columnDefs", [
+            [
+                "responsivePriority" => 1,
+                "targets"            => -1,
+            ],
+        ]);
+
+        return $dtOptions;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function renderRow(string $dtRow, $entity, int $rowNumber) {
+
+        DataTablesEntityHelper::isCompatible($entity, true);
+
+        switch ($dtRow) {
+
+            case DataTablesResponseInterface::DATATABLES_ROW_DATA:
+                return [
+                    "id"   => $entity->getId(),
+                    "name" => $this->getName(),
+                ];
+
+            case DataTablesResponseInterface::DATATABLES_ROW_ID:
+                return implode("_", [$this->getName(), $entity->getId()]);
+        }
+
+        return null;
+    }
+}
---
+++ tests/Provider/DefaultDataTablesProviderTest.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Tests\Provider;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+use WBW\Bundle\DataTablesBundle\Model\DataTablesOptionsInterface;
+use WBW\Bundle\DataTablesBundle\Model\DataTablesResponseInterface;
+use WBW\Bundle\DataTablesBundle\Tests\AbstractTestCase;
+use WBW\Bundle\DataTablesBundle\Tests\Fixtures\Entity\Employee;
+use WBW\Bundle\DataTablesBundle\Tests\Fixtures\Provider\TestDefaultDataTablesProvider;
+
+/**
+ * Default DataTables provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Tests\Provider
+ */
+class DefaultDataTablesProviderTest extends AbstractTestCase {
+
+    /**
+     * DataTable provider.
+     *
+     * @var TestDefaultDataTablesProvider
+     */
+    private $dtProvider;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Translator mock.
+        $translator = $this->getMockBuilder(TranslatorInterface::class)->getMock();
+
+        // Set a DataTables provider mock.
+        $this->dtProvider = new TestDefaultDataTablesProvider($translator);
+    }
+
+    /**
+     * Test getCsvExporter()
+     *
+     * @return void
+     */
+    public function testGetCsvExporter(): void {
+
+        $obj = $this->dtProvider;
+
+        $this->assertNull($obj->getCsvExporter());
+    }
+
+    /**
+     * Test getEditor()
+     *
+     * @return void
+     */
+    public function testGetEditor(): void {
+
+        $obj = $this->dtProvider;
+
+        $this->assertNull($obj->getEditor());
+    }
+
+    /**
+     * Test getMethod()
+     *
+     * @return void
+     */
+    public function testGetMethod(): void {
+
+        $obj = $this->dtProvider;
+
+        $this->assertNull($obj->getMethod());
+    }
+
+    /**
+     * Test getOptions()
+     *
+     * @return void
+     */
+    public function testGetOptions(): void {
+
+        $obj = $this->dtProvider;
+
+        $res = $obj->getOptions();
+        $this->assertInstanceOf(DataTablesOptionsInterface::class, $res);
+
+        $this->assertCount(5, $res->getOptions());
+
+        $this->assertNotNull($res->getOption("language"));
+        $this->assertEquals([[0, "asc"]], $res->getOption("order"));
+        $this->assertTrue($res->getOption("responsive"));
+        $this->assertEquals(["return" => true], $res->getOption("search"));
+        $this->assertEquals([["responsivePriority" => 1, "targets" => -1,]], $res->getOption("columnDefs"));
+    }
+
+    /**
+     * Test renderRow()
+     *
+     * @return void
+     */
+    public function testRenderRow(): void {
+
+        // Set an Entity mock.
+        $entity = $this->getMockBuilder(Employee::class)->getMock();
+        $entity->expects($this->any())->method("getId")->willReturn(1);
+
+        $obj = $this->dtProvider;
+
+        $this->assertNull($obj->renderRow(DataTablesResponseInterface::DATATABLES_ROW_ATTR, $entity, 0));
+        $this->assertNull($obj->renderRow(DataTablesResponseInterface::DATATABLES_ROW_CLASS, $entity, 0));
+        $this->assertEquals(["id" => 1, "name" => "test"], $obj->renderRow(DataTablesResponseInterface::DATATABLES_ROW_DATA, $entity, 0));
+        $this->assertEquals("test_1", $obj->renderRow(DataTablesResponseInterface::DATATABLES_ROW_ID, $entity, 0));
+    }
+
+}
