fa63982951ad3a57aeb943aa044b39afc3fd8574
webeweb <support@webeweb.fr>
2024-04-08 09:23:59 +0200

    Copy Configuration helper (from core-bundle)

---
+++ lib/common/DependencyInjection/WBWCommonExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\DependencyInjection;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Common extension.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\DependencyInjection
+ */
+class WBWCommonExtension {
+
+    /**
+     * Load a YAML configuration.
+     *
+     * @param string $path The path.
+     * @param string $file The file.
+     * @return array Returns the YAML configuration.
+     */
+    public static function loadYamlConfig(string $path, string $file): array {
+
+        $filename = realpath("$path/$file.yml");
+        if (false === $filename) {
+            return [];
+        }
+
+        $input = file_get_contents($filename);
+
+        return Yaml::parse($input);
+    }
+}
---
+++ lib/common/Tests/DependencyInjection/WBWCommonExtensionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\DependencyInjection;
+
+use WBW\Bundle\CommonBundle\DependencyInjection\WBWCommonExtension;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Common extension test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\DependencyInjection
+ */
+class WBWCommonExtensionTest extends AbstractTestCase {
+
+    /**
+     * Test loadYamlConfig()
+     *
+     * @return void
+     */
+    public function testLoadYamlConfig(): void {
+
+        // Set a path mock.
+        $path = realpath(__DIR__ . "/../Fixtures/Resources/config");
+
+        $res = WBWCommonExtension::loadYamlConfig($path, "config");
+        $this->assertNotEquals([], $res);
+    }
+
+    /**
+     * Test loadYamlConfig()
+     *
+     * @return void
+     */
+    public function testLoadYamlConfigWithoutFilename(): void {
+
+        // Set a path mock.
+        $path = realpath(__DIR__ . "/../Fixtures/Resources/config");
+
+        $res = WBWCommonExtension::loadYamlConfig($path, "error");
+        $this->assertEquals([], $res);
+    }
+}
