c53748e3dad4c840092424c6ab16b4ebf4c00354
webeweb <support@webeweb.fr>
2024-04-08 14:04:48 +0200

    Copy Assets Twig extension (from core-bundle)

---
+++ lib/common/Tests/Twig/Extension/AssetsTwigExtensionTraitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Twig\Extension;
+
+use Twig\Environment;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Twig\Extension\TestAssetsTwigExtensionTrait;
+use WBW\Bundle\CommonBundle\Twig\Extension\AssetsTwigExtension;
+
+/**
+ * Assets Twig extension trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Twig\Extension
+ */
+class AssetsTwigExtensionTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setAssetsTwigExtension()
+     *
+     * @return void
+     */
+    public function testSetAssetsTwigExtension(): void {
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set an Assets Twig extension mock.
+        $assetsTwigExtension = new AssetsTwigExtension($twigEnvironment);
+
+        $obj = new TestAssetsTwigExtensionTrait();
+
+        $obj->setAssetsTwigExtension($assetsTwigExtension);
+        $this->assertSame($assetsTwigExtension, $obj->getAssetsTwigExtension());
+    }
+}
---
+++ lib/common/Twig/Extension/AssetsTwigExtensionTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Twig\Extension;
+
+/**
+ * Assets Twig extension trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Twig\Extension
+ */
+trait AssetsTwigExtensionTrait {
+
+    /**
+     * Assets Twig extension.
+     *
+     * @var AssetsTwigExtension|null
+     */
+    private $assetsTwigExtension;
+
+    /**
+     * Get the assets Twig extension.
+     *
+     * @return AssetsTwigExtension|null Returns the assets Twig extension.
+     */
+    public function getAssetsTwigExtension(): ?AssetsTwigExtension {
+        return $this->assetsTwigExtension;
+    }
+
+    /**
+     * Set the assets Twig extension.
+     *
+     * @param AssetsTwigExtension|null $assetsTwigExtension The assets Twig extension.
+     * @return self Returns this instance.
+     */
+    protected function setAssetsTwigExtension(?AssetsTwigExtension $assetsTwigExtension): self {
+        $this->assetsTwigExtension = $assetsTwigExtension;
+        return $this;
+    }
+}
