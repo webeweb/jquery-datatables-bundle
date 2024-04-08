01ba83da3709d0c429e4f82e2f6198741326c7b2
webeweb <support@webeweb.fr>
2024-04-08 15:45:08 +0200

    Add DataTables exporter interface

---
+++ src/Provider/DataTablesExporterInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Provider;
+
+use WBW\Library\Symfony\Provider\ProviderInterface;
+
+/**
+ * DataTables exporter interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Provider
+ */
+interface DataTablesExporterInterface extends ProviderInterface {
+
+    /**
+     * Export the columns.
+     *
+     * @return string[] Returns the columns.
+     */
+    public function exportColumns(): array;
+
+    /**
+     * Export a row.
+     *
+     * @param object $entity The entity.
+     * @return mixed[] Returns an array representing this row.
+     */
+    public function exportRow($entity): array;
+}
