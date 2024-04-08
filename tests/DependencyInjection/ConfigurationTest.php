d0d64e272cb4c7ccb02feddc59dba09ee3dd6fa4
webeweb <support@webeweb.fr>
2024-04-08 09:52:21 +0200

    Add Configuration test

---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Tests\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use WBW\Bundle\DataTablesBundle\DependencyInjection\Configuration;
+use WBW\Bundle\DataTablesBundle\Tests\AbstractTestCase;
+
+/**
+ * Configuration test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Tests\DependencyInjection
+ */
+class ConfigurationTest extends AbstractTestCase {
+
+    /**
+     * Test getConfigTreeBuilder()
+     *
+     * @return void
+     */
+    public function testGetConfigTree(): void {
+
+        $obj = new Configuration();
+
+        $res = $obj->getConfigTreeBuilder();
+        $this->assertInstanceOf(TreeBuilder::class, $res);
+    }
+}
