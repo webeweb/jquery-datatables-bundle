5b39d30e5929ccddcae46ee4f8cb4d112ecc0a21
webeweb <support@webeweb.fr>
2024-04-10 10:44:31 +0200

    Add abstract web test case

---
+++ lib/common/Tests/AbstractWebTestCase.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Abstract web test case.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests
+ */
+class AbstractWebTestCase extends WebTestCase {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        static::ensureKernelShutdown();
+
+        // Set a Client mock.
+        $this->client = static::createClient();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function setUpBeforeClass(): void {
+        parent::setUpBeforeClass();
+
+        static::$kernel = static::createKernel();
+
+        // Clean the cache to avoid issues due to cache files.
+        $filesystem = new Filesystem();
+        $filesystem->remove(static::$kernel->getCacheDir());
+
+        static::$kernel->boot();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown(): void {
+        static::$kernel->shutdown();
+        parent::tearDown();
+    }
+}
