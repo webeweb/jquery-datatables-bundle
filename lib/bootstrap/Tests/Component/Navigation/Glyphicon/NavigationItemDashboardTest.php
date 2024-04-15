cc424bf9804d7c7ed7488203446af291f224b196
webeweb <support@webeweb.fr>
2024-04-15 16:14:09 +0200

    Copy Navigation node "dashboard" (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemDashboardTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
+
+use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemDashboard;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NavigationNodeInterface;
+
+/**
+ * Navigation item "dashboard" test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon
+ */
+class NavigationItemDashboardTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new NavigationItemDashboard("route");
+
+        $this->assertEquals("navigation.item.dashboard", $obj->getLabel());
+        $this->assertEquals("g:dashboard", $obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+}
