70cb86b7d87a11388dc9a79faf5fb55d45fee80d
webeweb <support@webeweb.fr>
2024-04-15 09:49:33 +0200

    Copy Bootstrap extension (from bootstrap-bundle)

---
+++ lib/bootstrap/DependencyInjection/WBWBootstrapExtension.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use WBW\Bundle\CommonBundle\DependencyInjection\Container\ContainerHelper;
+use WBW\Bundle\CommonBundle\DependencyInjection\WBWCommonExtension;
+
+/**
+ * Bootstrap extension.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\DependencyInjection
+ */
+class WBWBootstrapExtension extends Extension {
+
+    /**
+     * Extension alias.
+     *
+     * @var string
+     */
+    const EXTENSION_ALIAS = "wbw_bootstrap";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void {
+
+        $path = __DIR__ . "/../Resources/config";
+
+        $fileLocator = new FileLocator($path);
+
+        $serviceLoader = new YamlFileLoader($container, $fileLocator);
+        //$serviceLoader->load("providers.yml");
+        $serviceLoader->load("twig.yml");
+
+        /** @var ConfigurationInterface $configuration */
+        $configuration = $this->getConfiguration($configs, $container);
+
+        $config = $this->processConfiguration($configuration, $configs);
+
+        ContainerHelper::setParameter($container, $config, $this->getAlias(), "version");
+        ContainerHelper::setParameter($container, $config, $this->getAlias(), "plugins");
+        ContainerHelper::setParameter($container, $config, $this->getAlias(), "locales");
+
+        $assets = WBWCommonExtension::loadYamlConfig($path, "assets");
+        ContainerHelper::setParameters($container, $assets["assets"]);
+    }
+}
---
+++ lib/bootstrap/Tests/DependencyInjection/WBWBootstrapExtensionTest.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Throwable;
+use Twig\Environment;
+use WBW\Bundle\BootstrapBundle\DependencyInjection\Configuration;
+use WBW\Bundle\BootstrapBundle\DependencyInjection\WBWBootstrapExtension;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\AssetsTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\AlertTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\BadgeTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ButtonTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\LabelTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ProgressBarTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Content\CodeTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Content\TypographyTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\FontAwesomeTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\GlyphiconTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\IconTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\MaterialDesignIconicFontTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\MeteoconsTwigExtension;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Layout\GridTwigExtension;
+
+/**
+ * WBW Bootstrap extension test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\DependencyInjection
+ */
+class WBWBootstrapExtensionTest extends AbstractTestCase {
+
+    /**
+     * Configs.
+     *
+     * @var array<string,mixed>|null
+     */
+    private $configs;
+
+    /**
+     * Container builder.
+     *
+     * @var ContainerBuilder|null
+     */
+    private $containerBuilder;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a configs array mock.
+        $this->configs = [
+            WBWBootstrapExtension::EXTENSION_ALIAS => [],
+        ];
+
+        // Set a Twig environment mock.
+        $twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+
+        // Set a Container builder mock.
+        $this->containerBuilder = new ContainerBuilder();
+        $this->containerBuilder->set("twig", $twigEnvironment);
+    }
+
+    /**
+     * Test getAlias()
+     *
+     * @return void
+     */
+    public function testGetAlias(): void {
+
+        $obj = new WBWBootstrapExtension();
+
+        $this->assertEquals(WBWBootstrapExtension::EXTENSION_ALIAS, $obj->getAlias());
+    }
+
+    /**
+     * Test getConfiguration()
+     *
+     * @return void
+     */
+    public function testGetConfiguration(): void {
+
+        $obj = new WBWBootstrapExtension();
+
+        $this->assertInstanceOf(Configuration::class, $obj->getConfiguration([], $this->containerBuilder));
+    }
+
+    /**
+     * Test load()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testLoad(): void {
+
+        $obj = new WBWBootstrapExtension();
+
+        $obj->load($this->configs, $this->containerBuilder);
+
+        // Twig extensions
+        $this->assertInstanceOf(AssetsTwigExtension::class, $this->containerBuilder->get(AssetsTwigExtension::SERVICE_NAME));
+
+        $this->assertInstanceOf(AlertTwigExtension::class, $this->containerBuilder->get(AlertTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(BadgeTwigExtension::class, $this->containerBuilder->get(BadgeTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(ButtonTwigExtension::class, $this->containerBuilder->get(ButtonTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(LabelTwigExtension::class, $this->containerBuilder->get(LabelTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(ProgressBarTwigExtension::class, $this->containerBuilder->get(ProgressBarTwigExtension::SERVICE_NAME));
+
+        $this->assertInstanceOf(CodeTwigExtension::class, $this->containerBuilder->get(CodeTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(TypographyTwigExtension::class, $this->containerBuilder->get(TypographyTwigExtension::SERVICE_NAME));
+
+        $this->assertInstanceOf(FontAwesomeTwigExtension::class, $this->containerBuilder->get(FontAwesomeTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(GlyphiconTwigExtension::class, $this->containerBuilder->get(GlyphiconTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(IconTwigExtension::class, $this->containerBuilder->get(IconTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(MaterialDesignIconicFontTwigExtension::class, $this->containerBuilder->get(MaterialDesignIconicFontTwigExtension::SERVICE_NAME));
+        $this->assertInstanceOf(MeteoconsTwigExtension::class, $this->containerBuilder->get(MeteoconsTwigExtension::SERVICE_NAME));
+
+        $this->assertInstanceOf(GridTwigExtension::class, $this->containerBuilder->get(GridTwigExtension::SERVICE_NAME));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw_bootstrap", WBWBootstrapExtension::EXTENSION_ALIAS);
+    }
+}
