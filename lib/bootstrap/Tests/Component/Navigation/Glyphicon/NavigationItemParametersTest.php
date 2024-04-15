5157955356fbbfd900e248e6a1d83bdd6212e125
webeweb <support@webeweb.fr>
2024-04-15 16:14:35 +0200

    Copy Navigation node "parameters" (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemParametersTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
+
+use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemParameters;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NavigationNodeInterface;
+
+/**
+ * Navigation item "parameters" test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon
+ */
+class NavigationItemParametersTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new NavigationItemParameters("route");
+
+        $this->assertEquals("navigation.item.parameters", $obj->getLabel());
+        $this->assertEquals("g:wrench", $obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+}
