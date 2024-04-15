576b290d4915ef18cb9a9cc73b4adb7e965e1f14
webeweb <support@webeweb.fr>
2024-04-15 14:35:49 +0200

    Copy String Twig extension (from core-bundle)

---
+++ lib/common/Tests/Twig/Extension/StringTwigExtensionTest.php
@@ -0,0 +1,392 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Twig\Extension;
+
+use DateTime;
+use Throwable;
+use Twig\Environment;
+use Twig\Extension\ExtensionInterface;
+use Twig\Node\Node;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Twig\Extension\StringTwigExtension;
+
+/**
+ * String Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Twig\Extension
+ */
+class StringTwigExtensionTest extends AbstractTestCase {
+
+    /**
+     * Twig environment.
+     *
+     * @var Environment|null
+     */
+    private $twigEnvironment;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Twig environment mock.
+        $this->twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * Test dateFormat()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testDateFormat(): void {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->dateFormat(null));
+        $this->assertEquals("2018-01-14 18:00", $obj->dateFormat(new DateTime("2018-01-14 18:00")));
+        $this->assertEquals("14/01/2018 18:00", $obj->dateFormat(new DateTime("2018-01-14 18:00"), "d/m/Y H:i"));
+    }
+
+    /**
+     * Test getFilters()
+     *
+     * @return void
+     */
+    public function testGetFilters() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFilters();
+        $this->assertCount(11, $res);
+
+        $i = -1;
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("dateFormat", $res[$i]->getName());
+        $this->assertEquals([$obj, "dateFormat"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("htmlEntityDecode", $res[$i]->getName());
+        $this->assertEquals([$obj, "htmlEntityDecode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("htmlEntityEncode", $res[$i]->getName());
+        $this->assertEquals([$obj, "htmlEntityEncode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("jsonDecode", $res[$i]->getName());
+        $this->assertEquals([$obj, "jsonDecode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("md5", $res[$i]->getName());
+        $this->assertEquals([$obj, "md5"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringExtractUpperCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringExtractUpperCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringFormat", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringFormat"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringHumanReadable", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringHumanReadable"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringLowerCamelCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringLowerCamelCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringSnakeCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringSnakeCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFilter::class, $res[++$i]);
+        $this->assertEquals("stringUpperCamelCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringUpperCamelCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+    }
+
+    /**
+     * Test getFunctions()
+     *
+     * @return void
+     */
+    public function testGetFunctions() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(12, $res);
+
+        $i = -1;
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("dateFormat", $res[$i]->getName());
+        $this->assertEquals([$obj, "dateFormat"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("htmlEntityDecode", $res[$i]->getName());
+        $this->assertEquals([$obj, "htmlEntityDecode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("htmlEntityEncode", $res[$i]->getName());
+        $this->assertEquals([$obj, "htmlEntityEncode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("jsonDecode", $res[$i]->getName());
+        $this->assertEquals([$obj, "jsonDecode"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("md5", $res[$i]->getName());
+        $this->assertEquals([$obj, "md5"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringExtractUpperCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringExtractUpperCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringFormat", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringFormat"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringHumanReadable", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringHumanReadable"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringLowerCamelCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringLowerCamelCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringSnakeCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringSnakeCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringUniqId", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringUniqIdFunction"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("stringUpperCamelCase", $res[$i]->getName());
+        $this->assertEquals([$obj, "stringUpperCamelCase"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+    }
+
+    /**
+     * Test htmlEntityDecode()
+     *
+     * @return void
+     */
+    public function testHtmlEntityDecode(): void {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->htmlEntityDecode(null));
+        $this->assertEquals("&", $obj->htmlEntityDecode("&amp;"));
+    }
+
+    /**
+     * Test htmlEntityEncode()
+     *
+     * @return void
+     */
+    public function testHtmlEntityEncode(): void {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->htmlEntityEncode(null));
+        $this->assertEquals("&amp;", $obj->htmlEntityEncode("&"));
+    }
+
+    /**
+     * Test jsonDecode()
+     *
+     * @return void
+     */
+    public function testJsonDecode(): void {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->jsonDecode(null));
+        $this->assertEquals([], $obj->jsonDecode("{}"));
+    }
+
+    /**
+     * Test md5()
+     *
+     * @return void
+     */
+    public function testMd5(): void {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->md5(null));
+        $this->assertEquals("d41d8cd98f00b204e9800998ecf8427e", $obj->md5(""));
+    }
+
+    /**
+     * Test the stringExtractUpperCase() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringExtractUpperCase() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringExtractUpperCase(null));
+        $this->assertEquals("STE", $obj->stringExtractUpperCase("StringTwigExtension"));
+        $this->assertEquals("ste", $obj->stringExtractUpperCase("StringTwigExtension", true));
+    }
+
+    /**
+     * Test the stringFormat() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringFormat() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+        $this->assertEquals("", $obj->stringFormat(null, "_____ _____ _"));
+        $this->assertEquals("", $obj->stringFormat("Helloworld!", null));
+
+        $this->assertEquals("Hello world !", $obj->stringFormat("Helloworld!", "_____ _____ _"));
+        $this->assertEquals("+33 6 12 34 56 78", $obj->stringFormat("612345678", "+33 _ __ __ __ __"));
+    }
+
+    /**
+     * Test the stringHumanReadable() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringHumanReadable() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringHumanReadable(null));
+        $this->assertEquals("String twig extension", $obj->stringHumanReadable("StringTwigExtension"));
+    }
+
+    /**
+     * Test the stringLowerCamelCase() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringLowerCamelCase() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringLowerCamelCase(null));
+        $this->assertEquals("stringTwigExtension", $obj->stringLowerCamelCase("StringTwigExtension"));
+    }
+
+    /**
+     * Test the stringSnakeCase() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringSnakeCase() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringSnakeCase(null));
+        $this->assertEquals("string_twig_extension", $obj->stringSnakeCase("StringTwigExtension"));
+        $this->assertEquals("string-twig-extension", $obj->stringSnakeCase("StringTwigExtension", "-"));
+    }
+
+    /**
+     * Test the stringUniqidFunction() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringUniqidFunction() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringUniqidFunction(-1));
+
+        $res = $obj->stringUniqidFunction();
+        $this->assertRegExp("/^[a-z0-9]{13}$/", $res);
+
+        $map = [];
+        for ($i = 0; $i < 20; ++$i) {
+
+            $tmp = $obj->stringUniqidFunction();
+            $this->assertArrayNotHasKey($tmp, $map);
+
+            $map[$tmp] = $tmp;
+        }
+    }
+
+    /**
+     * Test the stringUpperCamelCase() method
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testStringUpperCamelCase() {
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertNull($obj->stringUpperCamelCase(null));
+        $this->assertEquals("StringTwigExtension", $obj->stringUpperCamelCase("StringTwigExtension"));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $this->assertEquals("wbw.common.twig.extension.string", StringTwigExtension::SERVICE_NAME);
+
+        $obj = new StringTwigExtension($this->twigEnvironment);
+
+        $this->assertInstanceOf(ExtensionInterface::class, $obj);
+
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+
+}
---
+++ lib/common/Twig/Extension/StringTwigExtension.php
@@ -0,0 +1,251 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Twig\Extension;
+
+use DateTime;
+use RuntimeException;
+use Throwable;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use WBW\Library\Types\Helper\DateTimeHelper;
+use WBW\Library\Types\Helper\StringHelper;
+
+/**
+ * String Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Twig\Extension
+ */
+class StringTwigExtension extends AbstractTwigExtension {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.common.twig.extension.string";
+
+    /**
+     * Format a date/time.
+     *
+     * @param DateTime|null $dateTime The date/time.
+     * @param string $format The format.
+     * @return string|null Returns the formatted date/time.
+     */
+    public function dateFormat(?DateTime $dateTime, string $format = DateTimeHelper::DATETIME_FORMAT): ?string {
+        return DateTimeHelper::toString($dateTime, $format);
+    }
+
+    /**
+     * Get the Twig filters.
+     *
+     * @return TwigFilter[] Returns the Twig filters.
+     */
+    public function getFilters(): array {
+
+        return [
+            new TwigFilter("dateFormat", [$this, "dateFormat"], ["is_safe" => ["html"]]),
+
+            new TwigFilter("htmlEntityDecode", [$this, "htmlEntityDecode"], ["is_safe" => ["html"]]),
+            new TwigFilter("htmlEntityEncode", [$this, "htmlEntityEncode"], ["is_safe" => ["html"]]),
+
+            new TwigFilter("jsonDecode", [$this, "jsonDecode"], ["is_safe" => ["html"]]),
+
+            new TwigFilter("md5", [$this, "md5"], ["is_safe" => ["html"]]),
+
+            new TwigFilter("stringExtractUpperCase", [$this, "stringExtractUpperCase"], ["is_safe" => ["html"]]),
+            new TwigFilter("stringFormat", [$this, "stringFormat"], ["is_safe" => ["html"]]),
+            new TwigFilter("stringHumanReadable", [$this, "stringHumanReadable"], ["is_safe" => ["html"]]),
+            new TwigFilter("stringLowerCamelCase", [$this, "stringLowerCamelCase"], ["is_safe" => ["html"]]),
+            new TwigFilter("stringSnakeCase", [$this, "stringSnakeCase"], ["is_safe" => ["html"]]),
+            new TwigFilter("stringUpperCamelCase", [$this, "stringUpperCamelCase"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return TwigFunction[] Returns the Twig functions.
+     */
+    public function getFunctions(): array {
+
+        return [
+            new TwigFunction("dateFormat", [$this, "dateFormat"], ["is_safe" => ["html"]]),
+
+            new TwigFunction("htmlEntityDecode", [$this, "htmlEntityDecode"], ["is_safe" => ["html"]]),
+            new TwigFunction("htmlEntityEncode", [$this, "htmlEntityEncode"], ["is_safe" => ["html"]]),
+
+            new TwigFunction("jsonDecode", [$this, "jsonDecode"], ["is_safe" => ["html"]]),
+
+            new TwigFunction("md5", [$this, "md5"], ["is_safe" => ["html"]]),
+
+            new TwigFunction("stringExtractUpperCase", [$this, "stringExtractUpperCase"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringFormat", [$this, "stringFormat"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringHumanReadable", [$this, "stringHumanReadable"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringLowerCamelCase", [$this, "stringLowerCamelCase"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringSnakeCase", [$this, "stringSnakeCase"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringUniqId", [$this, "stringUniqIdFunction"], ["is_safe" => ["html"]]),
+            new TwigFunction("stringUpperCamelCase", [$this, "stringUpperCamelCase"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * Decode HTML entities.
+     *
+     * @param string|null $string The string.
+     * @return string|null Returns the decoded HTML entities.
+     */
+    public function htmlEntityDecode(?string $string): ?string {
+
+        if (null === $string) {
+            return null;
+        }
+
+        return html_entity_decode($string);
+    }
+
+    /**
+     * Encode HTML entities.
+     *
+     * @param string|null $string The string.
+     * @return string|null Returns the encoded HTML entities.
+     */
+    public function htmlEntityEncode(?string $string): ?string {
+
+        if (null === $string) {
+            return null;
+        }
+
+        return htmlentities($string);
+    }
+
+    /**
+     * Decode a JSON string.
+     *
+     * @param string|null $string The string.
+     * @return array|null Returns the decoded JSON string.
+     */
+    public function jsonDecode(?string $string): ?array {
+
+        if (null === $string) {
+            return null;
+        }
+
+        return json_decode($string, true);
+    }
+
+    /**
+     * MD5.
+     *
+     * @param string|null $string The string.
+     * @return string|null Returns the MD5.
+     */
+    public function md5(?string $string): ?string {
+
+        if (null === $string) {
+            return null;
+        }
+
+        return md5($string);
+    }
+
+    /**
+     * Display the extracted upper case letters.
+     *
+     * @param string|null $str The string.
+     * @param bool $lower Lower case ?
+     * @return string|null Returns the extracted upper case letters.
+     */
+    public function stringExtractUpperCase(?string $str, bool $lower = false): ?string {
+        return StringHelper::extractUpperCase($str, $lower);
+    }
+
+    /**
+     * Display a formatted string.
+     *
+     * @param string|null $string The string.
+     * @param string|null $format The format.
+     * @return string|null Returns the formatted string.
+     */
+    public function stringFormat(?string $string, ?string $format): ?string {
+        return StringHelper::format($string, $format);
+    }
+
+    /**
+     * Display an human-readable string.
+     *
+     * @param string|null $str The string.
+     * @return string|null Returns the human-readable string.
+     */
+    public function stringHumanReadable(?string $str): ?string {
+        return StringHelper::toHumanReadable($str);
+    }
+
+    /**
+     * Display a lower camel case string.
+     *
+     * @param string|null $str The string.
+     * @return string|null Returns the lower camel case string.
+     */
+    public function stringLowerCamelCase(?string $str): ?string {
+        return StringHelper::toLowerCamelCase($str);
+    }
+
+    /**
+     * Display a snake case string.
+     *
+     * @param string|null $str The string.
+     * @param string $sep The separator.
+     * @return string|null Returns the snake case.
+     */
+    public function stringSnakeCase(?string $str, string $sep = "_"): ?string {
+        return StringHelper::toSnakeCase($str, $sep);
+    }
+
+    /**
+     * Display a unique id.
+     *
+     * @param int $length The length.
+     * @return string|null Returns the unique id.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function stringUniqIdFunction(int $length = 13): ?string {
+
+        if ($length < 1) {
+            return null;
+        }
+
+        if (true === function_exists("random_bytes")) {
+            $bytes = random_bytes((int) ceil($length / 2));
+        } else if (true === function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes((int) ceil($length / 2));
+        } else {
+            throw new RuntimeException("random_bytes() and openssl_random_pseudo_bytes() are not available");
+        }
+
+        $hex = bin2hex($bytes);
+
+        return substr($hex, 0, $length);
+    }
+
+    /**
+     * Display an upper camel case string.
+     *
+     * @param string|null $str The string.
+     * @return string|null Returns the upper camel case string.
+     */
+    public function stringUpperCamelCase(?string $str): ?string {
+        return StringHelper::toUpperCamelCase($str);
+    }
+}
