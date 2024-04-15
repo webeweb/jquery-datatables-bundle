3faef6820a15a810fded54da76f499e6c469f19c
webeweb <support@webeweb.fr>
2024-04-15 16:12:23 +0200

    Copy Breadcrumb node action "export" (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemExportTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon;
+
+use WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon\BreadcrumbItemExport;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NavigationNodeInterface;
+
+/**
+ * Breadcrumb item "export" test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon
+ */
+class BreadcrumbItemExportTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new BreadcrumbItemExport("route");
+
+        $this->assertEquals("navigation.item.export", $obj->getLabel());
+        $this->assertEquals("g:open", $obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+}
