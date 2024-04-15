96c312510c8eb94203836ecc05c1ae677ba490dc
webeweb <support@webeweb.fr>
2024-04-15 16:15:41 +0200

    Copy Navigation node action "import" (from bootstrap-bundle)

---
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemImportTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
+
+use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemImport;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NavigationNodeInterface;
+
+/**
+ * Navigation item "import" test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon
+ */
+class NavigationItemImportTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new NavigationItemImport("route");
+
+        $this->assertEquals("navigation.item.import", $obj->getLabel());
+        $this->assertEquals("g:save", $obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+}
