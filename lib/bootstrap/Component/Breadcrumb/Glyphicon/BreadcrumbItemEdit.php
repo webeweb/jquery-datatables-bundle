0da968909fefb4cd87387ad261a42771f9e68b55
webeweb <support@webeweb.fr>
2024-04-15 16:12:12 +0200

    Copy Breadcrumb node action "edit" (from bootstrap-bundle)

---
+++ lib/bootstrap/Component/Breadcrumb/Glyphicon/BreadcrumbItemEdit.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon;
+
+use WBW\Library\Symfony\Assets\Navigation\BreadcrumbNode;
+
+/**
+ * Breadcrumb item "edit".
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon
+ */
+class BreadcrumbItemEdit extends BreadcrumbNode {
+
+    /**
+     * Constructor.
+     *
+     * @param string|null $route The route.
+     * @param string $matcher The matcher.
+     */
+    public function __construct(?string $route, string $matcher = self::MATCHER_URL) {
+        parent::__construct("navigation.item.edit", "g:pencil", $route, $matcher);
+    }
+}
---
+++ lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemEditTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon;
+
+use WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon\BreadcrumbItemEdit;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Library\Symfony\Assets\NavigationNodeInterface;
+
+/**
+ * Breadcrumb item "edit" test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon
+ */
+class BreadcrumbItemEditTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new BreadcrumbItemEdit("route");
+
+        $this->assertEquals("navigation.item.edit", $obj->getLabel());
+        $this->assertEquals("g:pencil", $obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertEquals("route", $obj->getUri());
+    }
+}
