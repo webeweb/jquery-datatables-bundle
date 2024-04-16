97b91d8c58f80acf779affa68f52ba34c330d76b
webeweb <support@webeweb.fr>
2024-04-16 16:06:14 +0200

    Copy abstract icon (from core-library)

---
+++ lib/widget/Tests/Fixtures/Icon/TestIcon.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Fixtures\Icon;
+
+use WBW\Bundle\WidgetBundle\Icon\AbstractIcon;
+
+/**
+ * Test icon.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Fixtures\Icon
+ */
+class TestIcon extends AbstractIcon {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+}
