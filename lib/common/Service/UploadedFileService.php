cbf757ff3a5d494e27f238214c94ea389b60f358
webeweb <support@webeweb.fr>
2024-04-16 11:29:23 +0200

    Copy Uploaded file service (from core-library)

---
+++ lib/common/Service/UploadedFileService.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Service;
+
+use SplFileInfo;
+use Throwable;
+use WBW\Library\Traits\Strings\StringDirectoryTrait;
+use WBW\Library\Types\Helper\StringHelper;
+
+/**
+ * Uploaded file service.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Service
+ */
+class UploadedFileService implements UploadedFileServiceInterface {
+
+    use StringDirectoryTrait {
+        setDirectory as protected;
+    }
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.common.service.uploaded_file";
+
+    /**
+     * Upload directory.
+     *
+     * @var string
+     */
+    public const UPLOAD_DIRECTORY = "/uploads";
+
+    /**
+     * Constructor.
+     *
+     * @param string $directory The directory.
+     */
+    public function __construct(string $directory) {
+        $this->setDirectory($directory);
+    }
+
+    /**
+     * Determine if a filename exists.
+     *
+     * @param string $filename The filename.
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function exists(string $filename): bool {
+        return file_exists($this->path($filename));
+    }
+
+    /**
+     * Create a directory.
+     *
+     * @param string $directory The directory.
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    protected function mkdir(string $directory): bool {
+
+        if (true === file_exists($directory)) {
+            return true;
+        }
+
+        return mkdir($directory, 0755, true);
+    }
+
+    /**
+     * Path.
+     *
+     * @param string $filename The filename.
+     * @return string Returns the path.
+     */
+    public function path(string $filename): string {
+
+        return implode(DIRECTORY_SEPARATOR, [
+            $this->getDirectory(),
+            $filename,
+        ]);
+    }
+
+    /**
+     * Save.
+     *
+     * @param SplFileInfo $uploadedFile The uploaded file.
+     * @param string $subdirectory The subdirectory.
+     * @param string|null $filename The filename.
+     * @param int $permissions The permissions.
+     * @return string|null Returns the uploaded file path.
+     */
+    public function save(SplFileInfo $uploadedFile, string $subdirectory, string $filename = null, int $permissions = 0600): ?string {
+
+        // Directory
+        $dir = implode("", [
+            $this->getDirectory(),
+            self::UPLOAD_DIRECTORY,
+            $subdirectory,
+        ]);
+
+        // Destination
+        $dst = implode(DIRECTORY_SEPARATOR, [
+            $dir,
+            null !== $filename ? $filename : $uploadedFile->getFilename(),
+        ]);
+
+        if (false === $this->mkdir($dir) || false === rename($uploadedFile->getPathname(), $dst)) {
+            return null;
+        }
+
+        chmod($dst, $permissions);
+
+        return str_replace($this->getDirectory(), "", $dst);
+    }
+
+    /**
+     * Unique id.
+     *
+     * @return string Returns the unique id.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function uniqId(): string {
+
+        $random  = random_bytes(32);
+        $bin2hex = bin2hex($random);
+
+        return StringHelper::format($bin2hex, "________-____-____-____-____________");
+    }
+
+    /**
+     * Unlink.
+     *
+     * @param string $filename The filename.
+     * @return bool|null Returns true in case of success, false otherwise.
+     */
+    public function unlink(string $filename): ?bool {
+
+        if (false === $this->exists($filename)) {
+            return null;
+        }
+
+        return unlink($this->path($filename));
+    }
+}
---
+++ lib/common/Tests/Service/UploadedFileServiceTest.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Service;
+
+use SplFileInfo;
+use Throwable;
+use WBW\Bundle\CommonBundle\Service\UploadedFileService;
+use WBW\Bundle\CommonBundle\Service\UploadedFileServiceInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Uploaded file service test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Service
+ */
+class UploadedFileServiceTest extends AbstractTestCase {
+
+    /**
+     * Directory.
+     *
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a directory mock.
+        $this->directory = realpath(__DIR__ . "/../../../../var");
+    }
+
+    /**
+     * Test exists()
+     *
+     * @return void
+     */
+    public function testExists(): void {
+
+        $obj = new UploadedFileService($this->directory);
+
+        $this->assertFalse($obj->exists("basename.bak"));
+    }
+
+    /**
+     * Test path()
+     *
+     * @return void
+     */
+    public function testPath(): void {
+
+        $dir = $this->directory;
+
+        $obj = new UploadedFileService($this->directory);
+
+        $this->assertEquals("$dir/basename.bak", $obj->path("basename.bak"));
+    }
+
+    /**
+     * Test save()
+     *
+     * @return void
+     */
+    public function testSave(): void {
+
+        // Set the directory mock.
+        $dir = $this->directory . "/uploads/subdirectory";
+
+        if (true === file_exists("$dir/basename.bak")) {
+            unlink("$dir/basename.bak");
+        }
+        if (true === file_exists($dir)) {
+            rmdir($dir);
+        }
+
+        // Set the file mocks.
+        $src = __DIR__ . "/UploadedFileServiceTest.php";
+        $dst = __DIR__ . "/UploadServiceTest.bak";
+
+        if (true === file_exists($dst)) {
+            unlink($dst);
+        }
+
+        copy($src, $dst);
+
+        // Set an SPL file info mock.
+        $uploadedFile = new SplFileInfo($dst);
+
+        $obj = new UploadedFileService($this->directory);
+
+        $res = $obj->save($uploadedFile, "/subdirectory", "basename.bak");
+        $this->assertEquals("/uploads/subdirectory/basename.bak", $res);
+
+        $this->assertFileExists("$dir/basename.bak");
+    }
+
+    /**
+     * Test uniqId()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testUniqId(): void {
+
+        $obj = new UploadedFileService($this->directory);
+
+        $this->assertRegExp("/^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$/", $obj->uniqId());
+    }
+
+    /**
+     * Test unlink()
+     *
+     * @return void
+     */
+    public function testUnlink(): void {
+
+        $obj = new UploadedFileService($this->directory);
+
+        $this->assertNull($obj->unlink("exception"));
+        $this->assertTrue($obj->unlink("/uploads/subdirectory/basename.bak"));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.common.service.uploaded_file", UploadedFileService::SERVICE_NAME);
+        $this->assertEquals("/uploads", UploadedFileService::UPLOAD_DIRECTORY);
+
+        $obj = new UploadedFileService($this->directory);
+
+        $this->assertInstanceOf(UploadedFileServiceInterface::class, $obj);
+
+        $this->assertEquals($this->directory, $obj->getDirectory());
+    }
+}
