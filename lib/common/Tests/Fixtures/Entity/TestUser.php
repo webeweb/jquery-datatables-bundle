965e8ac48892136143aee08ff92e979f6378e056
webeweb <support@webeweb.fr>
2024-04-16 12:33:34 +0200

    Copy test User (from core-bundle)

---
+++ lib/common/Tests/Fixtures/Entity/TestUser.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Fixtures\Entity;
+
+use WBW\Bundle\CommonBundle\Model\User;
+
+/**
+ * Test user.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Fixtures\Model
+ */
+class TestUser extends User {
+
+}
