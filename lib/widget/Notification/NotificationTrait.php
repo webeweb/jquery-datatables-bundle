fee836ea5b4f73219fe6121d67f61a7d9f9c662e
webeweb <support@webeweb.fr>
2024-04-16 16:03:04 +0200

    Copy Notification trait (copy from core-library)

---
+++ lib/widget/Notification/NotificationTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Notification;
+
+/**
+ * Notification trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Notification
+ */
+trait NotificationTrait {
+
+    /**
+     * Notification.
+     *
+     * @var NotificationInterface|null
+     */
+    protected $notification;
+
+    /**
+     * Get the notification.
+     *
+     * @return NotificationInterface|null Returns the notification.
+     */
+    public function getNotification(): ?NotificationInterface {
+        return $this->notification;
+    }
+
+    /**
+     * Set the notification.
+     *
+     * @param NotificationInterface|null $notification The notification.
+     * @return self Returns this instance.
+     */
+    protected function setNotification(?NotificationInterface $notification): self {
+        $this->notification = $notification;
+        return $this;
+    }
+}
