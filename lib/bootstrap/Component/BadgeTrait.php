a6376c3f49ec4a3fa765af21db43728ed972c58e
webeweb <support@webeweb.fr>
2024-04-16 09:24:23 +0200

    Add Badge trait

---
+++ lib/bootstrap/Component/BadgeTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Component;
+
+/**
+ * Badge trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Component
+ */
+trait BadgeTrait {
+
+    /**
+     * Badge.
+     *
+     * @var BadgeInterface|null
+     */
+    protected $badge;
+
+    /**
+     * Get the badge.
+     *
+     * @return BadgeInterface|null Returns the badge.
+     */
+    public function getBadge(): ?BadgeInterface {
+        return $this->badge;
+    }
+
+    /**
+     * Set the badge.
+     *
+     * @param BadgeInterface|null $badge The badge.
+     * @return self Returns this instance.
+     */
+    public function setBadge(?BadgeInterface $badge): self {
+        $this->badge = $badge;
+        return $this;
+    }
+}
---
+++ lib/bootstrap/Tests/Component/BadgeTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Component;
+
+use WBW\Bundle\BootstrapBundle\Component\BadgeInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Tests\Fixtures\Component\TestBadgeTrait;
+
+/**
+ * Badge trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Component
+ */
+class BadgeTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setBadge()
+     *
+     * @return void
+     */
+    public function testSetBadge(): void {
+
+        // Set a Badge mock.
+        $badge = $this->getMockBuilder(BadgeInterface::class)->getMock();
+
+        $obj = new TestBadgeTrait();
+
+        $obj->setBadge($badge);
+        $this->assertSame($badge, $obj->getBadge());
+    }
+}
