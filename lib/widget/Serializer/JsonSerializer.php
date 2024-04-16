41dfe01a370079561cb0c7ebb4fb61f921e63307
webeweb <support@webeweb.fr>
2024-04-16 16:04:44 +0200

    Copy JSON serializer (copy from core-library)

---
+++ lib/widget/Serializer/JsonSerializer.php
@@ -0,0 +1,187 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Serializer;
+
+use WBW\Bundle\WidgetBundle\Alert\AlertInterface;
+use WBW\Bundle\WidgetBundle\Badge\BadgeInterface;
+use WBW\Bundle\WidgetBundle\Button\ButtonInterface;
+use WBW\Bundle\WidgetBundle\Calendar\FullCalendarEventInterface;
+use WBW\Bundle\WidgetBundle\Icon\IconInterface;
+use WBW\Bundle\WidgetBundle\Label\LabelInterface;
+use WBW\Bundle\WidgetBundle\Notification\NotificationInterface;
+use WBW\Bundle\WidgetBundle\ProgressBar\ProgressBarInterface;
+use WBW\Bundle\WidgetBundle\Toast\ToastInterface;
+use WBW\Library\Serializer\SerializerKeys as BaseSerializerKeys;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * JSON serializer.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Serializer
+ */
+class JsonSerializer {
+
+    /**
+     * Serialize an alert.
+     *
+     * @param AlertInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeAlert(AlertInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize an badge.
+     *
+     * @param BadgeInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeBadge(BadgeInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a button.
+     *
+     * @param ButtonInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeButton(ButtonInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a Full Calendar event.
+     *
+     * @param FullCalendarEventInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeFullCalendarEvent(FullCalendarEventInterface $model): array {
+
+        $output = [];
+
+        ArrayHelper::set($output, "id", $model->getId(), [null]);
+        ArrayHelper::set($output, "groupId", $model->getGroupId(), [null]);
+        ArrayHelper::set($output, "allDay", $model->getAllDay(), [null]);
+
+        if (null !== $model->getStart()) {
+            ArrayHelper::set($output, "start", $model->getStart()->format("Y-m-d\TH:i:s"));
+        }
+
+        if (null !== $model->getEnd()) {
+            ArrayHelper::set($output, "end", $model->getEnd()->format("Y-m-d\TH:i:s"));
+        }
+
+        ArrayHelper::set($output, "startStr", $model->getStartStr(), [null]);
+        ArrayHelper::set($output, "endStr", $model->getEndStr(), [null]);
+        ArrayHelper::set($output, "title", $model->getTitle(), [null]);
+        ArrayHelper::set($output, "url", $model->getUrl(), [null]);
+        ArrayHelper::set($output, "classNames", $model->getClassNames(), [null]);
+        ArrayHelper::set($output, "editable", $model->getEditable(), [null]);
+        ArrayHelper::set($output, "startEditable", $model->getStartEditable(), [null]);
+        ArrayHelper::set($output, "durationEditable", $model->getDurationEditable(), [null]);
+        ArrayHelper::set($output, "resourceEditable", $model->getResourceEditable(), [null]);
+        ArrayHelper::set($output, "display", $model->getDisplay(), [null]);
+        ArrayHelper::set($output, "backgroundColor", $model->getBackgroundColor(), [null]);
+        ArrayHelper::set($output, "borderColor", $model->getBorderColor(), [null]);
+        ArrayHelper::set($output, "textColor", $model->getTextColor(), [null]);
+        ArrayHelper::set($output, "extraParams", $model->getExtraParams());
+
+        return $output;
+    }
+
+    /**
+     * Serialize an icon.
+     *
+     * @param IconInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeIcon(IconInterface $model): array {
+
+        return [
+            BaseSerializerKeys::NAME  => $model->getName(),
+            BaseSerializerKeys::STYLE => $model->getStyle(),
+        ];
+    }
+
+    /**
+     * Serialize a label.
+     *
+     * @param LabelInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeLabel(LabelInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a notification.
+     *
+     * @param NotificationInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeNotification(NotificationInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a progress bar.
+     *
+     * @param ProgressBarInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeProgressBar(ProgressBarInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+
+    /**
+     * Serialize a toast.
+     *
+     * @param ToastInterface $model The model.
+     * @return array<string,mixed> Returns the serialized model.
+     */
+    public static function serializeToast(ToastInterface $model): array {
+
+        return [
+            BaseSerializerKeys::CONTENT => $model->getContent(),
+            BaseSerializerKeys::TYPE    => $model->getType(),
+        ];
+    }
+}
---
+++ lib/widget/Tests/Serializer/JsonSerializerTest.php
@@ -0,0 +1,244 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Serializer;
+
+use DateTime;
+use WBW\Bundle\WidgetBundle\Alert\AlertInterface;
+use WBW\Bundle\WidgetBundle\Badge\BadgeInterface;
+use WBW\Bundle\WidgetBundle\Button\ButtonInterface;
+use WBW\Bundle\WidgetBundle\Calendar\FullCalendarEventInterface;
+use WBW\Bundle\WidgetBundle\Icon\IconInterface;
+use WBW\Bundle\WidgetBundle\Label\LabelInterface;
+use WBW\Bundle\WidgetBundle\Notification\NotificationInterface;
+use WBW\Bundle\WidgetBundle\ProgressBar\ProgressBarInterface;
+use WBW\Bundle\WidgetBundle\Serializer\JsonSerializer;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+use WBW\Bundle\WidgetBundle\Toast\ToastInterface;
+use WBW\Library\Serializer\SerializerKeys as BaseSerializerKeys;
+
+/**
+ * JSON serializer test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Serializer
+ */
+class JsonSerializerTest extends AbstractTestCase {
+
+    /**
+     * Test serializeAlert()
+     *
+     * @return void
+     */
+    public function testSerializeAlert(): void {
+
+        // Set an Alert mock.
+        $model = $this->getMockBuilder(AlertInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeAlert($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeBadge()
+     *
+     * @return void
+     */
+    public function testSerializeBadge(): void {
+
+        // Set a Badge mock.
+        $model = $this->getMockBuilder(BadgeInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeBadge($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeButton()
+     *
+     * @return void
+     */
+    public function testSerializeButton(): void {
+
+        // Set a Button mock.
+        $model = $this->getMockBuilder(ButtonInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeButton($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeFullCalendarEvent()
+     *
+     * @return void
+     */
+    public function testSerializeFullCalendarEvent(): void {
+
+        // Set the date/time mocks.
+        $start = new DateTime("2021-11-29 10:50:00.000000");
+        $end   = new DateTime("2021-11-29 11:00:00.000000");
+
+        // Set a FullCalendarEvent mock.
+        $model = $this->getMockBuilder(FullCalendarEventInterface::class)->getMock();
+        $model->expects($this->any())->method("getId")->willReturn("id");
+        $model->expects($this->any())->method("getGroupId")->willReturn("groupId");
+        $model->expects($this->any())->method("getAllDay")->willReturn(true);
+        $model->expects($this->any())->method("getStart")->willReturn($start);
+        $model->expects($this->any())->method("getEnd")->willReturn($end);
+        $model->expects($this->any())->method("getStartStr")->willReturn("startStr");
+        $model->expects($this->any())->method("getEndStr")->willReturn("endStr");
+        $model->expects($this->any())->method("getTitle")->willReturn("title");
+        $model->expects($this->any())->method("getUrl")->willReturn("url");
+        $model->expects($this->any())->method("getClassNames")->willReturn(["className"]);
+        $model->expects($this->any())->method("getEditable")->willReturn(false);
+        $model->expects($this->any())->method("getStartEditable")->willReturn(true);
+        $model->expects($this->any())->method("getDurationEditable")->willReturn(false);
+        $model->expects($this->any())->method("getResourceEditable")->willReturn(true);
+        $model->expects($this->any())->method("getDisplay")->willReturn("none");
+        $model->expects($this->any())->method("getBackgroundColor")->willReturn("backgroundColor");
+        $model->expects($this->any())->method("getBorderColor")->willReturn("borderColor");
+        $model->expects($this->any())->method("getTextColor")->willReturn("textColor");
+
+        $res = JsonSerializer::serializeFullCalendarEvent($model);
+        $this->assertCount(19, $res);
+
+        $this->assertEquals($model->getId(), $res["id"]);
+        $this->assertEquals($model->getGroupId(), $res["groupId"]);
+        $this->assertEquals($model->getAllDay(), $res["allDay"]);
+        $this->assertEquals($model->getStart()->format("Y-m-d\TH:i:s"), $res["start"]);
+        $this->assertEquals($model->getEnd()->format("Y-m-d\TH:i:s"), $res["end"]);
+        $this->assertEquals($model->getStartStr(), $res["startStr"]);
+        $this->assertEquals($model->getEndStr(), $res["endStr"]);
+        $this->assertEquals($model->getTitle(), $res["title"]);
+        $this->assertEquals($model->getUrl(), $res["url"]);
+        $this->assertEquals($model->getClassNames(), $res["classNames"]);
+        $this->assertEquals($model->getEditable(), $res["editable"]);
+        $this->assertEquals($model->getStartEditable(), $res["startEditable"]);
+        $this->assertEquals($model->getDurationEditable(), $res["durationEditable"]);
+        $this->assertEquals($model->getResourceEditable(), $res["resourceEditable"]);
+        $this->assertEquals($model->getDisplay(), $res["display"]);
+        $this->assertEquals($model->getBackgroundColor(), $res["backgroundColor"]);
+        $this->assertEquals($model->getBorderColor(), $res["borderColor"]);
+        $this->assertEquals($model->getTextColor(), $res["textColor"]);
+    }
+
+    /**
+     * Test serializeIcon()
+     *
+     * @return void
+     */
+    public function testSerializeIcon(): void {
+
+        // Set a Icon mock.
+        $model = $this->getMockBuilder(IconInterface::class)->getMock();
+        $model->expects($this->any())->method("getName")->willReturn(BaseSerializerKeys::NAME);
+        $model->expects($this->any())->method("getStyle")->willReturn(BaseSerializerKeys::STYLE);
+
+        $res = JsonSerializer::serializeIcon($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getName(), $res[BaseSerializerKeys::NAME]);
+        $this->assertEquals($model->getStyle(), $res[BaseSerializerKeys::STYLE]);
+    }
+
+    /**
+     * Test serializeLabel()
+     *
+     * @return void
+     */
+    public function testSerializeLabel(): void {
+
+        // Set a Label mock.
+        $model = $this->getMockBuilder(LabelInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeLabel($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeNotification()
+     *
+     * @return void
+     */
+    public function testSerializeNotification(): void {
+
+        // Set a Notification mock.
+        $model = $this->getMockBuilder(NotificationInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeNotification($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeProgressBar()
+     *
+     * @return void
+     */
+    public function testSerializeProgressBar(): void {
+
+        // Set a Progress bar mock.
+        $model = $this->getMockBuilder(ProgressBarInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeProgressBar($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+
+    /**
+     * Test serializeToast()
+     *
+     * @return void
+     */
+    public function testSerializeToast(): void {
+
+        // Set a Toast mock.
+        $model = $this->getMockBuilder(ToastInterface::class)->getMock();
+        $model->expects($this->any())->method("getContent")->willReturn(BaseSerializerKeys::CONTENT);
+        $model->expects($this->any())->method("getType")->willReturn(BaseSerializerKeys::TYPE);
+
+        $res = JsonSerializer::serializeToast($model);
+        $this->assertCount(2, $res);
+
+        $this->assertEquals($model->getContent(), $res[BaseSerializerKeys::CONTENT]);
+        $this->assertEquals($model->getType(), $res[BaseSerializerKeys::TYPE]);
+    }
+}
