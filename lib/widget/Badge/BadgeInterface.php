0b8be9f04bc8a363ede033a119b409ef39bc3dbd
webeweb <support@webeweb.fr>
2024-04-16 15:58:33 +0200

    Copy Badge interface (copy from core-library)

---
+++ lib/widget/Badge/BadgeInterface.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Badge;
+
+use JsonSerializable;
+
+/**
+ * Badge interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Badge
+ */
+interface BadgeInterface extends JsonSerializable {
+
+    /**
+     * Badge type "danger".
+     *
+     * @var string
+     */
+    public const BADGE_TYPE_DANGER = "danger";
+
+    /**
+     * Badge type "info".
+     *
+     * @var string
+     */
+    public const BADGE_TYPE_INFO = "info";
+
+    /**
+     * Badge type "success".
+     *
+     * @var string
+     */
+    public const BADGE_TYPE_SUCCESS = "success";
+
+    /**
+     * Badge type "warning".
+     *
+     * @var string
+     */
+    public const BADGE_TYPE_WARNING = "warning";
+
+    /**
+     * Get the content.
+     *
+     * @return string|null Returns the content.
+     */
+    public function getContent(): ?string;
+
+    /**
+     * Get the type.
+     *
+     * @return string|null Returns the type.
+     */
+    public function getType(): ?string;
+
+    /**
+     * Set the content.
+     *
+     * @param string|null $content The content.
+     * @return BadgeInterface Returns this badge.
+     */
+    public function setContent(?string $content);
+}
