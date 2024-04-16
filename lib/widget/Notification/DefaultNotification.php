29415a2f76bd54ab763177f5e73f81f150bcb255
webeweb <support@webeweb.fr>
2024-04-16 16:07:53 +0200

    Copy Default notification (from core-library)

---
+++ lib/widget/Notification/DefaultNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Notification;
+
+/**
+ * Default notification.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Notification
+ */
+class DefaultNotification extends AbstractNotification {
+
+    /**
+     * Constructor.
+     *
+     * @param string $type The type.
+     * @param string $content The content.
+     */
+    public function __construct(string $type, string $content) {
+        parent::__construct($type, $content);
+    }
+}
---
+++ lib/widget/Tests/Notification/DefaultNotificationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Notification;
+
+use WBW\Bundle\WidgetBundle\Notification\DefaultNotification;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Default notification test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Notification
+ */
+class DefaultNotificationTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DefaultNotification("type", "default");
+
+        $this->assertEquals("type", $obj->getType());
+        $this->assertEquals("default", $obj->getContent());
+    }
+}
