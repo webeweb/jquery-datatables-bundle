66477e830187428b460f12c1417036c99f8d1339
webeweb <support@webeweb.fr>
2024-04-16 16:07:42 +0200

    Copy Danger notification (from core-library)

---
+++ lib/widget/Notification/DangerNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Notification;
+
+/**
+ * Danger notification.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Notification
+ */
+class DangerNotification extends AbstractNotification {
+
+    /**
+     * Constructor.
+     *
+     * @param string $content The content.
+     */
+    public function __construct(string $content) {
+        parent::__construct(self::NOTIFICATION_TYPE_DANGER, $content);
+    }
+}
