c0e60e5de7c6121cfb39cf7a220651fb82d0e9fa
webeweb <support@webeweb.fr>
2024-04-16 11:28:37 +0200

    Copy Uploaded file service interface (from core-library)

---
+++ lib/common/Service/UploadedFileServiceInterface.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-library package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Service;
+
+use SplFileInfo;
+use Throwable;
+
+/**
+ * Uploaded file service interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Service
+ */
+interface UploadedFileServiceInterface {
+
+    /**
+     * Determine if a filename exists.
+     *
+     * @param string $filename The filename.
+     * @return bool Returns true in case of success, false otherwise.
+     */
+    public function exists(string $filename): bool;
+
+    /**
+     * Get the directory.
+     *
+     * @return string|null Returns the directory.
+     */
+    public function getDirectory(): ?string;
+
+    /**
+     * Path.
+     *
+     * @param string $filename The filename.
+     * @return string Returns the path.
+     */
+    public function path(string $filename): string;
+
+    /**
+     * Save.
+     *
+     * @param SplFileInfo $uploadedFile The uploaded file.
+     * @param string $subdirectory The subdirectory.
+     * @param string|null $filename The filename.
+     * @return string|null Returns the uploaded file path.
+     */
+    public function save(SplFileInfo $uploadedFile, string $subdirectory, string $filename = null): ?string;
+
+    /**
+     * Unique id.
+     *
+     * @return string Returns the unique id.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function uniqId(): string;
+
+    /**
+     * Unlink.
+     *
+     * @param string $filename The filename.
+     * @return bool|null Returns true in case of success, false otherwise.
+     */
+    public function unlink(string $filename): ?bool;
+}
