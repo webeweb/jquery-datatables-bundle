b435489e82821e8e0f1fd8320c1de37dc7239086
webeweb <support@webeweb.fr>
2024-04-16 16:07:20 +0200

    Copy abstract toast (from core-library)

---
+++ lib/widget/Tests/Fixtures/Toast/TestToast.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Fixtures\Toast;
+
+use WBW\Bundle\WidgetBundle\Toast\AbstractToast;
+use WBW\Bundle\WidgetBundle\Toast\ToastInterface;
+
+/**
+ * Test toast.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Fixtures\Toast
+ */
+class TestToast extends AbstractToast {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct("t", "c");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContent(string $content): ToastInterface {
+        return parent::setContent($content);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setType(string $type): ToastInterface {
+        return parent::setType($type);
+    }
+}
