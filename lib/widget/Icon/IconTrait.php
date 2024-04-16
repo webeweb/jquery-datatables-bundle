990cb1d89b93d9878c7ea54688c3acdc6345bbe3
webeweb <support@webeweb.fr>
2024-04-16 16:02:18 +0200

    Copy Icon trait (copy from core-library)

---
+++ lib/widget/Icon/IconTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Icon;
+
+/**
+ * Icon trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Icon
+ */
+trait IconTrait {
+
+    /**
+     * Icon.
+     *
+     * @var IconInterface|null
+     */
+    protected $icon;
+
+    /**
+     * Get the icon.
+     *
+     * @return IconInterface|null Returns the icon.
+     */
+    public function getIcon(): ?IconInterface {
+        return $this->icon;
+    }
+
+    /**
+     * Set the icon.
+     *
+     * @param IconInterface|null $icon The icon.
+     * @return self Returns this instance.
+     */
+    protected function setIcon(?IconInterface $icon): self {
+        $this->icon = $icon;
+        return $this;
+    }
+}
