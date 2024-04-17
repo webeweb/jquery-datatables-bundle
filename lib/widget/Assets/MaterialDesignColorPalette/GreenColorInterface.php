627a5dd2aa2e89d25104539b30ee81846b85dd04
webeweb <support@webeweb.fr>
2024-04-17 14:04:36 +0200

    Copy Green color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/GreenColorInterface.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\GreenColorInterface as BaseGreenColorInterface;
+
+/**
+ * Green color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface GreenColorInterface extends BaseGreenColorInterface {
+
+    /**
+     * Green color "100"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_100 = "#C8E6C9";
+
+    /**
+     * Green color "200"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_200 = "#A5D6A7";
+
+    /**
+     * Green color "300"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_300 = "#81C784";
+
+    /**
+     * Green color "400"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_400 = "#66BB6A";
+
+    /**
+     * Green color "50"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_50 = "#E8F5E9";
+
+    /**
+     * Green color "500"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_500 = "#4CAF50";
+
+    /**
+     * Green color "600"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_600 = "#43A047";
+
+    /**
+     * Green color "700"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_700 = "#388E3C";
+
+    /**
+     * Green color "800"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_800 = "#2E7D32";
+
+    /**
+     * Green color "900"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_900 = "#1B5E20";
+
+    /**
+     * Green color "A100"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_A100 = "#B9F6CA";
+
+    /**
+     * Green color "A200"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_A200 = "#69F0AE";
+
+    /**
+     * Green color "A400"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_A400 = "#00E676";
+
+    /**
+     * Green color "A700"
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_A700 = "#00C853";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/GreenColorInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\GreenColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Green color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class GreenColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#E8F5E9", GreenColorInterface::GREEN_COLOR_50);
+        $this->assertEquals("#C8E6C9", GreenColorInterface::GREEN_COLOR_100);
+        $this->assertEquals("#A5D6A7", GreenColorInterface::GREEN_COLOR_200);
+        $this->assertEquals("#81C784", GreenColorInterface::GREEN_COLOR_300);
+        $this->assertEquals("#66BB6A", GreenColorInterface::GREEN_COLOR_400);
+        $this->assertEquals("#4CAF50", GreenColorInterface::GREEN_COLOR_500);
+        $this->assertEquals("#43A047", GreenColorInterface::GREEN_COLOR_600);
+        $this->assertEquals("#388E3C", GreenColorInterface::GREEN_COLOR_700);
+        $this->assertEquals("#2E7D32", GreenColorInterface::GREEN_COLOR_800);
+        $this->assertEquals("#1B5E20", GreenColorInterface::GREEN_COLOR_900);
+        $this->assertEquals("#B9F6CA", GreenColorInterface::GREEN_COLOR_A100);
+        $this->assertEquals("#69F0AE", GreenColorInterface::GREEN_COLOR_A200);
+        $this->assertEquals("#00E676", GreenColorInterface::GREEN_COLOR_A400);
+        $this->assertEquals("#00C853", GreenColorInterface::GREEN_COLOR_A700);
+    }
+}
