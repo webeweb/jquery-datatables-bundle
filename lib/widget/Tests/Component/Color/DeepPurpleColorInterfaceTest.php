3494e28167dd2ff2517d37e15e9d5ca70d678c3b
webeweb <support@webeweb.fr>
2024-04-17 11:07:31 +0200

    Copy Deep purple color provider interface (from core-library)

---
+++ lib/widget/Tests/Component/Color/DeepPurpleColorInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Component\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\DeepPurpleColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Deep purple color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Component\Color
+ */
+class DeepPurpleColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("deep-purple", DeepPurpleColorInterface::DEEP_PURPLE_COLOR_NAME);
+    }
+}
