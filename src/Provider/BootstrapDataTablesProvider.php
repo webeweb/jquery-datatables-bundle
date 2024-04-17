f7edf17b944b91f374571e57bee276c866444830
webeweb <support@webeweb.fr>
2024-04-17 16:03:51 +0200

    Update Bootstrap DataTables provider: - replace use statement

--- src/Provider/BootstrapDataTablesProvider.php
+++ src/Provider/BootstrapDataTablesProvider.php
@@ -27,10 +27,10 @@ use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ButtonTwigExtension;
 use WBW\Bundle\BootstrapBundle\Twig\Extension\Component\ButtonTwigExtensionTrait;
 use WBW\Bundle\CommonBundle\Routing\RouterTrait;
 use WBW\Bundle\DataTablesBundle\Renderer\BootstrapButtonRendererTrait;
-use WBW\Library\Symfony\Renderer\DateTimesRendererTrait;
-use WBW\Library\Symfony\Renderer\Floats\FloatRendererTrait;
-use WBW\Library\Symfony\Renderer\Strings\StringWrapperTrait;
-use WBW\Library\Symfony\Renderer\StringsRendererTrait;
+use WBW\Bundle\WidgetBundle\Renderer\DateTimesRendererTrait;
+use WBW\Bundle\WidgetBundle\Renderer\Floats\FloatRendererTrait;
+use WBW\Bundle\WidgetBundle\Renderer\Strings\StringWrapperTrait;
+use WBW\Bundle\WidgetBundle\Renderer\StringsRendererTrait;
 
 /**
  * Bootstrap DataTables provider.
