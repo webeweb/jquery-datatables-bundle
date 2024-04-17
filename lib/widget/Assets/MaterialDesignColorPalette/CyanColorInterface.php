dba18fe366b7b5edd667153963dbc03c6626e88f
webeweb <support@webeweb.fr>
2024-04-17 14:04:10 +0200

    Copy Cyan color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/CyanColorInterface.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\CyanColorInterface as BaseCyanColorInterface;
+
+/**
+ * Cyan color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface CyanColorInterface extends BaseCyanColorInterface {
+
+    /**
+     * Cyan color "100"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_100 = "#B2EBF2";
+
+    /**
+     * Cyan color "200"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_200 = "#80DEEA";
+
+    /**
+     * Cyan color "300"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_300 = "#4DD0E1";
+
+    /**
+     * Cyan color "400"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_400 = "#26C6DA";
+
+    /**
+     * Cyan color "50"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_50 = "#E0F7FA";
+
+    /**
+     * Cyan color "500"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_500 = "#00BCD4";
+
+    /**
+     * Cyan color "600"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_600 = "#00ACC1";
+
+    /**
+     * Cyan color "700"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_700 = "#0097A7";
+
+    /**
+     * Cyan color "800"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_800 = "#00838F";
+
+    /**
+     * Cyan color "900"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_900 = "#006064";
+
+    /**
+     * Cyan color "A100"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_A100 = "#84FFFF";
+
+    /**
+     * Cyan color "A200"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_A200 = "#18FFFF";
+
+    /**
+     * Cyan color "A400"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_A400 = "#00E5FF";
+
+    /**
+     * Cyan color "A700"
+     *
+     * @var string
+     */
+    public const CYAN_COLOR_A700 = "#00B8D4";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/CyanColorInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\CyanColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Cyan color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class CyanColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#E0F7FA", CyanColorInterface::CYAN_COLOR_50);
+        $this->assertEquals("#B2EBF2", CyanColorInterface::CYAN_COLOR_100);
+        $this->assertEquals("#80DEEA", CyanColorInterface::CYAN_COLOR_200);
+        $this->assertEquals("#4DD0E1", CyanColorInterface::CYAN_COLOR_300);
+        $this->assertEquals("#26C6DA", CyanColorInterface::CYAN_COLOR_400);
+        $this->assertEquals("#00BCD4", CyanColorInterface::CYAN_COLOR_500);
+        $this->assertEquals("#00ACC1", CyanColorInterface::CYAN_COLOR_600);
+        $this->assertEquals("#0097A7", CyanColorInterface::CYAN_COLOR_700);
+        $this->assertEquals("#00838F", CyanColorInterface::CYAN_COLOR_800);
+        $this->assertEquals("#006064", CyanColorInterface::CYAN_COLOR_900);
+        $this->assertEquals("#84FFFF", CyanColorInterface::CYAN_COLOR_A100);
+        $this->assertEquals("#18FFFF", CyanColorInterface::CYAN_COLOR_A200);
+        $this->assertEquals("#00E5FF", CyanColorInterface::CYAN_COLOR_A400);
+        $this->assertEquals("#00B8D4", CyanColorInterface::CYAN_COLOR_A700);
+    }
+}
