5a8dff63e3c4fe3fdfdae700ace47752da570736
webeweb <support@webeweb.fr>
2024-04-17 14:05:02 +0200

    Copy Light blue color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/LightBlueColorInterface.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\LightBlueColorInterface as BaseLightBlueColorInterface;
+
+/**
+ * Light blue color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface LightBlueColorInterface extends BaseLightBlueColorInterface {
+
+    /**
+     * Light blue color "100"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_100 = "#B3E5FC";
+
+    /**
+     * Light blue color "200"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_200 = "#81D4FA";
+
+    /**
+     * Light blue color "300"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_300 = "#4FC3F7";
+
+    /**
+     * Light blue color "400"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_400 = "#29B6F6";
+
+    /**
+     * Light blue color "50"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_50 = "#E1F5FE";
+
+    /**
+     * Light blue color "500"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_500 = "#03A9F4";
+
+    /**
+     * Light blue color "600"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_600 = "#039BE5";
+
+    /**
+     * Light blue color "700"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_700 = "#0288D1";
+
+    /**
+     * Light blue color "800"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_800 = "#0277BD";
+
+    /**
+     * Light blue color "900"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_900 = "#01579B";
+
+    /**
+     * Light blue color "A100"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_A100 = "#80D8FF";
+
+    /**
+     * Light blue color "A200"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_A200 = "#40C4FF";
+
+    /**
+     * Light blue color "A400"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_A400 = "#00B0FF";
+
+    /**
+     * Light blue color "A700"
+     *
+     * @var string
+     */
+    public const LIGHT_BLUE_COLOR_A700 = "#0091EA";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/LightBlueColorInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\LightBlueColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Light blue color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class LightBlueColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#E1F5FE", LightBlueColorInterface::LIGHT_BLUE_COLOR_50);
+        $this->assertEquals("#B3E5FC", LightBlueColorInterface::LIGHT_BLUE_COLOR_100);
+        $this->assertEquals("#81D4FA", LightBlueColorInterface::LIGHT_BLUE_COLOR_200);
+        $this->assertEquals("#4FC3F7", LightBlueColorInterface::LIGHT_BLUE_COLOR_300);
+        $this->assertEquals("#29B6F6", LightBlueColorInterface::LIGHT_BLUE_COLOR_400);
+        $this->assertEquals("#03A9F4", LightBlueColorInterface::LIGHT_BLUE_COLOR_500);
+        $this->assertEquals("#039BE5", LightBlueColorInterface::LIGHT_BLUE_COLOR_600);
+        $this->assertEquals("#0288D1", LightBlueColorInterface::LIGHT_BLUE_COLOR_700);
+        $this->assertEquals("#0277BD", LightBlueColorInterface::LIGHT_BLUE_COLOR_800);
+        $this->assertEquals("#01579B", LightBlueColorInterface::LIGHT_BLUE_COLOR_900);
+        $this->assertEquals("#80D8FF", LightBlueColorInterface::LIGHT_BLUE_COLOR_A100);
+        $this->assertEquals("#40C4FF", LightBlueColorInterface::LIGHT_BLUE_COLOR_A200);
+        $this->assertEquals("#00B0FF", LightBlueColorInterface::LIGHT_BLUE_COLOR_A400);
+        $this->assertEquals("#0091EA", LightBlueColorInterface::LIGHT_BLUE_COLOR_A700);
+    }
+}
