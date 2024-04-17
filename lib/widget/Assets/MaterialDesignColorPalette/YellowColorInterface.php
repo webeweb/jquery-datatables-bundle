0997a8b90b4ad5b9916210f857a5f4be60200eae
webeweb <support@webeweb.fr>
2024-04-17 14:06:09 +0200

    Copy Yellow color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/YellowColorInterface.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\YellowColorInterface as BaseYellowColorInterface;
+
+/**
+ * Yellow color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface YellowColorInterface extends BaseYellowColorInterface {
+
+    /**
+     * Yellow color "100"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_100 = "#FFF9C4";
+
+    /**
+     * Yellow color "200"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_200 = "#FFF59D";
+
+    /**
+     * Yellow color "300"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_300 = "#FFF176";
+
+    /**
+     * Yellow color "400"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_400 = "#FFEE58";
+
+    /**
+     * Yellow color "50"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_50 = "#FFFDE7";
+
+    /**
+     * Yellow color "500"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_500 = "#FFEB3B";
+
+    /**
+     * Yellow color "600"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_600 = "#FDD835";
+
+    /**
+     * Yellow color "700"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_700 = "#FBC02D";
+
+    /**
+     * Yellow color "800"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_800 = "#F9A825";
+
+    /**
+     * Yellow color "900"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_900 = "#F57F17";
+
+    /**
+     * Yellow color "A100"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_A100 = "#FFFF8D";
+
+    /**
+     * Yellow color "A200"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_A200 = "#FFFF00";
+
+    /**
+     * Yellow color "A400"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_A400 = "#FFEA00";
+
+    /**
+     * Yellow color "A700"
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_A700 = "#FFD600";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/YellowColorInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\YellowColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Yellow color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class YellowColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#FFFDE7", YellowColorInterface::YELLOW_COLOR_50);
+        $this->assertEquals("#FFF9C4", YellowColorInterface::YELLOW_COLOR_100);
+        $this->assertEquals("#FFF59D", YellowColorInterface::YELLOW_COLOR_200);
+        $this->assertEquals("#FFF176", YellowColorInterface::YELLOW_COLOR_300);
+        $this->assertEquals("#FFEE58", YellowColorInterface::YELLOW_COLOR_400);
+        $this->assertEquals("#FFEB3B", YellowColorInterface::YELLOW_COLOR_500);
+        $this->assertEquals("#FDD835", YellowColorInterface::YELLOW_COLOR_600);
+        $this->assertEquals("#FBC02D", YellowColorInterface::YELLOW_COLOR_700);
+        $this->assertEquals("#F9A825", YellowColorInterface::YELLOW_COLOR_800);
+        $this->assertEquals("#F57F17", YellowColorInterface::YELLOW_COLOR_900);
+        $this->assertEquals("#FFFF8D", YellowColorInterface::YELLOW_COLOR_A100);
+        $this->assertEquals("#FFFF00", YellowColorInterface::YELLOW_COLOR_A200);
+        $this->assertEquals("#FFEA00", YellowColorInterface::YELLOW_COLOR_A400);
+        $this->assertEquals("#FFD600", YellowColorInterface::YELLOW_COLOR_A700);
+    }
+}
