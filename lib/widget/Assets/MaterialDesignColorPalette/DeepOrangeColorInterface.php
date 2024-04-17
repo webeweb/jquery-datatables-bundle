2700bafbeaa49bcc23b32425eed687f633b2908a
webeweb <support@webeweb.fr>
2024-04-17 14:04:18 +0200

    Copy Deep orange color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/DeepOrangeColorInterface.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\DeepOrangeColorInterface as BaseDeepOrangeColorInterface;
+
+/**
+ * Deep orange color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface DeepOrangeColorInterface extends BaseDeepOrangeColorInterface {
+
+    /**
+     * Deep orange color "100"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_100 = "#FFCCBC";
+
+    /**
+     * Deep orange color "200"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_200 = "#FFAB91";
+
+    /**
+     * Deep orange color "300"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_300 = "#FF8A65";
+
+    /**
+     * Deep orange color "400"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_400 = "#FF7043";
+
+    /**
+     * Deep orange color "50"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_50 = "#FBE9E7";
+
+    /**
+     * Deep orange color "500"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_500 = "#FF5722";
+
+    /**
+     * Deep orange color "600"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_600 = "#F4511E";
+
+    /**
+     * Deep orange color "700"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_700 = "#E64A19";
+
+    /**
+     * Deep orange color "800"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_800 = "#D84315";
+
+    /**
+     * Deep orange color "900"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_900 = "#BF360C";
+
+    /**
+     * Deep orange color "A100"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_A100 = "#FF9E80";
+
+    /**
+     * Deep orange color "A200"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_A200 = "#FF6E40";
+
+    /**
+     * Deep orange color "A400"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_A400 = "#FF3D00";
+
+    /**
+     * Deep orange color "A700"
+     *
+     * @var string
+     */
+    public const DEEP_ORANGE_COLOR_A700 = "#DD2C00";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/DeepOrangeColorInterfaceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\DeepOrangeColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Deep orange color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class DeepOrangeColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#FBE9E7", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_50);
+        $this->assertEquals("#FFCCBC", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_100);
+        $this->assertEquals("#FFAB91", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_200);
+        $this->assertEquals("#FF8A65", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_300);
+        $this->assertEquals("#FF7043", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_400);
+        $this->assertEquals("#FF5722", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_500);
+        $this->assertEquals("#F4511E", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_600);
+        $this->assertEquals("#E64A19", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_700);
+        $this->assertEquals("#D84315", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_800);
+        $this->assertEquals("#BF360C", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_900);
+        $this->assertEquals("#FF9E80", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_A100);
+        $this->assertEquals("#FF6E40", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_A200);
+        $this->assertEquals("#FF3D00", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_A400);
+        $this->assertEquals("#DD2C00", DeepOrangeColorInterface::DEEP_ORANGE_COLOR_A700);
+    }
+}
