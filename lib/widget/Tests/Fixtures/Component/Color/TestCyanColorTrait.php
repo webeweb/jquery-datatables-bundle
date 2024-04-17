8701fbf71fab91f5f4696ce5699cfe776d97af80
webeweb <support@webeweb.fr>
2024-04-17 11:11:12 +0200

    Copy Cyan color provider trait (from core-library)

---
+++ lib/widget/Tests/Fixtures/Component/Color/TestCyanColorTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Fixtures\Component\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\CyanColorTrait;
+
+/**
+ * Test cyan color trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Fixtures\Component\Color
+ */
+class TestCyanColorTrait {
+
+    use CyanColorTrait {
+        setCyanColor as public;
+    }
+}
