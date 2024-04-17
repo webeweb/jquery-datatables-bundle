a0ec30d16020b7d2777fe92969a5098a96ac9258
webeweb <support@webeweb.fr>
2024-04-17 14:04:02 +0200

    Copy Brown color provider interface (from core-library)

---
+++ lib/widget/Assets/MaterialDesignColorPalette/BrownColorInterface.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Component\Color\BrownColorInterface as BaseBrownColorInterface;
+
+/**
+ * Brown color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette
+ */
+interface BrownColorInterface extends BaseBrownColorInterface {
+
+    /**
+     * Brown color "100"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_100 = "#D7CCC8";
+
+    /**
+     * Brown color "200"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_200 = "#BCAAA4";
+
+    /**
+     * Brown color "300"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_300 = "#A1887F";
+
+    /**
+     * Brown color "400"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_400 = "#8D6E63";
+
+    /**
+     * Brown color "50"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_50 = "#EFEBE9";
+
+    /**
+     * Brown color "500"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_500 = "#795548";
+
+    /**
+     * Brown color "600"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_600 = "#6D4C41";
+
+    /**
+     * Brown color "700"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_700 = "#5D4037";
+
+    /**
+     * Brown color "800"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_800 = "#4E342E";
+
+    /**
+     * Brown color "900"
+     *
+     * @var string
+     */
+    public const BROWN_COLOR_900 = "#3E2723";
+}
---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/BrownColorInterfaceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\BrownColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Brown color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class BrownColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#EFEBE9", BrownColorInterface::BROWN_COLOR_50);
+        $this->assertEquals("#D7CCC8", BrownColorInterface::BROWN_COLOR_100);
+        $this->assertEquals("#BCAAA4", BrownColorInterface::BROWN_COLOR_200);
+        $this->assertEquals("#A1887F", BrownColorInterface::BROWN_COLOR_300);
+        $this->assertEquals("#8D6E63", BrownColorInterface::BROWN_COLOR_400);
+        $this->assertEquals("#795548", BrownColorInterface::BROWN_COLOR_500);
+        $this->assertEquals("#6D4C41", BrownColorInterface::BROWN_COLOR_600);
+        $this->assertEquals("#5D4037", BrownColorInterface::BROWN_COLOR_700);
+        $this->assertEquals("#4E342E", BrownColorInterface::BROWN_COLOR_800);
+        $this->assertEquals("#3E2723", BrownColorInterface::BROWN_COLOR_900);
+    }
+}
