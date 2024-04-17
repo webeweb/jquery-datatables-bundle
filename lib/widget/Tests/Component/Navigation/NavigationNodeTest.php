0dcd278adf22a84f3854f47d1bf398c84850d279
webeweb <support@webeweb.fr>
2024-04-17 17:16:31 +0200

    Copy Navigation node (from core-library)

---
+++ lib/widget/Tests/Component/Navigation/NavigationNodeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Component\Navigation;
+
+use WBW\Bundle\WidgetBundle\Component\Navigation\NavigationNode;
+use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * Navigation node test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Component\Navigation
+ */
+class NavigationNodeTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new NavigationNode("id");
+
+        $this->assertFalse($obj->getActive());
+        $this->assertFalse($obj->getEnable());
+        $this->assertNull($obj->getIcon());
+        $this->assertEquals(NavigationNodeInterface::MATCHER_URL, $obj->getMatcher());
+        $this->assertNull($obj->getTarget());
+        $this->assertNull($obj->getUri());
+        $this->assertTrue($obj->getVisible());
+    }
+}
