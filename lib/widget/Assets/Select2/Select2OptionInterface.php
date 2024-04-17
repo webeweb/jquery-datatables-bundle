f984a79b429b6ec9506a95a1c9907ada11a323aa
webeweb <support@webeweb.fr>
2024-04-17 09:57:08 +0200

    Copy Select2 option interface (from core-library)

---
+++ lib/widget/Assets/Select2/Select2OptionInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Assets\Select2;
+
+/**
+ * Select2 option interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Symfony\Assets
+ */
+interface Select2OptionInterface {
+
+    /**
+     * Get a Select2 option "id".
+     *
+     * @return mixed Returns the Select2 option "id".
+     */
+    public function getSelect2OptionId();
+
+    /**
+     * Get a Select2 option "text".
+     *
+     * @return string Returns the Select2 option "text".
+     */
+    public function getSelect2OptionText(): ?string;
+}
