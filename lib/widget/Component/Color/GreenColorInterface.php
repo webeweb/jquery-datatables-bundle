35945790aa7c1f43ac1991cfdd4749147972cabf
webeweb <support@webeweb.fr>
2024-04-17 11:07:42 +0200

    Copy Green color provider interface (from core-library)

---
+++ lib/widget/Component/Color/GreenColorInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Component\Color;
+
+use WBW\Bundle\WidgetBundle\Component\ColorInterface;
+
+/**
+ * Green color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Component\Color
+ */
+interface GreenColorInterface extends ColorInterface {
+
+    /**
+     * Green color name.
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_NAME = "green";
+}
