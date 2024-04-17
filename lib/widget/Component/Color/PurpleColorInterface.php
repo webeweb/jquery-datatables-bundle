08961ceb6466207b414fe11e3028fdfdd0042c3c
webeweb <support@webeweb.fr>
2024-04-17 11:09:17 +0200

    Copy Purple color provider interface (from core-library)

---
+++ lib/widget/Component/Color/PurpleColorInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Component\Color;
+
+use WBW\Bundle\WidgetBundle\Component\ColorInterface;
+
+/**
+ * Purple color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Component\Color
+ */
+interface PurpleColorInterface extends ColorInterface {
+
+    /**
+     * Purple color name.
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_NAME = "purple";
+}
