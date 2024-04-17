3e58006c7749fcc235547b5e982a5fbc09037026
webeweb <support@webeweb.fr>
2024-04-17 15:04:26 +0200

    Update Indigo color interface: - rename constants

--- lib/widget/Assets/MaterialDesignColorPalette/IndigoColorInterface.php
+++ lib/widget/Assets/MaterialDesignColorPalette/IndigoColorInterface.php
@@ -24,100 +24,100 @@ use WBW\Bundle\WidgetBundle\Component\Color\IndigoColorInterface as BaseIndigoCo
 interface IndigoColorInterface extends BaseIndigoColorInterface {
 
     /**
-     * Indigo color "100"
+     * Indigo color value "100"
      *
      * @var string
      */
-    public const INDIGO_COLOR_100 = "#C5CAE9";
+    public const INDIGO_COLOR_VALUE_100 = "#C5CAE9";
 
     /**
-     * Indigo color "200"
+     * Indigo color value "200"
      *
      * @var string
      */
-    public const INDIGO_COLOR_200 = "#9FA8DA";
+    public const INDIGO_COLOR_VALUE_200 = "#9FA8DA";
 
     /**
-     * Indigo color "300"
+     * Indigo color value "300"
      *
      * @var string
      */
-    public const INDIGO_COLOR_300 = "#7986CB";
+    public const INDIGO_COLOR_VALUE_300 = "#7986CB";
 
     /**
-     * Indigo color "400"
+     * Indigo color value "400"
      *
      * @var string
      */
-    public const INDIGO_COLOR_400 = "#5C6BC0";
+    public const INDIGO_COLOR_VALUE_400 = "#5C6BC0";
 
     /**
-     * Indigo color "50"
+     * Indigo color value "50"
      *
      * @var string
      */
-    public const INDIGO_COLOR_50 = "#E8EAF6";
+    public const INDIGO_COLOR_VALUE_50 = "#E8EAF6";
 
     /**
-     * Indigo color "500"
+     * Indigo color value "500"
      *
      * @var string
      */
-    public const INDIGO_COLOR_500 = "#3F51B5";
+    public const INDIGO_COLOR_VALUE_500 = "#3F51B5";
 
     /**
-     * Indigo color "600"
+     * Indigo color value "600"
      *
      * @var string
      */
-    public const INDIGO_COLOR_600 = "#3949AB";
+    public const INDIGO_COLOR_VALUE_600 = "#3949AB";
 
     /**
-     * Indigo color "700"
+     * Indigo color value "700"
      *
      * @var string
      */
-    public const INDIGO_COLOR_700 = "#303F9F";
+    public const INDIGO_COLOR_VALUE_700 = "#303F9F";
 
     /**
-     * Indigo color "800"
+     * Indigo color value "800"
      *
      * @var string
      */
-    public const INDIGO_COLOR_800 = "#283593";
+    public const INDIGO_COLOR_VALUE_800 = "#283593";
 
     /**
-     * Indigo color "900"
+     * Indigo color value "900"
      *
      * @var string
      */
-    public const INDIGO_COLOR_900 = "#1A237E";
+    public const INDIGO_COLOR_VALUE_900 = "#1A237E";
 
     /**
-     * Indigo color "A100"
+     * Indigo color value "A100"
      *
      * @var string
      */
-    public const INDIGO_COLOR_A100 = "#8C9EFF";
+    public const INDIGO_COLOR_VALUE_A100 = "#8C9EFF";
 
     /**
-     * Indigo color "A200"
+     * Indigo color value "A200"
      *
      * @var string
      */
-    public const INDIGO_COLOR_A200 = "#536DFE";
+    public const INDIGO_COLOR_VALUE_A200 = "#536DFE";
 
     /**
-     * Indigo color "A400"
+     * Indigo color value "A400"
      *
      * @var string
      */
-    public const INDIGO_COLOR_A400 = "#3D5AFE";
+    public const INDIGO_COLOR_VALUE_A400 = "#3D5AFE";
 
     /**
-     * Indigo color "A700"
+     * Indigo color value "A700"
      *
      * @var string
      */
-    public const INDIGO_COLOR_A700 = "#304FFE";
+    public const INDIGO_COLOR_VALUE_A700 = "#304FFE";
 }
--- lib/widget/Tests/Assets/MaterialDesignColorPalette/IndigoColorInterfaceTest.php
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/IndigoColorInterfaceTest.php
@@ -31,19 +31,19 @@ class IndigoColorInterfaceTest extends AbstractTestCase {
      */
     public function test__construct(): void {
 
-        $this->assertEquals("#E8EAF6", IndigoColorInterface::INDIGO_COLOR_50);
-        $this->assertEquals("#C5CAE9", IndigoColorInterface::INDIGO_COLOR_100);
-        $this->assertEquals("#9FA8DA", IndigoColorInterface::INDIGO_COLOR_200);
-        $this->assertEquals("#7986CB", IndigoColorInterface::INDIGO_COLOR_300);
-        $this->assertEquals("#5C6BC0", IndigoColorInterface::INDIGO_COLOR_400);
-        $this->assertEquals("#3F51B5", IndigoColorInterface::INDIGO_COLOR_500);
-        $this->assertEquals("#3949AB", IndigoColorInterface::INDIGO_COLOR_600);
-        $this->assertEquals("#303F9F", IndigoColorInterface::INDIGO_COLOR_700);
-        $this->assertEquals("#283593", IndigoColorInterface::INDIGO_COLOR_800);
-        $this->assertEquals("#1A237E", IndigoColorInterface::INDIGO_COLOR_900);
-        $this->assertEquals("#8C9EFF", IndigoColorInterface::INDIGO_COLOR_A100);
-        $this->assertEquals("#536DFE", IndigoColorInterface::INDIGO_COLOR_A200);
-        $this->assertEquals("#3D5AFE", IndigoColorInterface::INDIGO_COLOR_A400);
-        $this->assertEquals("#304FFE", IndigoColorInterface::INDIGO_COLOR_A700);
+        $this->assertEquals("#E8EAF6", IndigoColorInterface::INDIGO_COLOR_VALUE_50);
+        $this->assertEquals("#C5CAE9", IndigoColorInterface::INDIGO_COLOR_VALUE_100);
+        $this->assertEquals("#9FA8DA", IndigoColorInterface::INDIGO_COLOR_VALUE_200);
+        $this->assertEquals("#7986CB", IndigoColorInterface::INDIGO_COLOR_VALUE_300);
+        $this->assertEquals("#5C6BC0", IndigoColorInterface::INDIGO_COLOR_VALUE_400);
+        $this->assertEquals("#3F51B5", IndigoColorInterface::INDIGO_COLOR_VALUE_500);
+        $this->assertEquals("#3949AB", IndigoColorInterface::INDIGO_COLOR_VALUE_600);
+        $this->assertEquals("#303F9F", IndigoColorInterface::INDIGO_COLOR_VALUE_700);
+        $this->assertEquals("#283593", IndigoColorInterface::INDIGO_COLOR_VALUE_800);
+        $this->assertEquals("#1A237E", IndigoColorInterface::INDIGO_COLOR_VALUE_900);
+        $this->assertEquals("#8C9EFF", IndigoColorInterface::INDIGO_COLOR_VALUE_A100);
+        $this->assertEquals("#536DFE", IndigoColorInterface::INDIGO_COLOR_VALUE_A200);
+        $this->assertEquals("#3D5AFE", IndigoColorInterface::INDIGO_COLOR_VALUE_A400);
+        $this->assertEquals("#304FFE", IndigoColorInterface::INDIGO_COLOR_VALUE_A700);
     }
 }
