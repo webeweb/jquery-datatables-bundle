bee5ec9d5f03181677fea925ed5303122228968f
webeweb <support@webeweb.fr>
2024-04-17 14:06:03 +0200

    Copy White color provider interface (from core-library)

---
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/WhiteColorInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette;
+
+use WBW\Bundle\WidgetBundle\Assets\MaterialDesignColorPalette\WhiteColorInterface;
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+
+/**
+ * White color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Assets\MaterialDesignColorPalette
+ */
+class WhiteColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#FFFFFF", WhiteColorInterface::WHITE_COLOR_500);
+    }
+}
