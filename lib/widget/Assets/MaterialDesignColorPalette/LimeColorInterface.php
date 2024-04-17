a6feb50984d6ed1f60050b0e9b330332dc434a5b
webeweb <support@webeweb.fr>
2024-04-17 15:05:13 +0200

    Update Lime color interface: - rename constants

--- lib/widget/Assets/MaterialDesignColorPalette/LimeColorInterface.php
+++ lib/widget/Assets/MaterialDesignColorPalette/LimeColorInterface.php
@@ -24,100 +24,100 @@ use WBW\Bundle\WidgetBundle\Component\Color\LimeColorInterface as BaseLimeColorI
 interface LimeColorInterface extends BaseLimeColorInterface {
 
     /**
-     * Lime color "100"
+     * Lime color value "100"
      *
      * @var string
      */
-    public const LIME_COLOR_100 = "#F0F4C3";
+    public const LIME_COLOR_VALUE_100 = "#F0F4C3";
 
     /**
-     * Lime color "200"
+     * Lime color value "200"
      *
      * @var string
      */
-    public const LIME_COLOR_200 = "#E6EE9C";
+    public const LIME_COLOR_VALUE_200 = "#E6EE9C";
 
     /**
-     * Lime color "300"
+     * Lime color value "300"
      *
      * @var string
      */
-    public const LIME_COLOR_300 = "#DCE775";
+    public const LIME_COLOR_VALUE_300 = "#DCE775";
 
     /**
-     * Lime color "400"
+     * Lime color value "400"
      *
      * @var string
      */
-    public const LIME_COLOR_400 = "#D4E157";
+    public const LIME_COLOR_VALUE_400 = "#D4E157";
 
     /**
-     * Lime color "50"
+     * Lime color value "50"
      *
      * @var string
      */
-    public const LIME_COLOR_50 = "#F9FBE7";
+    public const LIME_COLOR_VALUE_50 = "#F9FBE7";
 
     /**
-     * Lime color "500"
+     * Lime color value "500"
      *
      * @var string
      */
-    public const LIME_COLOR_500 = "#CDDC39";
+    public const LIME_COLOR_VALUE_500 = "#CDDC39";
 
     /**
-     * Lime color "600"
+     * Lime color value "600"
      *
      * @var string
      */
-    public const LIME_COLOR_600 = "#C0CA33";
+    public const LIME_COLOR_VALUE_600 = "#C0CA33";
 
     /**
-     * Lime color "700"
+     * Lime color value "700"
      *
      * @var string
      */
-    public const LIME_COLOR_700 = "#AFB42B";
+    public const LIME_COLOR_VALUE_700 = "#AFB42B";
 
     /**
-     * Lime color "800"
+     * Lime color value "800"
      *
      * @var string
      */
-    public const LIME_COLOR_800 = "#9E9D24";
+    public const LIME_COLOR_VALUE_800 = "#9E9D24";
 
     /**
-     * Lime color "900"
+     * Lime color value "900"
      *
      * @var string
      */
-    public const LIME_COLOR_900 = "#827717";
+    public const LIME_COLOR_VALUE_900 = "#827717";
 
     /**
-     * Lime color "A100"
+     * Lime color value "A100"
      *
      * @var string
      */
-    public const LIME_COLOR_A100 = "#F4FF81";
+    public const LIME_COLOR_VALUE_A100 = "#F4FF81";
 
     /**
-     * Lime color "A200"
+     * Lime color value "A200"
      *
      * @var string
      */
-    public const LIME_COLOR_A200 = "#EEFF41";
+    public const LIME_COLOR_VALUE_A200 = "#EEFF41";
 
     /**
-     * Lime color "A400"
+     * Lime color value "A400"
      *
      * @var string
      */
-    public const LIME_COLOR_A400 = "#C6FF00";
+    public const LIME_COLOR_VALUE_A400 = "#C6FF00";
 
     /**
-     * Lime color "A700"
+     * Lime color value "A700"
      *
      * @var string
      */
-    public const LIME_COLOR_A700 = "#AEEA00";
+    public const LIME_COLOR_VALUE_A700 = "#AEEA00";
 }
--- lib/widget/Tests/Assets/MaterialDesignColorPalette/LimeColorInterfaceTest.php
+++ lib/widget/Tests/Assets/MaterialDesignColorPalette/LimeColorInterfaceTest.php
@@ -31,19 +31,19 @@ class LimeColorInterfaceTest extends AbstractTestCase {
      */
     public function test__construct(): void {
 
-        $this->assertEquals("#F9FBE7", LimeColorInterface::LIME_COLOR_50);
-        $this->assertEquals("#F0F4C3", LimeColorInterface::LIME_COLOR_100);
-        $this->assertEquals("#E6EE9C", LimeColorInterface::LIME_COLOR_200);
-        $this->assertEquals("#DCE775", LimeColorInterface::LIME_COLOR_300);
-        $this->assertEquals("#D4E157", LimeColorInterface::LIME_COLOR_400);
-        $this->assertEquals("#CDDC39", LimeColorInterface::LIME_COLOR_500);
-        $this->assertEquals("#C0CA33", LimeColorInterface::LIME_COLOR_600);
-        $this->assertEquals("#AFB42B", LimeColorInterface::LIME_COLOR_700);
-        $this->assertEquals("#9E9D24", LimeColorInterface::LIME_COLOR_800);
-        $this->assertEquals("#827717", LimeColorInterface::LIME_COLOR_900);
-        $this->assertEquals("#F4FF81", LimeColorInterface::LIME_COLOR_A100);
-        $this->assertEquals("#EEFF41", LimeColorInterface::LIME_COLOR_A200);
-        $this->assertEquals("#C6FF00", LimeColorInterface::LIME_COLOR_A400);
-        $this->assertEquals("#AEEA00", LimeColorInterface::LIME_COLOR_A700);
+        $this->assertEquals("#F9FBE7", LimeColorInterface::LIME_COLOR_VALUE_50);
+        $this->assertEquals("#F0F4C3", LimeColorInterface::LIME_COLOR_VALUE_100);
+        $this->assertEquals("#E6EE9C", LimeColorInterface::LIME_COLOR_VALUE_200);
+        $this->assertEquals("#DCE775", LimeColorInterface::LIME_COLOR_VALUE_300);
+        $this->assertEquals("#D4E157", LimeColorInterface::LIME_COLOR_VALUE_400);
+        $this->assertEquals("#CDDC39", LimeColorInterface::LIME_COLOR_VALUE_500);
+        $this->assertEquals("#C0CA33", LimeColorInterface::LIME_COLOR_VALUE_600);
+        $this->assertEquals("#AFB42B", LimeColorInterface::LIME_COLOR_VALUE_700);
+        $this->assertEquals("#9E9D24", LimeColorInterface::LIME_COLOR_VALUE_800);
+        $this->assertEquals("#827717", LimeColorInterface::LIME_COLOR_VALUE_900);
+        $this->assertEquals("#F4FF81", LimeColorInterface::LIME_COLOR_VALUE_A100);
+        $this->assertEquals("#EEFF41", LimeColorInterface::LIME_COLOR_VALUE_A200);
+        $this->assertEquals("#C6FF00", LimeColorInterface::LIME_COLOR_VALUE_A400);
+        $this->assertEquals("#AEEA00", LimeColorInterface::LIME_COLOR_VALUE_A700);
     }
 }
