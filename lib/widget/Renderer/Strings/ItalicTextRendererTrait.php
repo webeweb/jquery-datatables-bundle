7394a06aa80e76cc72f67e95029a7f1627480185
webeweb <support@webeweb.fr>
2024-04-17 15:57:32 +0200

    Copy Italic text renderer trait (from core-library)

---
+++ lib/widget/Renderer/Strings/ItalicTextRendererTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Renderer\Strings;
+
+use WBW\Library\Types\Helper\StringHelper;
+
+/**
+ * Italic text renderer trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Renderer\Strings
+ */
+trait ItalicTextRendererTrait {
+
+    /**
+     * Render a italic text.
+     *
+     * @param string|null $text The text.
+     * @return string|null Returns the italic text.
+     */
+    protected function renderItalicText(?string $text): ?string {
+
+        if (null === $text || "" === $text) {
+            return null;
+        }
+
+        return StringHelper::domNode("em", $text);
+    }
+}
---
+++ lib/widget/Tests/Renderer/Strings/ItalicTextRendererTraitTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Tests\Renderer\Strings;
+
+use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
+use WBW\Bundle\WidgetBundle\Tests\Fixtures\Renderer\Strings\TestItalicTextRendererTrait;
+
+/**
+ * Italic text renderer trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Tests\Renderer\Strings
+ */
+class ItalicTextRendererTraitTest extends AbstractTestCase {
+
+    /**
+     * Test renderItalicText()
+     *
+     * @return void
+     */
+    public function testRenderItalicText(): void {
+
+        $obj = new TestItalicTextRendererTrait();
+
+        $this->assertNull($obj->renderItalicText(null));
+        $this->assertNull($obj->renderItalicText(""));
+        $this->assertEquals('<em>content</em>', $obj->renderItalicText("content"));
+    }
+}
