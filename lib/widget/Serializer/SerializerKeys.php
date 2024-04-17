03899649c75639923519c85cd112d3a2d2aa0f73
webeweb <support@webeweb.fr>
2024-04-17 16:55:49 +0200

    Copy Serializer keys (from core-library)

---
+++ lib/widget/Serializer/SerializerKeys.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\WidgetBundle\Serializer;
+
+/**
+ * Serializer keys.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\WidgetBundle\Serializer
+ */
+class SerializerKeys {
+
+    /**
+     * Serializer key "matcher".
+     *
+     * @var string
+     */
+    public const MATCHER = "matcher";
+}
