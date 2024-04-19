f3ce8e7ea07ca62197d986ea207d38e373d5c178
webeweb <support@webeweb.fr>
2024-04-19 14:41:21 +0200

    Improve Breadcrumb item "new users group" test

--- lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemNewUsersGroupTest.php
+++ lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemNewUsersGroupTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon\BreadcrumbItemNewUsersGroup;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
 use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
@@ -33,6 +34,9 @@ class BreadcrumbItemNewUsersGroupTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new BreadcrumbItemNewUsersGroup("route");
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(NavigationNodeInterface::class, $obj);
 
         $this->assertEquals("navigation.item.new", $obj->getLabel());
         $this->assertEquals("g:plus", $obj->getIcon());
