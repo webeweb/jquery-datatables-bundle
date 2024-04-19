f6e6c3a3ba8136cda6d39edc48ba4fef6572fc5e
webeweb <support@webeweb.fr>
2024-04-19 12:17:19 +0200

    Add Tabler icon

---
+++ lib/bootstrap/Extend/TablerIcon.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Extend;
+
+use WBW\Bundle\WidgetBundle\Component\AbstractIcon;
+
+/**
+ * Tabler icon.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Extend
+ */
+class TablerIcon extends AbstractIcon implements TablerIconInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+}
---
+++ lib/bootstrap/Tests/Extend/TablerIconTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Extend;
+
+use JsonSerializable;
+use WBW\Bundle\BootstrapBundle\Extend\TablerIcon;
+use WBW\Bundle\BootstrapBundle\Extend\TablerIconInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\WidgetBundle\Component\IconInterface;
+
+/**
+ * Font Awesome icon test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Extend
+ */
+class TablerIconTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new TablerIcon();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(IconInterface::class, $obj);
+        $this->assertInstanceOf(TablerIconInterface::class, $obj);
+    }
+}
