598e683358c18d35e8b80e9a47daf8197fde9910
webeweb <support@webeweb.fr>
2024-04-19 12:12:04 +0200

    Improve Light button test

--- lib/bootstrap/Tests/Component/Button/LightButtonTest.php
+++ lib/bootstrap/Tests/Component/Button/LightButtonTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Button;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Button\LightButton;
 use WBW\Bundle\BootstrapBundle\Component\ButtonInterface;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
@@ -33,6 +34,9 @@ class LightButtonTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new LightButton();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(ButtonInterface::class, $obj);
 
         $this->assertEquals(ButtonInterface::BUTTON_TYPE_LIGHT, $obj->getType());
     }
