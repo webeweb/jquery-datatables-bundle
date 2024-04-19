01bd3297847c0c1fb1c50c0f0a5b0f62208a846b
webeweb <support@webeweb.fr>
2024-04-19 11:39:58 +0200

    Add Tabler icon interface

---
+++ lib/bootstrap/Extend/TablerIconInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Extend;
+
+use WBW\Bundle\WidgetBundle\Component\IconInterface;
+
+/**
+ * Tabler icon interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Extend
+ */
+interface TablerIconInterface extends IconInterface {
+
+}
