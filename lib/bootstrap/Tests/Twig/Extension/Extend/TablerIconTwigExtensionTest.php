6807fddeef268c1b776353b67e337477af25c88b
webeweb <support@webeweb.fr>
2024-04-19 15:02:09 +0200

    Add Tabler icon Twig extension

---
+++ lib/bootstrap/Tests/Twig/Extension/Extend/TablerIconTwigExtensionTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend;
+
+use Twig\Environment;
+use Twig\Extension\ExtensionInterface;
+use Twig\Node\Node;
+use Twig\TwigFunction;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\BootstrapBundle\Twig\Extension\Extend\TablerIconTwigExtension;
+use WBW\Bundle\WidgetBundle\Renderer\Component\IconRendererInterface;
+
+/**
+ * Tabler icon Twig extension test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Twig\Extension\Extend
+ */
+class TablerIconTwigExtensionTest extends AbstractTestCase {
+
+    /**
+     * Twig environment.
+     *
+     * @var Environment|null
+     */
+    private $twigEnvironment;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Twig environment mock.
+        $this->twigEnvironment = $this->getMockBuilder(Environment::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * Test tablerIconFunction()
+     *
+     * @return void
+     */
+    public function testTablerIconFunction(): void {
+
+        $obj = new TablerIconTwigExtension($this->twigEnvironment);
+
+        $arg = ["name" => "asterisk"];
+        $exp = '';
+
+        $this->assertEquals($exp, $obj->tablerIconFunction($arg));
+    }
+
+    /**
+     * Test getFilters()
+     *
+     * @return void
+     */
+    public function testGetFilters(): void {
+
+        $obj = new TablerIconTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFilters();
+        $this->assertCount(0, $res);
+    }
+
+    /**
+     * Test getFunctions()
+     *
+     * @return void
+     */
+    public function testGetFunctions(): void {
+
+        $obj = new TablerIconTwigExtension($this->twigEnvironment);
+
+        $res = $obj->getFunctions();
+        $this->assertCount(2, $res);
+
+        $i = -1;
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("tablerIcon", $res[$i]->getName());
+        $this->assertEquals([$obj, "tablerIconFunction"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+
+        $this->assertInstanceOf(TwigFunction::class, $res[++$i]);
+        $this->assertEquals("tiIcon", $res[$i]->getName());
+        $this->assertEquals([$obj, "tablerIconFunction"], $res[$i]->getCallable());
+        $this->assertEquals(["html"], $res[$i]->getSafe(new Node()));
+    }
+
+    /**
+     * Test renderIcon()
+     *
+     * @return void
+     */
+    public function testRenderIcon(): void {
+
+        $obj = new TablerIconTwigExtension($this->twigEnvironment);
+
+        $exp = '';
+
+        $this->assertEquals($exp, $obj->renderIcon("asterisk", "display: none;"));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.bootstrap.twig.extension.extend.tabler", TablerIconTwigExtension::SERVICE_NAME);
+
+        $obj = new TablerIconTwigExtension($this->twigEnvironment);
+
+        $this->assertInstanceOf(ExtensionInterface::class, $obj);
+        $this->assertInstanceOf(IconRendererInterface::class, $obj);
+
+        $this->assertSame($this->twigEnvironment, $obj->getTwigEnvironment());
+    }
+}
---
+++ lib/bootstrap/Twig/Extension/Extend/TablerIconTwigExtension.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Twig\Extension\Extend;
+
+use Twig\TwigFunction;
+use WBW\Bundle\WidgetBundle\Renderer\Component\IconRendererInterface;
+use WBW\Library\Types\Helper\ArrayHelper;
+
+/**
+ * Tabler icon Twig extension.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Twig\Extension\Extend
+ */
+class TablerIconTwigExtension extends AbstractTablerIconTwigExtension implements IconRendererInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.bootstrap.twig.extension.extend.tabler";
+
+    /**
+     * Render a Tabler icon.
+     *
+     * @param array<string,mixed> $args The arguments.
+     * @return string Returns the Tabler icon.
+     */
+    public function tablerIconFunction(array $args = []): string {
+        return $this->tablerIcon(ArrayHelper::get($args, "name", "house"), ArrayHelper::get($args, "style"));
+    }
+
+    /**
+     * Get the Twig functions.
+     *
+     * @return TwigFunction[] Returns the Twig functions.
+     */
+    public function getFunctions(): array {
+
+        return [
+            new TwigFunction("tablerIcon", [$this, "tablerIconFunction"], ["is_safe" => ["html"]]),
+            new TwigFunction("tiIcon", [$this, "tablerIconFunction"], ["is_safe" => ["html"]]),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function renderIcon(?string $name, ?string $style): string {
+        return $this->tablerIconFunction(["name" => $name, "style" => $style]);
+    }
+}
