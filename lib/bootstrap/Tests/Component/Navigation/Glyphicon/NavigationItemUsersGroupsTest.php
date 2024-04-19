fd22beb46f8a414a6ae5872b6edf36b89a1be2ba
webeweb <support@webeweb.fr>
2024-04-19 14:42:57 +0200

    Improve Navigation item "users groups" test

--- lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemUsersGroupsTest.php
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemUsersGroupsTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemUsersGroups;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
 use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
@@ -33,6 +34,9 @@ class NavigationItemUsersGroupsTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new NavigationItemUsersGroups("route");
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(NavigationNodeInterface::class, $obj);
 
         $this->assertEquals("navigation.item.users_groups", $obj->getLabel());
         $this->assertEquals("g:user", $obj->getIcon());
