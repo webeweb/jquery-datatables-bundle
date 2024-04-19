08f3edeb562adbb550899c3404a9b7bbe0abd5a5
webeweb <support@webeweb.fr>
2024-04-19 14:41:12 +0200

    Improve Breadcrumb item "new" test

--- lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemNewTest.php
+++ lib/bootstrap/Tests/Component/Breadcrumb/Glyphicon/BreadcrumbItemNewTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Breadcrumb\Glyphicon;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Breadcrumb\Glyphicon\BreadcrumbItemNew;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
 use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
@@ -33,6 +34,9 @@ class BreadcrumbItemNewTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new BreadcrumbItemNew("route");
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(NavigationNodeInterface::class, $obj);
 
         $this->assertEquals("navigation.item.new", $obj->getLabel());
         $this->assertEquals("g:plus", $obj->getIcon());
