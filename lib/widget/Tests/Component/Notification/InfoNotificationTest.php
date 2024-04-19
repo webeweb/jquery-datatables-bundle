c78d5bc5c4de3d4e60106e32c7b118d98e65c75f
webeweb <support@webeweb.fr>
2024-04-19 12:15:45 +0200

    Improve Info notification test

--- lib/widget/Tests/Component/Notification/InfoNotificationTest.php
+++ lib/widget/Tests/Component/Notification/InfoNotificationTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\WidgetBundle\Tests\Component\Notification;
 
+use JsonSerializable;
 use WBW\Bundle\WidgetBundle\Component\Notification\InfoNotification;
 use WBW\Bundle\WidgetBundle\Component\NotificationInterface;
 use WBW\Bundle\WidgetBundle\Tests\AbstractTestCase;
@@ -33,6 +34,9 @@ class InfoNotificationTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new InfoNotification("info");
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(NotificationInterface::class, $obj);
 
         $this->assertEquals(NotificationInterface::NOTIFICATION_TYPE_INFO, $obj->getType());
         $this->assertEquals("info", $obj->getContent());
