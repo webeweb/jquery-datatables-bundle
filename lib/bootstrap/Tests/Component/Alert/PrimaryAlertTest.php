4cdcd81f0cae259de7745810567014603b3b8c6b
webeweb <support@webeweb.fr>
2024-04-19 12:10:08 +0200

    Improve Primary alert test

--- lib/bootstrap/Tests/Component/Alert/PrimaryAlertTest.php
+++ lib/bootstrap/Tests/Component/Alert/PrimaryAlertTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Alert;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Alert\PrimaryAlert;
 use WBW\Bundle\BootstrapBundle\Component\AlertInterface;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
@@ -33,6 +34,9 @@ class PrimaryAlertTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new PrimaryAlert();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(AlertInterface::class, $obj);
 
         $this->assertEquals(AlertInterface::ALERT_TYPE_PRIMARY, $obj->getType());
     }
