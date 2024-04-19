2f98e7482af51c643ac05a06ca270ec3f402d495
webeweb <support@webeweb.fr>
2024-04-19 14:41:43 +0200

    Improve Navigation item "deshboard" test

--- lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemDashboardTest.php
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemDashboardTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemDashboard;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
 use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
@@ -33,6 +34,9 @@ class NavigationItemDashboardTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new NavigationItemDashboard("route");
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(NavigationNodeInterface::class, $obj);
 
         $this->assertEquals("navigation.item.dashboard", $obj->getLabel());
         $this->assertEquals("g:dashboard", $obj->getIcon());
