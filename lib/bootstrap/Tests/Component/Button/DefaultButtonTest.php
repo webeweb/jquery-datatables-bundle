a79894d0919ba9d041297e0955eac6b77221b334
webeweb <support@webeweb.fr>
2024-04-19 12:11:51 +0200

    Improve Default button test

--- lib/bootstrap/Tests/Component/Button/DefaultButtonTest.php
+++ lib/bootstrap/Tests/Component/Button/DefaultButtonTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Button;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Button\DefaultButton;
 use WBW\Bundle\BootstrapBundle\Component\ButtonInterface;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
@@ -33,6 +34,9 @@ class DefaultButtonTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new DefaultButton();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(ButtonInterface::class, $obj);
 
         $this->assertEquals(ButtonInterface::BUTTON_TYPE_DEFAULT, $obj->getType());
     }
