9af7bef958e1b5d7103ea57aea16d69834d0ce5a
webeweb <support@webeweb.fr>
2024-04-19 12:12:11 +0200

    Improve Link button test

--- lib/bootstrap/Tests/Component/Button/LinkButtonTest.php
+++ lib/bootstrap/Tests/Component/Button/LinkButtonTest.php
@@ -13,6 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Tests\Component\Button;
 
+use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Button\LinkButton;
 use WBW\Bundle\BootstrapBundle\Component\ButtonInterface;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
@@ -33,6 +34,9 @@ class LinkButtonTest extends AbstractTestCase {
     public function test__construct(): void {
 
         $obj = new LinkButton();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(ButtonInterface::class, $obj);
 
         $this->assertEquals(ButtonInterface::BUTTON_TYPE_LINK, $obj->getType());
     }
