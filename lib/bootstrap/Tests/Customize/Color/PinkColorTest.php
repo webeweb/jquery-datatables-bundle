cb4f52f3c3c76eef975e627e31b7166666968ef5
webeweb <support@webeweb.fr>
2024-04-22 14:44:07 +0200

    Add Pink color

---
+++ lib/bootstrap/Tests/Customize/Color/PinkColorTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use JsonSerializable;
+use WBW\Bundle\BootstrapBundle\Customize\Color\PinkColor;
+use WBW\Bundle\BootstrapBundle\Customize\Color\PinkColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\WidgetBundle\Component\Color\PinkColorInterface as BasePinkColorInterface;
+use WBW\Bundle\WidgetBundle\Component\ColorInterface;
+
+/**
+ * Pink color test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package  WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class PinkColorTest extends AbstractTestCase {
+
+    /**
+     * Test getValues()
+     *
+     * @return void
+     */
+    public function testGetValues(): void {
+
+        $obj = new PinkColor();
+
+        $res = $obj->getValues();
+        $this->assertCount(9, $res);
+
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_100, $res[ColorInterface::COLOR_VALUE_100]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_200, $res[ColorInterface::COLOR_VALUE_200]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_300, $res[ColorInterface::COLOR_VALUE_300]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_400, $res[ColorInterface::COLOR_VALUE_400]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_500, $res[ColorInterface::COLOR_VALUE_500]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_500, $res[ColorInterface::COLOR_VALUE_500]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_600, $res[ColorInterface::COLOR_VALUE_600]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_700, $res[ColorInterface::COLOR_VALUE_700]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_800, $res[ColorInterface::COLOR_VALUE_800]);
+        $this->assertEquals(PinkColorInterface::PINK_COLOR_VALUE_900, $res[ColorInterface::COLOR_VALUE_900]);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new PinkColor();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(ColorInterface::class, $obj);
+        $this->assertInstanceOf(PinkColorInterface::class, $obj);
+
+        $this->assertEquals(BasePinkColorInterface::PINK_COLOR_NAME, $obj->getName());
+    }
+}
