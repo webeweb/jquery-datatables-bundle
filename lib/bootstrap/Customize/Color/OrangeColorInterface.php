6d31812d9ff1395afbb08d2c7768c315fabdbb21
webeweb <support@webeweb.fr>
2024-04-22 14:20:13 +0200

    Add Orange color interface

---
+++ lib/bootstrap/Customize/Color/OrangeColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\OrangeColorInterface as BaseOrangeColorInterface;
+
+/**
+ * Orange color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface OrangeColorInterface extends BaseOrangeColorInterface {
+
+    /**
+     * Orange color "100".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_100 = "#ffe5d0";
+
+    /**
+     * Orange color "200".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_200 = "#fecba1";
+
+    /**
+     * Orange color "300".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_300 = "#feb272";
+
+    /**
+     * Orange color "400".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_400 = "#fd9843";
+
+    /**
+     * Orange color "500".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_500 = "#fd7e14";
+
+    /**
+     * Orange color "600".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_600 = "#ca6510";
+
+    /**
+     * Orange color "700".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_700 = "#984c0c";
+
+    /**
+     * Orange color "800".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_800 = "#653208";
+
+    /**
+     * Orange color "900".
+     *
+     * @var string
+     */
+    public const ORANGE_COLOR_VALUE_900 = "#331904";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/OrangeColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\OrangeColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Orange color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class OrangeColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#ffe5d0", OrangeColorInterface::ORANGE_COLOR_VALUE_100);
+        $this->assertEquals("#fecba1", OrangeColorInterface::ORANGE_COLOR_VALUE_200);
+        $this->assertEquals("#feb272", OrangeColorInterface::ORANGE_COLOR_VALUE_300);
+        $this->assertEquals("#fd9843", OrangeColorInterface::ORANGE_COLOR_VALUE_400);
+        $this->assertEquals("#fd7e14", OrangeColorInterface::ORANGE_COLOR_VALUE_500);
+        $this->assertEquals("#ca6510", OrangeColorInterface::ORANGE_COLOR_VALUE_600);
+        $this->assertEquals("#984c0c", OrangeColorInterface::ORANGE_COLOR_VALUE_700);
+        $this->assertEquals("#653208", OrangeColorInterface::ORANGE_COLOR_VALUE_800);
+        $this->assertEquals("#331904", OrangeColorInterface::ORANGE_COLOR_VALUE_900);
+    }
+}
