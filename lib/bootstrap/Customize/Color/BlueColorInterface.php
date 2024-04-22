f979a068843493b9d85a5031d4aed5aa7ecad4aa
webeweb <support@webeweb.fr>
2024-04-22 14:19:33 +0200

    Add Blue color interface

---
+++ lib/bootstrap/Customize/Color/BlueColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\BlueColorInterface as BaseBlueColorInterface;
+
+/**
+ * Blue color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface BlueColorInterface extends BaseBlueColorInterface {
+
+    /**
+     * Blue color "100".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_100 = "#cfe2ff";
+
+    /**
+     * Blue color "200".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_200 = "#9ec5fe";
+
+    /**
+     * Blue color "300".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_300 = "#6ea8fe";
+
+    /**
+     * Blue color "400".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_400 = "#3d8bfd";
+
+    /**
+     * Blue color "500".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_500 = "#0d6efd";
+
+    /**
+     * Blue color "600".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_600 = "#0a58ca";
+
+    /**
+     * Blue color "700".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_700 = "#084298";
+
+    /**
+     * Blue color "800".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_800 = "#052c65";
+
+    /**
+     * Blue color "900".
+     *
+     * @var string
+     */
+    public const BLUE_COLOR_VALUE_900 = "#031633";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/BlueColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\BlueColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Blue color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class BlueColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#cfe2ff", BlueColorInterface::BLUE_COLOR_VALUE_100);
+        $this->assertEquals("#9ec5fe", BlueColorInterface::BLUE_COLOR_VALUE_200);
+        $this->assertEquals("#6ea8fe", BlueColorInterface::BLUE_COLOR_VALUE_300);
+        $this->assertEquals("#3d8bfd", BlueColorInterface::BLUE_COLOR_VALUE_400);
+        $this->assertEquals("#0d6efd", BlueColorInterface::BLUE_COLOR_VALUE_500);
+        $this->assertEquals("#0a58ca", BlueColorInterface::BLUE_COLOR_VALUE_600);
+        $this->assertEquals("#084298", BlueColorInterface::BLUE_COLOR_VALUE_700);
+        $this->assertEquals("#052c65", BlueColorInterface::BLUE_COLOR_VALUE_800);
+        $this->assertEquals("#031633", BlueColorInterface::BLUE_COLOR_VALUE_900);
+    }
+}
