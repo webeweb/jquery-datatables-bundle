0f5a0432e7d8f0fd405d856350a795b6a11a3cb9
webeweb <support@webeweb.fr>
2024-04-22 08:41:58 +0200

    Copy Assets provider interface (from core-bundle)

---
+++ lib/common/Tests/Provider/AssetsProviderInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider;
+
+use WBW\Bundle\CommonBundle\Provider\AssetsProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Assets provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider
+ */
+class AssetsProviderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("/Resources/assets", AssetsProviderInterface::ASSETS_RELATIVE_DIRECTORY);
+    }
+}
