1f5810c82ac074700c1ee1ef1bd1cfab770fdac6
webeweb <support@webeweb.fr>
2024-04-22 16:17:08 +0200

    Copy abstract manager (from core-library)

---
+++ lib/common/Manager/AbstractManager.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Manager;
+
+use Psr\Log\LoggerInterface;
+use WBW\Bundle\CommonBundle\Provider\ProviderInterface;
+use WBW\Library\Logger\LoggerTrait;
+
+/**
+ * Abstract manager.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Manager
+ */
+class AbstractManager implements ManagerInterface {
+
+    use LoggerTrait;
+
+    /**
+     * Providers.
+     *
+     * @var ProviderInterface[]|null
+     */
+    private $providers;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface|null $logger The logger.
+     */
+    public function __construct(LoggerInterface $logger = null) {
+        $this->setLogger($logger);
+        $this->setProviders([]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addProvider(ProviderInterface $provider): ManagerInterface {
+        $this->providers[] = $provider;
+        return $this->logInfo("A manager add a provider", ["_manager" => get_class($this), "_provider" => get_class($provider)]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function containsProvider(ProviderInterface $provider): bool {
+        return -1 !== $this->indexOfProvider($provider);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function &getProviders(): array {
+        return $this->providers;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasProviders(): bool {
+        return 0 < $this->size();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function indexOfProvider(ProviderInterface $provider): int {
+
+        for ($i = count($this->providers) - 1; 0 <= $i; --$i) {
+
+            if ($provider === $this->providers[$i]) {
+                return $i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Log an info.
+     *
+     * @param string $message The message.
+     * @param mixed[] $context The context.
+     * @return ManagerInterface Returns this manager.
+     */
+    protected function logInfo(string $message, array $context): ManagerInterface {
+
+        if (null !== $this->getLogger()) {
+            $this->getLogger()->info($message, $context);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set the providers.
+     *
+     * @param ProviderInterface[] $providers The providers.
+     * @return ManagerInterface Returns this manager.
+     */
+    protected function setProviders(array $providers): ManagerInterface {
+        $this->providers = $providers;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function size(): int {
+        return count($this->providers);
+    }
+}
---
+++ lib/common/Tests/Manager/AbstractManagerTest.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Manager;
+
+use Psr\Log\LoggerInterface;
+use Throwable;
+use WBW\Bundle\CommonBundle\Manager\ManagerInterface;
+use WBW\Bundle\CommonBundle\Provider\ProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Manager\TestManager;
+
+/**
+ * Abstract manager test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Manager
+ */
+class AbstractManagerTest extends AbstractTestCase {
+
+    /**
+     * Logger.
+     *
+     * @var LoggerInterface|null
+     */
+    private $logger;
+
+    /**
+     * Provider.
+     *
+     * @var ProviderInterface|null
+     */
+    private $provider;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Logger mock.
+        $this->logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+
+        // Set a Provider mock.
+        $this->provider = $this->getMockBuilder(ProviderInterface::class)->getMock();
+    }
+
+    /**
+     * Test addProvider()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testAddProvider(): void {
+
+        $obj = new TestManager($this->logger);
+
+        $obj->addProvider($this->provider);
+        $this->assertSame($this->provider, $obj->getProviders()[0]);
+        $this->assertEquals(1, $obj->size());
+    }
+
+    /**
+     * Test containsProvider()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testContainsProvider(): void {
+
+        $obj = new TestManager($this->logger);
+
+        $this->assertFalse($obj->containsProvider($this->provider));
+
+        $obj->addProvider($this->provider);
+        $this->assertTrue($obj->containsProvider($this->provider));
+    }
+
+    /**
+     * Test hasProviders()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testHasProviders(): void {
+
+        $obj = new TestManager($this->logger);
+
+        $this->assertFalse($obj->hasProviders());
+
+        $obj->addProvider($this->provider);
+        $this->assertTrue($obj->hasProviders());
+    }
+
+    /**
+     * Test indexOfProvider()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testIndexOfProvider(): void {
+
+        // Set a Provider mock.
+        $provider = $this->getMockBuilder(ProviderInterface::class)->getMock();
+
+        $obj = new TestManager($this->logger);
+
+        $obj->addProvider($provider);
+        $this->assertEquals(-1, $obj->indexOfProvider($this->provider));
+
+        $obj->addProvider($this->provider);
+        $this->assertEquals(1, $obj->indexOfProvider($this->provider));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new TestManager($this->logger);
+
+        $this->assertInstanceOf(ManagerInterface::class, $obj);
+
+        $this->assertEquals([], $obj->getProviders());
+        $this->assertEquals(0, $obj->size());
+    }
+}
