7da700d4eb87d4d481324510e32eb09dd9a83ee2
webeweb <support@webeweb.fr>
2024-04-22 14:44:23 +0200

    Add Red color

---
+++ lib/bootstrap/Tests/Customize/Color/RedColorTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use JsonSerializable;
+use WBW\Bundle\BootstrapBundle\Customize\Color\RedColor;
+use WBW\Bundle\BootstrapBundle\Customize\Color\RedColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+use WBW\Bundle\WidgetBundle\Component\Color\RedColorInterface as BaseRedColorInterface;
+use WBW\Bundle\WidgetBundle\Component\ColorInterface;
+
+/**
+ * Red color test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package  WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class RedColorTest extends AbstractTestCase {
+
+    /**
+     * Test getValues()
+     *
+     * @return void
+     */
+    public function testGetValues(): void {
+
+        $obj = new RedColor();
+
+        $res = $obj->getValues();
+        $this->assertCount(9, $res);
+
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_100, $res[ColorInterface::COLOR_VALUE_100]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_200, $res[ColorInterface::COLOR_VALUE_200]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_300, $res[ColorInterface::COLOR_VALUE_300]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_400, $res[ColorInterface::COLOR_VALUE_400]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_500, $res[ColorInterface::COLOR_VALUE_500]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_500, $res[ColorInterface::COLOR_VALUE_500]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_600, $res[ColorInterface::COLOR_VALUE_600]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_700, $res[ColorInterface::COLOR_VALUE_700]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_800, $res[ColorInterface::COLOR_VALUE_800]);
+        $this->assertEquals(RedColorInterface::RED_COLOR_VALUE_900, $res[ColorInterface::COLOR_VALUE_900]);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new RedColor();
+
+        $this->assertInstanceOf(JsonSerializable::class, $obj);
+        $this->assertInstanceOf(ColorInterface::class, $obj);
+        $this->assertInstanceOf(RedColorInterface::class, $obj);
+
+        $this->assertEquals(BaseRedColorInterface::RED_COLOR_NAME, $obj->getName());
+    }
+}
