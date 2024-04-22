9d87187a17245884f37cb789c9191a624ab4f3fb
webeweb <support@webeweb.fr>
2024-04-22 14:19:57 +0200

    Add Green color interface

---
+++ lib/bootstrap/Customize/Color/GreenColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\GreenColorInterface as BaseGreenColorInterface;
+
+/**
+ * Green color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface GreenColorInterface extends BaseGreenColorInterface {
+
+    /**
+     * Green color "100".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_100 = "#d1e7dd";
+
+    /**
+     * Green color "200".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_200 = "#a3cfbb";
+
+    /**
+     * Green color "300".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_300 = "#75b798";
+
+    /**
+     * Green color "400".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_400 = "#479f76";
+
+    /**
+     * Green color "500".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_500 = "#198754";
+
+    /**
+     * Green color "600".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_600 = "#146c43";
+
+    /**
+     * Green color "700".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_700 = "#0f5132";
+
+    /**
+     * Green color "800".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_800 = "#0a3622";
+
+    /**
+     * Green color "900".
+     *
+     * @var string
+     */
+    public const GREEN_COLOR_VALUE_900 = "#051b11";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/GreenColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\GreenColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Green color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class GreenColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#d1e7dd", GreenColorInterface::GREEN_COLOR_VALUE_100);
+        $this->assertEquals("#a3cfbb", GreenColorInterface::GREEN_COLOR_VALUE_200);
+        $this->assertEquals("#75b798", GreenColorInterface::GREEN_COLOR_VALUE_300);
+        $this->assertEquals("#479f76", GreenColorInterface::GREEN_COLOR_VALUE_400);
+        $this->assertEquals("#198754", GreenColorInterface::GREEN_COLOR_VALUE_500);
+        $this->assertEquals("#146c43", GreenColorInterface::GREEN_COLOR_VALUE_600);
+        $this->assertEquals("#0f5132", GreenColorInterface::GREEN_COLOR_VALUE_700);
+        $this->assertEquals("#0a3622", GreenColorInterface::GREEN_COLOR_VALUE_800);
+        $this->assertEquals("#051b11", GreenColorInterface::GREEN_COLOR_VALUE_900);
+    }
+}
