f371fd130e94469e5cc5a54dcdd9973d0652bf23
webeweb <support@webeweb.fr>
2024-04-22 09:40:14 +0200

    Copy Javascript manager interface

---
+++ lib/common/Manager/JavascriptManagerInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Manager;
+
+/**
+ * Javascript manager interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Manager
+ */
+interface JavascriptManagerInterface extends ManagerInterface {
+
+    /**
+     * Get the javascripts.
+     *
+     * @return mixed[] Returns the javascripts.
+     */
+    public function getJavascripts(): array;
+}
