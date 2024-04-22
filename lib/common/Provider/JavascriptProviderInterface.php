3ac4fd7e7ff0e9b36dd3dab24a2c39c9b275f777
webeweb <support@webeweb.fr>
2024-04-22 08:48:17 +0200

    Copy Javascript provider interface (from core-library)

---
+++ lib/common/Provider/JavascriptProviderInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider;
+
+/**
+ * Javascript provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider
+ */
+interface JavascriptProviderInterface extends ProviderInterface {
+
+    /**
+     * Content-type.
+     *
+     * @var string
+     */
+    public const JAVASCRIPT_PROVIDER_CONTENT_TYPE = "application/javascript";
+
+    /**
+     * Extension.
+     *
+     * @var string
+     */
+    public const JAVASCRIPT_PROVIDER_EXTENSION = "js";
+
+    /**
+     * Tag name.
+     *
+     * @var string
+     */
+    public const JAVASCRIPT_PROVIDER_TAG_NAME = "wbw.common.provider.javascript";
+
+    /**
+     * Get the javascripts.
+     *
+     * @return mixed[] Returns the javascripts.
+     */
+    public function getJavascripts(): array;
+}
---
+++ lib/common/Tests/Provider/JavascriptProviderInterfaceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider;
+
+use WBW\Bundle\CommonBundle\Provider\JavascriptProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Javascript provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider
+ */
+class JavascriptProviderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("application/javascript", JavascriptProviderInterface::JAVASCRIPT_PROVIDER_CONTENT_TYPE);
+        $this->assertEquals("js", JavascriptProviderInterface::JAVASCRIPT_PROVIDER_EXTENSION);
+        $this->assertEquals("wbw.common.provider.javascript", JavascriptProviderInterface::JAVASCRIPT_PROVIDER_TAG_NAME);
+    }
+}
