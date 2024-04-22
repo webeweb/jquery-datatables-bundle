647e87737087d92255738a5dfdd5493e464adeb6
webeweb <support@webeweb.fr>
2024-04-22 14:20:42 +0200

    Add Teal color interface

---
+++ lib/bootstrap/Customize/Color/TealColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\TealColorInterface as BaseTealColorInterface;
+
+/**
+ * Teal color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface TealColorInterface extends BaseTealColorInterface {
+
+    /**
+     * Teal color "100".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_100 = "#d2f4ea";
+
+    /**
+     * Teal color "200".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_200 = "#a6e9d5";
+
+    /**
+     * Teal color "300".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_300 = "#79dfc1";
+
+    /**
+     * Teal color "400".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_400 = "#4dd4ac";
+
+    /**
+     * Teal color "500".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_500 = "#20c997";
+
+    /**
+     * Teal color "600".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_600 = "#1aa179";
+
+    /**
+     * Teal color "700".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_700 = "#13795b";
+
+    /**
+     * Teal color "800".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_800 = "#0d503c";
+
+    /**
+     * Teal color "900".
+     *
+     * @var string
+     */
+    public const TEAL_COLOR_VALUE_900 = "#06281e";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/TealColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\TealColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Teal color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class TealColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#d2f4ea", TealColorInterface::TEAL_COLOR_VALUE_100);
+        $this->assertEquals("#a6e9d5", TealColorInterface::TEAL_COLOR_VALUE_200);
+        $this->assertEquals("#79dfc1", TealColorInterface::TEAL_COLOR_VALUE_300);
+        $this->assertEquals("#4dd4ac", TealColorInterface::TEAL_COLOR_VALUE_400);
+        $this->assertEquals("#20c997", TealColorInterface::TEAL_COLOR_VALUE_500);
+        $this->assertEquals("#1aa179", TealColorInterface::TEAL_COLOR_VALUE_600);
+        $this->assertEquals("#13795b", TealColorInterface::TEAL_COLOR_VALUE_700);
+        $this->assertEquals("#0d503c", TealColorInterface::TEAL_COLOR_VALUE_800);
+        $this->assertEquals("#06281e", TealColorInterface::TEAL_COLOR_VALUE_900);
+    }
+}
