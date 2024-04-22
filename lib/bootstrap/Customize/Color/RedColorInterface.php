7c963056165bb92d8e3e01a00ca73537c19df7a6
webeweb <support@webeweb.fr>
2024-04-22 14:20:36 +0200

    Add Red color interface

---
+++ lib/bootstrap/Customize/Color/RedColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\RedColorInterface as BaseRedColorInterface;
+
+/**
+ * Red color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface RedColorInterface extends BaseRedColorInterface {
+
+    /**
+     * Red color "100".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_100 = "#f8d7da";
+
+    /**
+     * Red color "200".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_200 = "#f1aeb5";
+
+    /**
+     * Red color "300".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_300 = "#ea868f";
+
+    /**
+     * Red color "400".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_400 = "#e35d6a";
+
+    /**
+     * Red color "500".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_500 = "#dc3545";
+
+    /**
+     * Red color "600".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_600 = "#b02a37";
+
+    /**
+     * Red color "700".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_700 = "#842029";
+
+    /**
+     * Red color "800".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_800 = "#58151c";
+
+    /**
+     * Red color "900".
+     *
+     * @var string
+     */
+    public const RED_COLOR_VALUE_900 = "#2c0b0e";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/RedColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\RedColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Red color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class RedColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#f8d7da", RedColorInterface::RED_COLOR_VALUE_100);
+        $this->assertEquals("#f1aeb5", RedColorInterface::RED_COLOR_VALUE_200);
+        $this->assertEquals("#ea868f", RedColorInterface::RED_COLOR_VALUE_300);
+        $this->assertEquals("#e35d6a", RedColorInterface::RED_COLOR_VALUE_400);
+        $this->assertEquals("#dc3545", RedColorInterface::RED_COLOR_VALUE_500);
+        $this->assertEquals("#b02a37", RedColorInterface::RED_COLOR_VALUE_600);
+        $this->assertEquals("#842029", RedColorInterface::RED_COLOR_VALUE_700);
+        $this->assertEquals("#58151c", RedColorInterface::RED_COLOR_VALUE_800);
+        $this->assertEquals("#2c0b0e", RedColorInterface::RED_COLOR_VALUE_900);
+    }
+}
