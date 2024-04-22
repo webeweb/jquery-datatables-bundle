5dc1a59e85ff4d38384af7a666e08ee225a8253a
webeweb <support@webeweb.fr>
2024-04-22 10:12:22 +0200

    Copy Views controller test (from core-bundle)

---
+++ lib/common/Tests/Controller/ViewsControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the core-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Controller;
+
+use WBW\Bundle\CommonBundle\Tests\DefaultWebTestCase;
+
+/**
+ * Views controller test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Controller
+ */
+class ViewsControllerTest extends DefaultWebTestCase {
+
+    /**
+     * Test lib/common/Resources/views/assets/_javascripts.html.twig
+     *
+     * @return void
+     */
+    public function testAssetsJavascriptsAction(): void {
+
+        $client = $this->client;
+
+        $client->request("GET", "/common/assets/javascripts");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("text/html; charset=UTF-8", $client->getResponse()->headers->get("Content-Type"));
+    }
+
+    /**
+     * Test lib/common/Resources/views/assets/_stylesheets.html.twig
+     *
+     * @return void
+     */
+    public function testAssetsStylesheetsAction(): void {
+
+        $client = $this->client;
+
+        $client->request("GET", "/common/assets/stylesheets");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("text/html; charset=UTF-8", $client->getResponse()->headers->get("Content-Type"));
+    }
+}
