9f2b0c7d6171d4b99e606b3c9ff394a52467f5b3
webeweb <support@webeweb.fr>
2024-04-22 14:20:49 +0200

    Add Yellow color interface

---
+++ lib/bootstrap/Customize/Color/YellowColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\YellowColorInterface as BaseYellowColorInterface;
+
+/**
+ * Yellow color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface YellowColorInterface extends BaseYellowColorInterface {
+
+    /**
+     * Yellow color "100".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_100 = "#fff3cd";
+
+    /**
+     * Yellow color "200".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_200 = "#ffe69c";
+
+    /**
+     * Yellow color "300".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_300 = "#ffda6a";
+
+    /**
+     * Yellow color "400".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_400 = "#ffcd39";
+
+    /**
+     * Yellow color "500".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_500 = "#ffc107";
+
+    /**
+     * Yellow color "600".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_600 = "#cc9a06";
+
+    /**
+     * Yellow color "700".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_700 = "#997404";
+
+    /**
+     * Yellow color "800".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_800 = "#664d03";
+
+    /**
+     * Yellow color "900".
+     *
+     * @var string
+     */
+    public const YELLOW_COLOR_VALUE_900 = "#332701";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/YellowColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\YellowColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Yellow color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class YellowColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#fff3cd", YellowColorInterface::YELLOW_COLOR_VALUE_100);
+        $this->assertEquals("#ffe69c", YellowColorInterface::YELLOW_COLOR_VALUE_200);
+        $this->assertEquals("#ffda6a", YellowColorInterface::YELLOW_COLOR_VALUE_300);
+        $this->assertEquals("#ffcd39", YellowColorInterface::YELLOW_COLOR_VALUE_400);
+        $this->assertEquals("#ffc107", YellowColorInterface::YELLOW_COLOR_VALUE_500);
+        $this->assertEquals("#cc9a06", YellowColorInterface::YELLOW_COLOR_VALUE_600);
+        $this->assertEquals("#997404", YellowColorInterface::YELLOW_COLOR_VALUE_700);
+        $this->assertEquals("#664d03", YellowColorInterface::YELLOW_COLOR_VALUE_800);
+        $this->assertEquals("#332701", YellowColorInterface::YELLOW_COLOR_VALUE_900);
+    }
+}
