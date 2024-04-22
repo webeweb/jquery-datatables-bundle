1214803ddad7d315f18fc07acf132b93307ab806
webeweb <support@webeweb.fr>
2024-04-22 14:20:30 +0200

    Add Purple color interface

---
+++ lib/bootstrap/Customize/Color/PurpleColorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Customize\Color;
+
+use WBW\Bundle\WidgetBundle\Component\Color\PurpleColorInterface as BasePurpleColorInterface;
+
+/**
+ * Purple color interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Customize\Color
+ */
+interface PurpleColorInterface extends BasePurpleColorInterface {
+
+    /**
+     * Purple color "100".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_100 = "#e2d9f3";
+
+    /**
+     * Purple color "200".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_200 = "#c5b3e6";
+
+    /**
+     * Purple color "300".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_300 = "#a98eda";
+
+    /**
+     * Purple color "400".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_400 = "#8c68cd";
+
+    /**
+     * Purple color "500".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_500 = "#6f42c1";
+
+    /**
+     * Purple color "600".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_600 = "#59359a";
+
+    /**
+     * Purple color "700".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_700 = "#432874";
+
+    /**
+     * Purple color "800".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_800 = "#2c1a4d";
+
+    /**
+     * Purple color "900".
+     *
+     * @var string
+     */
+    public const PURPLE_COLOR_VALUE_900 = "#160d27";
+}
---
+++ lib/bootstrap/Tests/Customize/Color/PurpleColorInterfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\BootstrapBundle\Tests\Customize\Color;
+
+use WBW\Bundle\BootstrapBundle\Customize\Color\PurpleColorInterface;
+use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
+
+/**
+ * Purple color interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\BootstrapBundle\Tests\Customize\Color
+ */
+class PurpleColorInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("#e2d9f3", PurpleColorInterface::PURPLE_COLOR_VALUE_100);
+        $this->assertEquals("#c5b3e6", PurpleColorInterface::PURPLE_COLOR_VALUE_200);
+        $this->assertEquals("#a98eda", PurpleColorInterface::PURPLE_COLOR_VALUE_300);
+        $this->assertEquals("#8c68cd", PurpleColorInterface::PURPLE_COLOR_VALUE_400);
+        $this->assertEquals("#6f42c1", PurpleColorInterface::PURPLE_COLOR_VALUE_500);
+        $this->assertEquals("#59359a", PurpleColorInterface::PURPLE_COLOR_VALUE_600);
+        $this->assertEquals("#432874", PurpleColorInterface::PURPLE_COLOR_VALUE_700);
+        $this->assertEquals("#2c1a4d", PurpleColorInterface::PURPLE_COLOR_VALUE_800);
+        $this->assertEquals("#160d27", PurpleColorInterface::PURPLE_COLOR_VALUE_900);
+    }
+}
