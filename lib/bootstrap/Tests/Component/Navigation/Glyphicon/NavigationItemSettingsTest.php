f144bc18f1de944512e40c15ffc49faa2517b1f4
webeweb <support@webeweb.fr>
2024-04-25 17:01:09 +0200

    Update Navigation item "settings" test: - change "use" statements

--- lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemSettingsTest.php
+++ lib/bootstrap/Tests/Component/Navigation/Glyphicon/NavigationItemSettingsTest.php
@@ -16,7 +16,7 @@ namespace WBW\Bundle\BootstrapBundle\Tests\Component\Navigation\Glyphicon;
 use JsonSerializable;
 use WBW\Bundle\BootstrapBundle\Component\Navigation\Glyphicon\NavigationItemSettings;
 use WBW\Bundle\BootstrapBundle\Tests\AbstractTestCase;
-use WBW\Bundle\WidgetBundle\Component\NavigationNodeInterface;
+use WBW\Library\Widget\Component\NavigationNodeInterface;
 
 /**
  * Navigation item "settings" test.
