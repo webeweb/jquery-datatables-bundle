0eb73f710c5215ac0bdd97e51b59bc66db857299
webeweb <support@webeweb.fr>
2024-04-25 17:25:33 +0200

    Update abstract controller test: - change "use" statements

--- lib/common/Tests/Controller/AbstractControllerTest.php
+++ lib/common/Tests/Controller/AbstractControllerTest.php
@@ -29,8 +29,8 @@ use WBW\Bundle\CommonBundle\Event\ToastEvent;
 use WBW\Bundle\CommonBundle\Service\SessionService;
 use WBW\Bundle\CommonBundle\Tests\AbstractWebTestCase;
 use WBW\Bundle\CommonBundle\Tests\Fixtures\Controller\TestAbstractController;
-use WBW\Bundle\WidgetBundle\Component\NotificationInterface;
-use WBW\Bundle\WidgetBundle\Component\ToastInterface;
+use WBW\Library\Widget\Component\NotificationInterface;
+use WBW\Library\Widget\Component\ToastInterface;
 
 /**
  * Abstract controller test.
