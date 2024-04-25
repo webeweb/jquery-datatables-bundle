3ace3be242b39aa0520ab516b475a333d5edf958
webeweb <support@webeweb.fr>
2024-04-25 16:53:04 +0200

    Update Font Awesome icon renderer: - change "use" statements

--- lib/bootstrap/Renderer/Extend/FontAwesomeIconRenderer.php
+++ lib/bootstrap/Renderer/Extend/FontAwesomeIconRenderer.php
@@ -14,7 +14,7 @@ declare(strict_types = 1);
 namespace WBW\Bundle\BootstrapBundle\Renderer\Extend;
 
 use WBW\Bundle\BootstrapBundle\Extend\FontAwesomeIconInterface;
-use WBW\Bundle\WidgetBundle\Renderer\Component\IconRenderer;
+use WBW\Library\Widget\Renderer\Component\IconRenderer;
 
 /**
  * Font Awesome icon renderer.
