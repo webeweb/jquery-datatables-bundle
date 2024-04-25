d41175ff5d9210403bbb25ff302d0cd8cd8971c4
webeweb <support@webeweb.fr>
2024-04-25 16:49:46 +0200

    Update Orange color interface: - change "use" statements

--- lib/bootstrap/Customize/Color/OrangeColorInterface.php
+++ lib/bootstrap/Customize/Color/OrangeColorInterface.php
@@ -13,7 +13,7 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\BootstrapBundle\Customize\Color;
 
-use WBW\Bundle\WidgetBundle\Component\Color\OrangeColorInterface as BaseOrangeColorInterface;
+use WBW\Library\Widget\Component\Color\OrangeColorInterface as BaseOrangeColorInterface;
 
 /**
  * Orange color interface.
