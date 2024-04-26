309fd59754c8d4ff51f8319e8d532db5714b2982
webeweb <support@webeweb.fr>
2024-04-26 16:43:57 +0200

    Copy Navigation theme provider interface (from core-library)

---
+++ lib/common/Provider/Layout/NavigationLayoutProviderInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider\Layout;
+
+use WBW\Bundle\CommonBundle\Provider\LayoutProviderInterface;
+use WBW\Library\Widget\Component\Navigation\NavigationTree;
+
+/**
+ * Navigation layout provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Layout
+ */
+interface NavigationLayoutProviderInterface extends LayoutProviderInterface {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.common.provider.layout.navigation";
+
+    /**
+     * Get the tree.
+     *
+     * @return NavigationTree Returns the tree.
+     */
+    public function getTree(): NavigationTree;
+}
