fb98511193bff8aec34e0e5da68e7009c64d2c29
webeweb <support@webeweb.fr>
2024-04-26 12:55:55 +0200

    Copy Quote provider interface (from core-library)

---
+++ lib/common/Provider/QuoteProviderInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider;
+
+use WBW\Bundle\CommonBundle\Model\QuoteInterface;
+
+/**
+ * Quote provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider
+ */
+interface QuoteProviderInterface extends ProviderInterface {
+
+    /**
+     * Tag name.
+     *
+     * @var string
+     */
+    public const QUOTE_PROVIDER_TAG_NAME = "wbw.common.provider.quote";
+
+    /**
+     * Get the authors.
+     *
+     * @return string[] Returns the authors.
+     */
+    public function getAuthors(): array;
+
+    /**
+     * Get the domain.
+     *
+     * @return string Returns the domain.
+     */
+    public function getDomain(): string;
+
+    /**
+     * Get the quotes.
+     *
+     * @return QuoteInterface[] Returns the quotes.
+     */
+    public function getQuotes(): array;
+}
