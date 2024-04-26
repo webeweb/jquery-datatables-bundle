656415aba37762c528f1a19979a4d81bd8f9ebfd
webeweb <support@webeweb.fr>
2024-04-26 16:44:18 +0200

    Copy Search theme provider interface (from core-library)

---
+++ lib/common/Tests/Provider/Layout/SearchLayoutProviderInterfaceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider\Layout;
+
+use WBW\Bundle\CommonBundle\Provider\Layout\SearchLayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Search layout provider interface test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider\Layout
+ */
+class SearchLayoutProviderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.common.provider.layout.search", SearchLayoutProviderInterface::SERVICE_NAME);
+    }
+}
