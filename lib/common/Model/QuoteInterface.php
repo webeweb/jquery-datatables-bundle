c937c6248db857e966d5014375691aa030b91416
webeweb <support@webeweb.fr>
2024-04-26 12:50:52 +0200

    Copy Quote interface (from core-library)

---
+++ lib/common/Model/QuoteInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Model;
+
+use DateTime;
+
+/**
+ * Quote interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Model
+ */
+interface QuoteInterface {
+
+    /**
+     * Get the author.
+     *
+     * @return string|null Returns the author.
+     */
+    public function getAuthor(): ?string;
+
+    /**
+     * Get the content.
+     *
+     * @return string|null Returns the content.
+     */
+    public function getContent(): ?string;
+
+    /**
+     * Get the date.
+     *
+     * @return DateTime|null Returns the date.
+     */
+    public function getDate(): ?DateTime;
+
+    /**
+     * Get the saint.
+     *
+     * @return string|null Returns the saint.
+     */
+    public function getSaint(): ?string;
+}
