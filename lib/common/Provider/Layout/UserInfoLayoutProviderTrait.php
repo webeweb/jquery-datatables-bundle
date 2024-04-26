a5ebb72c0bfcf9f85feaaeafb43ffe7c7bc88e63
webeweb <support@webeweb.fr>
2024-04-26 16:46:41 +0200

    Copy User info theme provider trait (from core-library)

---
+++ lib/common/Provider/Layout/UserInfoLayoutProviderTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider\Layout;
+
+/**
+ * User info layout provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Layout
+ */
+trait UserInfoLayoutProviderTrait {
+
+    /**
+     * User info layout provider.
+     *
+     * @var UserInfoLayoutProviderInterface|null
+     */
+    private $userInfoLayoutProvider;
+
+    /**
+     * Get the user info layout provider.
+     *
+     * @return UserInfoLayoutProviderInterface|null Returns the user info layout provider.
+     */
+    public function getUserInfoLayoutProvider(): ?UserInfoLayoutProviderInterface {
+        return $this->userInfoLayoutProvider;
+    }
+
+    /**
+     * Set the user info layout provider.
+     *
+     * @param UserInfoLayoutProviderInterface|null $userInfoLayoutProvider The user info layout provider.
+     * @return self Returns this instance.
+     */
+    protected function setUserInfoLayoutProvider(?UserInfoLayoutProviderInterface $userInfoLayoutProvider): self {
+        $this->userInfoLayoutProvider = $userInfoLayoutProvider;
+        return $this;
+    }
+}
---
+++ lib/common/Tests/Provider/Layout/UserInfoLayoutProviderTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider\Layout;
+
+use WBW\Bundle\CommonBundle\Provider\Layout\UserInfoLayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Provider\Layout\TestUserInfoLayoutProviderTrait;
+
+/**
+ * User info layout provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider\Layout
+ */
+class UserInfoLayoutProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setUserInfoLayoutProvider()
+     *
+     * @return void
+     */
+    public function testSetUserInfoLayoutProvider(): void {
+
+        // Set a User info layout provider mock.
+        $userInfoLayoutProvider = $this->getMockBuilder(UserInfoLayoutProviderInterface::class)->getMock();
+
+        $obj = new TestUserInfoLayoutProviderTrait();
+
+        $obj->setUserInfoLayoutProvider($userInfoLayoutProvider);
+        $this->assertSame($userInfoLayoutProvider, $obj->getUserInfoLayoutProvider());
+    }
+}
