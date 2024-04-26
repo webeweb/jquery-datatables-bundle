6d589259c4af62736eaf76aa42f1bc6c0352bf50
webeweb <support@webeweb.fr>
2024-04-26 12:51:07 +0200

    Copy Quote (from core-library)

---
+++ lib/common/Model/Quote.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Model;
+
+use WBW\Library\Common\Traits\DateTimes\DateTimeDateTrait;
+use WBW\Library\Common\Traits\Strings\StringContentTrait;
+
+/**
+ * Quote.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Model
+ */
+class Quote implements QuoteInterface {
+
+    use DateTimeDateTrait;
+    use StringContentTrait;
+
+    /**
+     * Author.
+     *
+     * @var string
+     */
+    private $author;
+
+    /**
+     * Saint.
+     *
+     * @var string|null
+     */
+    private $saint;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAuthor(): ?string {
+        return $this->author;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSaint(): ?string {
+        return $this->saint;
+    }
+
+    /**
+     * Set the author.
+     *
+     * @param string $author The author.
+     * @return Quote Returns this quote.
+     */
+    public function setAuthor(string $author): QuoteInterface {
+        $this->author = $author;
+        return $this;
+    }
+
+    /**
+     * Set the saint.
+     *
+     * @param string $saint The saint.
+     * @return Quote Returns this quote.
+     */
+    public function setSaint(string $saint): QuoteInterface {
+        $this->saint = $saint;
+        return $this;
+    }
+}
---
+++ lib/common/Tests/Model/QuoteTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Model;
+
+use WBW\Bundle\CommonBundle\Model\Quote;
+use WBW\Bundle\CommonBundle\Model\QuoteInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Quote test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Model
+ */
+class QuoteTest extends AbstractTestCase {
+
+    /**
+     * Test setAuthor()
+     *
+     * @return void
+     */
+    public function testSetAuthor(): void {
+
+        $obj = new Quote();
+
+        $obj->setAuthor("author");
+        $this->assertEquals("author", $obj->getAuthor());
+    }
+
+    /**
+     * Test setSaint()
+     *
+     * @return void
+     */
+    public function testSetSaint(): void {
+
+        $obj = new Quote();
+
+        $obj->setSaint("saint");
+        $this->assertEquals("saint", $obj->getSaint());
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Quote();
+
+        $this->assertInstanceOf(QuoteInterface::class, $obj);
+
+        $this->assertNull($obj->getAuthor());
+        $this->assertNull($obj->getContent());
+        $this->assertNull($obj->getDate());
+        $this->assertNull($obj->getSaint());
+    }
+}
