acbbca04569998671702bc1a979ab9a723a2c6ef
webeweb <support@webeweb.fr>
2024-04-26 12:56:04 +0200

    Copy Quote provider trait (from core-library)

---
+++ lib/common/Tests/Provider/QuoteProviderTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider;
+
+use WBW\Bundle\CommonBundle\Provider\QuoteProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Provider\TestQuoteProviderTrait;
+
+/**
+ * Quote provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider
+ */
+class QuoteProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setProvider()
+     *
+     * @return void
+     */
+    public function testSetProvider(): void {
+
+        // Set a Quote provider mock.
+        $quoteProvider = $this->getMockBuilder(QuoteProviderInterface::class)->getMock();
+
+        $obj = new TestQuoteProviderTrait();
+
+        $obj->setQuoteProvider($quoteProvider);
+        $this->assertSame($quoteProvider, $obj->getQuoteProvider());
+    }
+}
