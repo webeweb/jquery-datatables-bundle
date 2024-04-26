86d0d9888ff1499209b02c8b8c09e13af99ab322
webeweb <support@webeweb.fr>
2024-04-26 16:45:11 +0200

    Copy Application theme provider trait (from core-library)

---
+++ lib/common/Provider/Layout/ApplicationLayoutProviderTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider\Layout;
+
+/**
+ * Application layout provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Layout
+ */
+trait ApplicationLayoutProviderTrait {
+
+    /**
+     * Application layout provider.
+     *
+     * @var ApplicationLayoutProviderInterface|null
+     */
+    private $applicationLayoutProvider;
+
+    /**
+     * Get the application layout provider.
+     *
+     * @return ApplicationLayoutProviderInterface|null Returns the application layout provider.
+     */
+    public function getApplicationLayoutProvider(): ?ApplicationLayoutProviderInterface {
+        return $this->applicationLayoutProvider;
+    }
+
+    /**
+     * Set the application layout provider.
+     *
+     * @param ApplicationLayoutProviderInterface|null $applicationLayoutProvider The application layout provider.
+     * @return self Returns this instance.
+     */
+    protected function setApplicationLayoutProvider(?ApplicationLayoutProviderInterface $applicationLayoutProvider): self {
+        $this->applicationLayoutProvider = $applicationLayoutProvider;
+        return $this;
+    }
+}
---
+++ lib/common/Tests/Provider/Layout/ApplicationLayoutProviderTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider\Layout;
+
+use WBW\Bundle\CommonBundle\Provider\Layout\ApplicationLayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Provider\Layout\TestApplicationLayoutProviderTrait;
+
+/**
+ * Application layout provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider\Layout
+ */
+class ApplicationLayoutProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setApplicationLayoutProvider()
+     *
+     * @return void
+     */
+    public function testSetApplicationLayoutProvider(): void {
+
+        // Set an Application layout provider mock.
+        $applicationLayoutProvider = $this->getMockBuilder(ApplicationLayoutProviderInterface::class)->getMock();
+
+        $obj = new TestApplicationLayoutProviderTrait();
+
+        $obj->setApplicationLayoutProvider($applicationLayoutProvider);
+        $this->assertSame($applicationLayoutProvider, $obj->getApplicationLayoutProvider());
+    }
+}
