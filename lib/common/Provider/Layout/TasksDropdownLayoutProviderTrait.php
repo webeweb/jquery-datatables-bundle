f6eb46d33322f3ced0266a30d04352ede24f0ed7
webeweb <support@webeweb.fr>
2024-04-26 16:46:33 +0200

    Copy Tasks dropdown theme provider trait (from core-library)

---
+++ lib/common/Provider/Layout/TasksDropdownLayoutProviderTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider\Layout;
+
+/**
+ * Tasks dropdown layout provider trait.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Layout
+ */
+trait TasksDropdownLayoutProviderTrait {
+
+    /**
+     * Tasks dropdown layout provider.
+     *
+     * @var TasksDropdownLayoutProviderInterface|null
+     */
+    private $tasksDropdownLayoutProvider;
+
+    /**
+     * Get the tasks dropdown layout provider.
+     *
+     * @return TasksDropdownLayoutProviderInterface|null Returns the tasks dropdown layout provider.
+     */
+    public function getTasksDropdownLayoutProvider(): ?TasksDropdownLayoutProviderInterface {
+        return $this->tasksDropdownLayoutProvider;
+    }
+
+    /**
+     * Set the tasks dropdown layout provider.
+     *
+     * @param TasksDropdownLayoutProviderInterface|null $tasksDropdownLayoutProvider The tasks dropdown layout provider.
+     * @return self Returns this instance.
+     */
+    protected function setTasksDropdownLayoutProvider(?TasksDropdownLayoutProviderInterface $tasksDropdownLayoutProvider): self {
+        $this->tasksDropdownLayoutProvider = $tasksDropdownLayoutProvider;
+        return $this;
+    }
+}
---
+++ lib/common/Tests/Provider/Layout/TasksDropdownLayoutProviderTraitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider\Layout;
+
+use WBW\Bundle\CommonBundle\Provider\Layout\TasksDropdownLayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Tests\Fixtures\Provider\Layout\TestTasksDropdownLayoutProviderTrait;
+
+/**
+ * Tasks dropdown layout provider trait test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Provider\Layout
+ */
+class TasksDropdownLayoutProviderTraitTest extends AbstractTestCase {
+
+    /**
+     * Test setTasksDropdownLayoutProvider()
+     *
+     * @return void
+     */
+    public function testSetTasksDropdownLayoutProvider(): void {
+
+        // Set a Tasks dropdown layout provider mock.
+        $tasksDropdownLayoutProvider = $this->getMockBuilder(TasksDropdownLayoutProviderInterface::class)->getMock();
+
+        $obj = new TestTasksDropdownLayoutProviderTrait();
+
+        $obj->setTasksDropdownLayoutProvider($tasksDropdownLayoutProvider);
+        $this->assertSame($tasksDropdownLayoutProvider, $obj->getTasksDropdownLayoutProvider());
+    }
+}
