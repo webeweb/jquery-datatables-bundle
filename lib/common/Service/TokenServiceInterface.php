7cac954f08b53e9f251b051fac3fb8bbef94ad30
webeweb <support@webeweb.fr>
2024-04-26 11:24:55 +0200

    Copy Token generator service interface (from core-library)

---
+++ lib/common/Service/TokenServiceInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Service;
+
+use Throwable;
+
+/**
+ * Token service interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Service
+ */
+interface TokenServiceInterface {
+
+    /**
+     * Generate a token.
+     *
+     * @return string Returns the generated token.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function generateToken(): string;
+}
