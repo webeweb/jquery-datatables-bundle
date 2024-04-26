89e9c21098979f688d0fda7235cec53d7d9aad6c
webeweb <support@webeweb.fr>
2024-04-26 13:43:23 +0200

    Copy Quote manager interface (from core-library)

---
+++ lib/common/Manager/QuoteManagerInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2022 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Manager;
+
+/**
+ * Quote manager interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Manager
+ */
+interface QuoteManagerInterface extends ManagerInterface {
+
+}
