7e87d711dec8bbb0057a46bc28c27618938861e3
webeweb <support@webeweb.fr>
2024-04-27 14:56:29 +0200

    Copy Default application layout provider (from core-library)

---
+++ lib/common/Provider/Layout/DefaultApplicationLayoutProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Provider\Layout;
+
+/**
+ * Default application layout provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Layout
+ */
+class DefaultApplicationLayoutProvider implements ApplicationLayoutProviderInterface {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription(): ?string {
+        return "Common bundle";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHome(): ?string {
+        return "/";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName(): ?string {
+        return "Common<b>bundle</b>";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRoute(): ?string {
+        return "/";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTitle(): ?string {
+        return "Common bundle";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getVersion(): ?string {
+        return "dev-master";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getYear(string $startYear = null): ?string {
+
+        $today = date("Y");
+        $years = ["2018"];
+
+        if (0 < intval($startYear)) {
+            if (intval($startYear) <= intval($today)) {
+                $years = [$startYear];
+            } else {
+                $years = [$today];
+            }
+        }
+
+        if ($years[0] !== $today) {
+            $years[] = $today;
+        }
+
+        return implode("-", $years);
+    }
+}
---
+++ lib/common/Tests/Provider/Layout/DefaultApplicationLayoutProviderTest.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Provider\Layout;
+
+use DateTime;
+use WBW\Bundle\CommonBundle\Provider\Layout\ApplicationLayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Provider\Layout\DefaultApplicationLayoutProvider;
+use WBW\Bundle\CommonBundle\Provider\LayoutProviderInterface;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+
+/**
+ * Default application layout provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Layout
+ */
+class DefaultApplicationLayoutProviderTest extends AbstractTestCase {
+
+    /**
+     * Test getDescription()
+     *
+     * @return void
+     */
+    public function testGetDescription(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("Common bundle", $obj->getDescription());
+    }
+
+    /**
+     * Test getHome()
+     *
+     * @return void
+     */
+    public function testGetHome(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("/", $obj->getHome());
+    }
+
+    /**
+     * Test getName()
+     *
+     * @return void
+     */
+    public function testGetName(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("Common<b>bundle</b>", $obj->getName());
+    }
+
+    /**
+     * Test getRoute()
+     *
+     * @return void
+     */
+    public function testGetRoute(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("/", $obj->getRoute());
+    }
+
+    /**
+     * Test getTitle()
+     *
+     * @return void
+     */
+    public function testGetTitle(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("Common bundle", $obj->getTitle());
+    }
+
+    /**
+     * Test getVersion()
+     *
+     * @return void
+     */
+    public function testGetVersion(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("dev-master", $obj->getVersion());
+    }
+
+    /**
+     * Test getYear()
+     *
+     * @return void
+     */
+    public function testGetYear(): void {
+
+        // Set a Date/time mock.
+        $now = new DateTime();
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertEquals("2018-{$now->format("Y")}", $obj->getYear());
+
+        $this->assertEquals("2019-{$now->format("Y")}", $obj->getYear("2019"));
+        $this->assertEquals("2020-{$now->format("Y")}", $obj->getYear("2020"));
+        $this->assertEquals("{$now->format("Y")}", $obj->getYear($now->format("Y")));
+        $this->assertEquals("{$now->format("Y")}", $obj->getYear((string) (intval($now->format("Y")) + 1)));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DefaultApplicationLayoutProvider();
+
+        $this->assertInstanceOf(LayoutProviderInterface::class, $obj);
+        $this->assertInstanceOf(ApplicationLayoutProviderInterface::class, $obj);
+    }
+}
