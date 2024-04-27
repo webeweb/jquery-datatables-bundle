dab1d784a1fb3841c28377783baaafc0e98e08f2
webeweb <support@webeweb.fr>
2024-04-27 11:38:12 +0200

    Add Layout manager interface

---
+++ lib/common/Manager/LayoutManagerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Manager;
+
+/**
+ * Layout manager interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Manager
+ */
+interface LayoutManagerInterface extends ManagerInterface {
+
+}
