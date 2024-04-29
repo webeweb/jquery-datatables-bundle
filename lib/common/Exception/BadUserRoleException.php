0693a46f03c882864def638bc2412ddd1b8e4a96
webeweb <support@webeweb.fr>
2024-04-29 16:45:46 +0200

    Copy Bad user role exception (from core-bundle)

---
+++ lib/common/Exception/BadUserRoleException.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Exception;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use WBW\Bundle\CommonBundle\Security\Core\User\UserHelper;
+use WBW\Bundle\CommonBundle\Security\Core\User\UserTrait;
+use WBW\Library\Common\Traits\Strings\ArrayRolesTrait;
+use WBW\Library\Common\Traits\Strings\StringOriginUrlTrait;
+use WBW\Library\Common\Traits\Strings\StringRedirectUrlTrait;
+
+/**
+ * Bad user role exception.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Exception
+ */
+class BadUserRoleException extends AbstractException {
+
+    use ArrayRolesTrait;
+    use StringOriginUrlTrait;
+    use StringRedirectUrlTrait;
+    use UserTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param UserInterface $user The user.
+     * @param array $roles The roles.
+     * @param string $redirectUrl The redirect.
+     * @param string $originUrl The route.
+     */
+    public function __construct(UserInterface $user, array $roles, string $redirectUrl, string $originUrl) {
+
+        $format  = 'User "%s" is not allowed to access to "%s" with roles [%s]';
+        $message = sprintf($format, UserHelper::getIdentifier($user), $originUrl, implode(",", $roles));
+
+        parent::__construct($message, 403);
+
+        $this->setOriginUrl($originUrl);
+        $this->setRedirectUrl($redirectUrl);
+        $this->setRoles($roles);
+        $this->setUser($user);
+    }
+}
---
+++ lib/common/Tests/Exception/BadUserRoleExceptionTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Exception;
+
+use WBW\Bundle\CommonBundle\Exception\BadUserRoleException;
+use WBW\Bundle\CommonBundle\Tests\AbstractTestCase;
+use WBW\Bundle\CommonBundle\Model\User;
+
+/**
+ * Bad user role exception test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Exception
+ */
+class BadUserRoleExceptionTest extends AbstractTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $user        = new User("anonymous");
+        $roles       = ["ROLE_ADMIN", "ROLE_USER"];
+        $originUrl   = "https://github.com/webeweb";
+        $redirectUrl = "https://github.com";
+
+        $obj = new BadUserRoleException($user, $roles, $redirectUrl, $originUrl);
+
+        $this->assertEquals('User "anonymous" is not allowed to access to "https://github.com/webeweb" with roles [ROLE_ADMIN,ROLE_USER]', $obj->getMessage());
+
+        $this->assertEquals($originUrl, $obj->getOriginUrl());
+        $this->assertEquals($redirectUrl, $obj->getRedirectUrl());
+        $this->assertEquals($roles, $obj->getRoles());
+        $this->assertSame($user, $obj->getUser());
+    }
+}
