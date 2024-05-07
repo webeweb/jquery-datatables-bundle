4d4a653d866c392fcdbc26921c421e4034fee3ce
webeweb <support@webeweb.fr>
2024-05-07 10:02:46 +0200

    Copy Twig controller (from core-bundle)

---
+++ lib/common/Controller/TwigController.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Controller;
+
+use DateTime;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+use Twig\Environment;
+use Twig\TwigFunction;
+use WBW\Bundle\CommonBundle\Manager\JavascriptManager;
+use WBW\Bundle\CommonBundle\Manager\JavascriptManagerTrait;
+use WBW\Bundle\CommonBundle\Manager\StylesheetManager;
+use WBW\Bundle\CommonBundle\Manager\StylesheetManagerTrait;
+use WBW\Bundle\CommonBundle\Provider\JavascriptProviderInterface;
+use WBW\Bundle\CommonBundle\Provider\StylesheetProviderInterface;
+
+/**
+ * Twig controller.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Controller
+ */
+class TwigController extends AbstractController {
+
+    use JavascriptManagerTrait {
+        setJavascriptManager as public;
+    }
+    use StylesheetManagerTrait {
+        setStylesheetManager as public;
+    }
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    const SERVICE_NAME = "wbw.common.controller.twig";
+
+    /**
+     * Function.
+     *
+     * @param Request $request The request.
+     * @param string $name The name.
+     * @return Response Returns the response.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function functionAction(Request $request, string $name): Response {
+
+        /** @var Environment $twig */
+        $twig = $this->getTwig();
+
+        $function = $twig->getFunction($name);
+        if (false === ($function instanceof TwigFunction)) {
+            return new JsonResponse([], 404);
+        }
+
+        $callable = $function->getCallable();
+
+        $content = call_user_func_array([
+            $callable[0],
+            $callable[1],
+        ], $request->get("args", []));
+
+        return new JsonResponse(true === is_array($content) ? $content : [$content]);
+    }
+
+    /**
+     * Get the last modified.
+     *
+     * @param string $view The view.
+     * @return DateTime|null Returns the last modified.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    protected function getLastModified(string $view): ?DateTime {
+
+        $twig = $this->getTwig();
+        if (null === $twig) {
+            return null;
+        }
+
+        $path = $twig->getLoader()->getSourceContext($view)->getPath();
+        $time = filemtime($path);
+        if (false === $time) {
+            return null;
+        }
+
+        return new DateTime("@$time");
+    }
+
+    /**
+     * Resource.
+     *
+     * @param Request $request The request.
+     * @param string $type The type.
+     * @param string $name The name.
+     * @return Response Returns the response.
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function resourceAction(Request $request, string $type, string $name): Response {
+
+        $resources   = [];
+        $contentType = null;
+
+        switch ($type) {
+
+            case JavascriptProviderInterface::JAVASCRIPT_PROVIDER_EXTENSION:
+
+                /** @var JavascriptManager $manager */
+                $manager   = $this->getJavascriptManager();
+                $resources = $manager->getJavascripts();
+
+                $contentType = JavascriptProviderInterface::JAVASCRIPT_PROVIDER_CONTENT_TYPE;
+                break;
+
+            case StylesheetProviderInterface::STYLESHEET_PROVIDER_EXTENSION:
+
+                /** @var StylesheetManager $manager */
+                $manager   = $this->getStylesheetManager();
+                $resources = $manager->getStylesheets();
+
+                $contentType = StylesheetProviderInterface::STYLESHEET_PROVIDER_CONTENT_TYPE;
+                break;
+        }
+
+        if (false === array_key_exists($name, $resources)) {
+            throw $this->createNotFoundException();
+        }
+
+        $modified = $this->getLastModified($resources[$name]);
+        if (null === $modified) {
+            $modified = new DateTime();
+        }
+
+        $content = $this->renderView($resources[$name], $request->query->all());
+
+        return new Response($content, 200, [
+            "Content-Type"  => $contentType,
+            "Last-Modified" => $modified->format("D, d M Y H:i:s T"),
+        ]);
+    }
+}
---
+++ lib/common/Tests/Controller/TwigControllerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\CommonBundle\Tests\Controller;
+
+use WBW\Bundle\CommonBundle\Controller\TwigController;
+use WBW\Bundle\CommonBundle\Tests\DefaultWebTestCase as AbstractWebTestCase;
+
+/**
+ * Twig controller test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Controller
+ */
+class TwigControllerTest extends AbstractWebTestCase {
+
+    /**
+     * Test functionAction()
+     *
+     * @return void
+     */
+    public function testFunctionAction(): void {
+
+        $arg = ["font" => "s", "name" => "camera-retro", "size" => "lg", "fixedWidth" => true, "bordered" => true, "pull" => "left", "animation" => "spin", "style" => "color: #FFFFFF;"];
+        $exp = '<i class="fas fa-camera-retro fa-lg fa-fw fa-border fa-pull-left fa-spin" style="color: #FFFFFF;"></i>';
+
+        $client = $this->client;
+
+        $client->request("POST", "/twig/function/fontAwesomeIcon", ["args" => [$arg]]);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("application/json", $client->getResponse()->headers->get("Content-Type"));
+
+        // Check the JSON response.
+        $res = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertEquals($exp, $res[0]);
+    }
+
+    /**
+     * Test functionAction()
+     *
+     * @return void
+     */
+    public function testFunctionActionWithStatus404(): void {
+
+        $client = $this->client;
+
+        $client->request("POST", "/twig/function/exception");
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals("application/json", $client->getResponse()->headers->get("Content-Type"));
+    }
+
+    /**
+     * Test resourceAction()
+     *
+     * @return void
+     */
+    public function testResourceAction(): void {
+
+        $client = $this->client;
+
+        $client->request("GET", "/twig/resource/404.js");
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertEquals("text/html; charset=UTF-8", $client->getResponse()->headers->get("Content-Type"));
+    }
+
+    /**
+     * Test resourceAction()
+     *
+     * @return void
+     */
+    public function testResourceActionWithJavascript(): void {
+
+        $client = $this->client;
+
+        $client->request("GET", "/twig/resource/WBWCommonLeaflet.js");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("application/javascript", $client->getResponse()->headers->get("Content-Type"));
+        $this->assertNotNull($client->getResponse()->headers->get("Last-Modified"));
+    }
+
+    /**
+     * Test resourceAction()
+     *
+     * @return void
+     */
+    public function testResourceActionWithStylesheet(): void {
+
+        $client = $this->client;
+
+        $client->request("GET", "/twig/resource/WBWCommonTest.css");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals("text/css; charset=utf-8", $client->getResponse()->headers->get("Content-Type"));
+        $this->assertNotNull($client->getResponse()->headers->get("Last-Modified"));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.common.controller.twig", TwigController::SERVICE_NAME);
+    }
+}
