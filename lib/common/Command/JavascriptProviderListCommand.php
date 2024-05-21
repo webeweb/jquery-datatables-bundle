eb1cb67ed7029fe4238c619e7e2ea955801a83b3
webeweb <support@webeweb.fr>
2024-05-21 09:58:36 +0200

    Update Javascript provider list command: - remove optional argument

--- lib/common/Command/JavascriptProviderListCommand.php
+++ lib/common/Command/JavascriptProviderListCommand.php
@@ -86,8 +86,8 @@ class JavascriptProviderListCommand extends AbstractProviderListCommand {
     protected function getHeaders(): array {
 
         return [
-            $this->translate("command.header.class", [], WBWCommonBundle::getTranslationDomain(), "en"),
-            $this->translate("command.header.javascripts", [], WBWCommonBundle::getTranslationDomain(), "en"),
+            $this->translate("command.header.class", [], WBWCommonBundle::getTranslationDomain()),
+            $this->translate("command.header.javascripts", [], WBWCommonBundle::getTranslationDomain()),
         ];
     }
 
