0ad356435abc77e36b36abe6c20548726632d59d
webeweb <support@webeweb.fr>
2024-05-28 18:38:11 +0200

    Add Image provider list command

---
+++ lib/common/Command/ImageProviderListCommand.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\CommonBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use WBW\Bundle\CommonBundle\Manager\ImageManagerTrait;
+use WBW\Bundle\CommonBundle\Provider\ImageProviderInterface;
+use WBW\Bundle\CommonBundle\WBWCommonBundle;
+
+/**
+ * Image provider list command.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Command
+ */
+class ImageProviderListCommand extends AbstractProviderListCommand {
+
+    use ImageManagerTrait {
+        setImageManager as public;
+    }
+
+    /**
+     * Command name.
+     *
+     * @var string
+     */
+    public const COMMAND_NAME = "wbw:common:image:provider:list";
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.common.command.image_provider_list";
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure(): void {
+        $this
+            ->setDescription("List the image providers")
+            ->setHelp(static::formatHelp("list the image providers"))
+            ->setName(self::COMMAND_NAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+
+        $io = $this->newStyle($input, $output);
+        $this->displayTitle($io, $this->getDescription());
+
+        $rows = [];
+
+        /** @var ImageProviderInterface $current */
+        foreach ($this->getImageManager()->getProviders() as $current) {
+            $rows[] = $this->renderRow($current);
+        }
+
+        $this->sortRows($rows);
+
+        $io->table($this->getHeaders(), $rows);
+        $this->displayFooter($io, count($rows), "command.footer.provider_list.success", "command.footer.provider_list.warning", WBWCommonBundle::getTranslationDomain(), "en");
+
+        return 0;
+    }
+
+    /**
+     * Get the headers.
+     *
+     * @return string[] Returns the headers.
+     */
+    protected function getHeaders(): array {
+
+        return [
+            $this->translate("command.header.name", [], WBWCommonBundle::getTranslationDomain(), "en"),
+            $this->translate("command.header.class", [], WBWCommonBundle::getTranslationDomain(), "en"),
+            $this->translate("command.header.images", [], WBWCommonBundle::getTranslationDomain(), "en"),
+        ];
+    }
+
+    /**
+     * Render a row.
+     *
+     * @param ImageProviderInterface $provider The provider.
+     * @return string[] Returns the rendered row.
+     */
+    protected function renderRow(ImageProviderInterface $provider): array {
+
+        $length = strlen($this->getHeaders()[2]);
+        $format = "%{$length}d";
+
+        return [
+            "",
+            get_class($provider),
+            "",
+        ];
+    }
+}
---
+++ lib/common/Tests/Command/ImageProviderListCommandTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\CommonBundle\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use WBW\Bundle\CommonBundle\Command\ImageProviderListCommand;
+use WBW\Bundle\CommonBundle\Manager\ImageManager;
+use WBW\Bundle\CommonBundle\Tests\DefaultWebTestCase as AbstractWebTestCase;
+
+/**
+ * Image provider list command test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Tests\Command
+ */
+class ImageProviderListCommandTest extends AbstractWebTestCase {
+
+    /**
+     * Test execute()
+     *
+     * @return void
+     */
+    public function testExecute(): void {
+
+        $obj = new ImageProviderListCommand();
+        $obj->setImageManager(static::$kernel->getContainer()->get(ImageManager::SERVICE_NAME . ".alias"));
+        $obj->setTranslator(static::$kernel->getContainer()->get("translator"));
+
+        // Set an Application mock.
+        $application = new Application(static::$kernel);
+        $application->add($obj);
+
+        // Set a Command mock.
+        $command = $application->find(ImageProviderListCommand::COMMAND_NAME);
+
+        // Set a Command tester.
+        $commandTester = new CommandTester($command);
+
+        $res = $commandTester->execute([
+            "command" => $command->getName(),
+        ]);
+        $this->assertEquals(0, $res);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString("List the image providers", $output);
+        $this->assertStringContainsString("[OK] No provider to list", $output);
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw:common:image:provider:list", ImageProviderListCommand::COMMAND_NAME);
+        $this->assertEquals("wbw.common.command.image_provider_list", ImageProviderListCommand::SERVICE_NAME);
+
+        $obj = new ImageProviderListCommand();
+
+        $this->assertEquals("List the image providers", $obj->getDescription());
+        $this->assertNotNull($obj->getHelp());
+        $this->assertEquals(ImageProviderListCommand::COMMAND_NAME, $obj->getName());
+    }
+}
