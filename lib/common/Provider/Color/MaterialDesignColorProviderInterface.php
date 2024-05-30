1a42f7c712659db7a9d6bfbc7eead677de6141f6
webeweb <support@webeweb.fr>
2024-05-30 11:11:29 +0200

    Add Material Design color provider interface

---
+++ lib/common/Provider/Color/MaterialDesignColorProviderInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\CommonBundle\Provider\Color;
+
+use WBW\Bundle\CommonBundle\Provider\ColorProviderInterface;
+
+/**
+ * Material design color provider interface.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\CommonBundle\Provider\Color
+ */
+interface MaterialDesignColorProviderInterface extends ColorProviderInterface {
+
+}
