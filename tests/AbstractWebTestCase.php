acdebecabb627d0e83a3394a03ee4c1162af6788
webeweb <support@webeweb.fr>
2025-12-01 11:06:51 +0100

    Update abstract web test case

--- tests/AbstractWebTestCase.php
+++ tests/AbstractWebTestCase.php
@@ -13,7 +13,10 @@ declare(strict_types = 1);
 
 namespace WBW\Bundle\DataTablesBundle\Tests;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Throwable;
 use WBW\Bundle\CommonBundle\Tests\DefaultWebTestCase as BaseWebTestCase;
+use WBW\Bundle\DataTablesBundle\Tests\Fixtures\TestFixtures;
 
 /**
  * Abstract web test case.
@@ -23,4 +26,21 @@ use WBW\Bundle\CommonBundle\Tests\DefaultWebTestCase as BaseWebTestCase;
  */
 abstract class AbstractWebTestCase extends BaseWebTestCase {
 
+    /**
+     * Set up the employee entities.
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    protected static function setUpEmployeeEntities(): void {
+
+        /** @var EntityManagerInterface $em */
+        $em = static::$kernel->getContainer()->get("doctrine.orm.entity_manager");
+
+        foreach (TestFixtures::getEmployees() as $entity) {
+            $em->persist($entity);
+        }
+
+        $em->flush();
+    }
 }
