b3ceda23ddae5f0cf7a67a58d81366fbcffc7f70
webeweb <support@webeweb.fr>
2025-12-01 11:44:31 +0100

    Add Read DataTables controller

---
+++ src/Controller/ReadDataTablesController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2025 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Controller;
+
+/**
+ * Read DataTables controller.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Controller
+ */
+class ReadDataTablesController extends AbstractDataTablesController {
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.datatables.controller.read";
+}
