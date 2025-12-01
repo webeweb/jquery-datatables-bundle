58e3ac8b6a3060ccf31e8327af353d8280f28798
webeweb <support@webeweb.fr>
2025-12-01 10:44:02 +0100

    Add Create DataTables controller

---
+++ tests/Controller/CreateDataTablesControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2025 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\DataTablesBundle\Tests\Controller;
+
+use WBW\Bundle\DataTablesBundle\Controller\CreateDataTablesController;
+use WBW\Bundle\DataTablesBundle\Tests\AbstractWebTestCase;
+
+/**
+ * Create DataTables controller test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DataTablesBundle\Tests\Controller
+ */
+class CreateDataTablesControllerTest extends AbstractWebTestCase {
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.datatables.controller.create", CreateDataTablesController::SERVICE_NAME);
+    }
+}
