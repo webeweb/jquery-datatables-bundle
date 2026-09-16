2fd3bb043f108fdbd14e09dd790168ee38fb5eb5
webeweb <support@webeweb.fr>
2026-09-16 19:17:17 +0200

    Copy abstract test case (from EDM bundle)

---
+++ lib/document/Tests/AbstractTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends BaseTestCase {
+
+}
