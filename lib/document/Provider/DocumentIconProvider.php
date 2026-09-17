915464210bc5386f30c58dfaa41536e1e3578d54
webeweb <support@webeweb.fr>
2026-09-17 12:13:44 +0200

    Copy Document icon provider (from EDM bundle)

---
+++ lib/document/Provider/DocumentIconProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Provider;
+
+use WBW\Bundle\CommonBundle\Provider\Image\MimeTypeImageProvider;
+use WBW\Bundle\CommonBundle\Provider\Image\MimeTypeImageProviderTrait;
+use WBW\Bundle\DocumentBundle\Model\DocumentInterface;
+
+/**
+ * Document icon provider.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Provider
+ */
+class DocumentIconProvider {
+
+    use MimeTypeImageProviderTrait;
+
+    /**
+     * Service name.
+     *
+     * @var string
+     */
+    public const SERVICE_NAME = "wbw.edm.provider.document_icon";
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->setMimeTypeImageProvider(new MimeTypeImageProvider());
+    }
+
+    /**
+     * Get an icon.
+     *
+     * @param DocumentInterface $document The document.
+     * @return string Returns the icon.
+     */
+    public function getIcon(DocumentInterface $document): string {
+
+        $name = $this->getMimeType($document);
+
+        return $this->getMimeTypeImageProvider()->getImage($name);
+    }
+
+    /**
+     * Get the icon asset.
+     *
+     * @param DocumentInterface $document The document.
+     * @return string Returns the icon asset.
+     */
+    public function getIconAsset(DocumentInterface $document): string {
+
+        $name = $this->getMimeType($document);
+
+        return $this->getMimeTypeImageProvider()->getImageUrl($name);
+    }
+
+    /**
+     * Get the mime type.
+     *
+     * @param DocumentInterface $document The document.
+     * @return string Returns the mime type.
+     */
+    protected function getMimeType(DocumentInterface $document): string {
+        return true === $document->isDirectory() ? "folder" : $document->getMimeType();
+    }
+}
---
+++ lib/document/Tests/Provider/DocumentIconProviderTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Tests\Provider;
+
+use WBW\Bundle\DocumentBundle\Entity\Document;
+use WBW\Bundle\DocumentBundle\Model\DocumentInterface;
+use WBW\Bundle\DocumentBundle\Provider\DocumentIconProvider;
+use WBW\Bundle\DocumentBundle\Tests\AbstractTestCase;
+
+/**
+ * Document icon provider test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Tests\Provider
+ */
+class DocumentIconProviderTest extends AbstractTestCase {
+
+    /**
+     * Test getIcon()
+     *
+     * @return void
+     */
+    public function testGetIcon(): void {
+
+        // Set a Document mock.
+        $document = new Document();
+        $document->setType(DocumentInterface::TYPE_DOCUMENT);
+        $document->setMimeType("application/x-php");
+
+        $obj = new DocumentIconProvider();
+
+        $this->assertEquals("application-x-php.svg", $obj->getIcon($document));
+    }
+
+    /**
+     * Test getIconAsset()
+     *
+     * @return void
+     */
+    public function testGetIconAsset(): void {
+
+        // Set a Document mock.
+        $document = new Document();
+        $document->setType(DocumentInterface::TYPE_DIRECTORY);
+
+        $obj = new DocumentIconProvider();
+
+        $this->assertStringContainsString("folder.svg", $obj->getIconAsset($document));
+    }
+
+    /**
+     * Test getIcon()
+     *
+     * @return void
+     */
+    public function testGetIconWithDirectory(): void {
+
+        // Set a Document mock.
+        $document = new Document();
+        $document->setType(DocumentInterface::TYPE_DIRECTORY);
+
+        $obj = new DocumentIconProvider();
+
+        $this->assertEquals("folder.svg", $obj->getIcon($document));
+    }
+
+    /**
+     * Test __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("wbw.edm.provider.document_icon", DocumentIconProvider::SERVICE_NAME);
+
+        $obj = new DocumentIconProvider();
+
+        $this->assertNotNull($obj->getMimeTypeImageProvider());
+    }
+}
