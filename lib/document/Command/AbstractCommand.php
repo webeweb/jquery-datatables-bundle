afa3cfca36078b4b76053611ab9f952f1680db81
webeweb <support@webeweb.fr>
2026-09-17 12:09:59 +0200

    Copy abstract command (from EDM bundle)

---
+++ lib/document/Command/AbstractCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Command;
+
+use WBW\Bundle\CommonBundle\Command\AbstractCommand as BaseCommand;
+
+/**
+ * Abstract command.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Command
+ */
+class AbstractCommand extends BaseCommand {
+
+}
