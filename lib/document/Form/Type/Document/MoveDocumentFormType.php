acf4ec6ea6e6806dba9c6b25a1c84bc2ee3b6ef7
webeweb <support@webeweb.fr>
2026-09-18 19:31:19 +0200

    Copy Move document form type (from EDM bundle)

---
+++ lib/document/Form/Type/Document/MoveDocumentFormType.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Form\Type\Document;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\CommonBundle\Form\Factory\ChoiceTypeFactory;
+use WBW\Bundle\DocumentBundle\Entity\Document;
+use WBW\Bundle\DocumentBundle\Form\Type\AbstractDocumentFormType;
+use WBW\Bundle\DocumentBundle\Model\DocumentInterface;
+use WBW\Bundle\DocumentBundle\WBWDocumentBundle;
+use WBW\Library\Common\Sorter\AlphabeticalTreeSort;
+
+/**
+ * Move document form type.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Form\Type\Document
+ */
+class MoveDocumentFormType extends AbstractDocumentFormType {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+
+        $disabled = $options["disabled"];
+
+        $sorter = new AlphabeticalTreeSort(array_values($options["entity.parent"]));
+        $sorter->sort();
+
+        $parent = ChoiceTypeFactory::newEntityType(Document::class, $sorter->getNodes());
+
+        $builder
+            ->add("parent", EntityType::class, array_merge([
+                "disabled" => $disabled,
+                "label"    => "label.parent",
+                "required" => false,
+            ], $parent))
+            ->addEventListener(FormEvents::PRE_SET_DATA, [$this, "onPreSetData"]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            "data_class"         => Document::class,
+            "translation_domain" => WBWDocumentBundle::getTranslationDomain(),
+        ]);
+        $resolver->setRequired("entity.parent");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getBlockPrefix(): string {
+        return parent::getBlockPrefix() . "_move";
+    }
+
+    /**
+     * On pre set data.
+     *
+     * @param FormEvent $event The form event.
+     * @return FormEvent Returns the form event.
+     */
+    public function onPreSetData(FormEvent $event): FormEvent {
+
+        /** @var DocumentInterface $document */
+        $document = $event->getData();
+
+        if (null !== $document) {
+            $document->saveParent();
+        }
+
+        return $event;
+    }
+}
---
+++ lib/document/Tests/Form/Type/Document/MoveDocumentFormTypeTest.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the jquery-datatables-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace WBW\Bundle\DocumentBundle\Tests\Form\Type\Document;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormEvent;
+use WBW\Bundle\CommonBundle\Tests\DefaultFormTypeTestCase;
+use WBW\Bundle\DocumentBundle\DependencyInjection\WBWDocumentExtension;
+use WBW\Bundle\DocumentBundle\Entity\Document;
+use WBW\Bundle\DocumentBundle\Form\Type\Document\MoveDocumentFormType;
+use WBW\Bundle\DocumentBundle\WBWDocumentBundle;
+
+/**
+ * Move document type test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Bundle\DocumentBundle\Tests\Form\Type\Document
+ */
+class MoveDocumentFormTypeTest extends DefaultFormTypeTestCase {
+
+    /**
+     * Test buildForm()
+     *
+     * @return void
+     */
+    public function testBuildForm(): void {
+
+        $obj = new MoveDocumentFormType();
+
+        $obj->buildForm($this->formBuilder, [
+            "disabled"      => false,
+            "entity.parent" => [],
+        ]);
+
+        $this->assertCount(1, $this->children);
+
+        $this->assertArrayHasKey("parent", $this->children);
+        $this->assertEquals(EntityType::class, $this->children["parent"]["type"]);
+        $this->assertFalse($this->children["parent"]["options"]["disabled"]);
+        $this->assertEquals("label.parent", $this->children["parent"]["options"]["label"]);
+        $this->assertFalse($this->children["parent"]["options"]["required"]);
+    }
+
+    /**
+     * Test configureOptions()
+     *
+     * @return void
+     */
+    public function testConfigureOptions(): void {
+
+        $obj = new MoveDocumentFormType();
+
+        $obj->configureOptions($this->resolver);
+
+        $res = [
+            "data_class"         => Document::class,
+            "translation_domain" => WBWDocumentBundle::getTranslationDomain(),
+        ];
+        $this->assertEquals($res, $this->defaults);
+    }
+
+    /**
+     * Test getBlockPrefix() method.
+     *
+     * @return void
+     */
+    public function testGetBlockPrefix(): void {
+
+        $obj = new MoveDocumentFormType();
+
+        $this->assertEquals(WBWDocumentExtension::EXTENSION_ALIAS . "_document_move", $obj->getBlockPrefix());
+    }
+
+    /**
+     * Test onPreSetData()
+     *
+     * @return void
+     */
+    public function testOnPreSetData(): void {
+
+        // Set a Document mock.
+        $document = new Document();
+        $document->setParent(new Document());
+
+        // Set a Form event mock.
+        $formEvent = new FormEvent($this->form, $document);
+
+        $obj = new MoveDocumentFormType();
+
+        $this->assertSame($formEvent, $obj->onPreSetData($formEvent));
+        $this->assertSame($document->getParent(), $document->getSavedParent());
+    }
+}
